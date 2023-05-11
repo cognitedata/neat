7c746771d0a9c90039ca4d3fbb5ea244b2ead1b6
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-05-11 09:14:39 +0200

    Workflow Approval Tests (#20)
    * tests: Added type hints for test client
    
    * tests: Setup Mock CogniteClient
    
    * tests: Created failing tests for workflow
    
    * feat: Wrote memory client
    
    * tests: Approval tests for default workflow

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -2,8 +2,16 @@ import pytest
 from fastapi.testclient import TestClient
 
 from cognite.neat.explorer.explorer import app
+from tests.api.memory_cognite_client import memory_cognite_client
 
 
-@pytest.fixture(scope="session")
-def fastapi_client():
-    return TestClient(app)
+@pytest.fixture(scope="function")
+def cognite_client():
+    with memory_cognite_client() as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+def fastapi_client(cognite_client):
+    with TestClient(app) as test_client:
+        yield test_client
--- tests/api/test_workflows.py
+++ tests/api/test_workflows.py
@@ -1,8 +1,12 @@
 import pytest
+from cognite.client import CogniteClient
+from starlette.testclient import TestClient
 
 from cognite.neat.constants import EXAMPLE_WORKFLOWS
 from cognite.neat.core.workflow import BaseWorkflow
 from cognite.neat.core.workflow.model import WorkflowDefinition
+from cognite.neat.explorer.data_classes.rest import RunWorkflowRequest
+from tests.api.memory_cognite_client import MemoryClient
 
 
 @pytest.fixture(scope="session")
@@ -20,7 +24,7 @@ def workflow_names() -> list[str]:
     return [example.name for example in EXAMPLE_WORKFLOWS.iterdir()]
 
 
-def test_load_example_workflows_loaded(workflow_names: list[str], fastapi_client):
+def test_load_example_workflows_loaded(workflow_names: list[str], fastapi_client: TestClient):
     # Act
     response = fastapi_client.get("/api/workflow/workflows")
 
@@ -29,7 +33,7 @@ def test_load_example_workflows_loaded(workflow_names: list[str], fastapi_client
     assert sorted(result["workflows"]) == sorted(workflow_names)
 
 
-def test_load_rules(transformation_rules, fastapi_client):
+def test_load_rules(transformation_rules, fastapi_client: TestClient):
     response = fastapi_client.get("/api/rules")
 
     # Assert
@@ -37,3 +41,19 @@ def test_load_rules(transformation_rules, fastapi_client):
     rules = response.json()
     assert len(transformation_rules.classes) == len(rules["classes"])
     assert len(transformation_rules.properties) == len(rules["properties"])
+
+
+def test_run_default_workflow(cognite_client: CogniteClient, fastapi_client: TestClient, data_regression):
+    response = fastapi_client.post(
+        "/api/workflow/start",
+        json=RunWorkflowRequest(name="default", sync=True, config={}, start_step="Not used").dict(),
+    )
+
+    assert response.status_code == 200
+    result = response.json()["result"]
+    assert result["error_text"] is None
+    data = {}
+    for resource_name in ["assets", "relationships", "labels"]:
+        memory: MemoryClient = getattr(cognite_client, resource_name)
+        data[resource_name] = memory.dump(ordered=True, exclude={"metadata.start_time", "metadata.update_time"})
+    data_regression.check(data, basename="default_workflow")
