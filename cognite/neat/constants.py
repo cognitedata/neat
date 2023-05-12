c0de032e56ce85707f5776bad85916c835f8d94a
nikokaoja <nikola.vasiljevic@cognite.com>
2023-05-12 17:47:17 +0200

    added tests

--- cognite/neat/constants.py
+++ cognite/neat/constants.py
@@ -9,5 +9,6 @@ EXAMPLE_SOURCE_GRAPHS = EXAMPLES_DIRECTORY / "source_graphs"
 EXAMPLE_WORKFLOWS = EXAMPLES_DIRECTORY / "workflows"
 
 TNT_TRANSFORMATION_RULES = EXAMPLE_RULES / "Rules-Nordic44-to-TNT.xlsx"
+SIMPLE_TRANSFORMATION_RULES = EXAMPLE_RULES / "sheet2cdf-transformation-rules.xlsx"
 NORDIC44_KNOWLEDGE_GRAPH = EXAMPLE_SOURCE_GRAPHS / "Knowledge-Graph-Nordic44.xml"
 UI_PATH = PACKAGE_DIRECTORY / "explorer-ui" / "neat-app" / "build"
