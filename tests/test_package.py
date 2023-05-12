9364b7722871dd52f2ced75b511e0225fc446562
anders-albert <anders0albert@gmail.com>
2023-05-12 17:02:08 +0200

    tests: Extended check of versions

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,10 +1,29 @@
+import re
 import tomllib
 
 from cognite import neat
-from tests.config import PYPROJECT_TOML
+from tests.config import PYPROJECT_TOML, ROOT
+
+
+def _extract_version_from_file(filename, search_pattern, error_message):
+    changelog = (ROOT / filename).read_text()
+    if not (changelog_version_result := re.search(search_pattern, changelog)):
+        raise ValueError(error_message)
+    return changelog_version_result.groups()[0]
 
 
 def test_consistent_version_variables():
     pyproject = tomllib.loads(PYPROJECT_TOML.read_text())
+    pyproject_toml = pyproject["tool"]["poetry"]["version"]
 
-    assert neat.__version__ == pyproject["tool"]["poetry"]["version"], "Inconsistent version variables"
+    changelog_version = _extract_version_from_file(
+        "CHANGELOG.md",
+        r"\[(\d+\.\d+\.\d+)\]",
+        "Failed to obtain changelog version",
+    )
+    docker_version = _extract_version_from_file(
+        "Makefile",
+        r'version="(\d+\.\d+\.\d+)"',
+        "Failed to obtain docker version",
+    )
+    assert neat.__version__ == changelog_version == pyproject_toml == docker_version, "Inconsistent version variables"
