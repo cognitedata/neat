040fe926580e84ca3fb71cdc06d7e06f7f8db4ef
nikokaoja <nikola.vasiljevic@cognite.com>
2023-05-31 13:31:52 +0200

    added transformation rules template

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,17 +1,17 @@
-# import pytest
-# from fastapi.testclient import TestClient
+import pytest
+from fastapi.testclient import TestClient
 
-# from cognite.neat.explorer.explorer import app
-# from tests.api.memory_cognite_client import memory_cognite_client
-
-
-# @pytest.fixture(scope="function")
-# def cognite_client():
-#     with memory_cognite_client() as client:
-#         yield client
+from cognite.neat.explorer.explorer import app
+from tests.api.memory_cognite_client import memory_cognite_client
 
 
-# @pytest.fixture(scope="function")
-# def fastapi_client(cognite_client):
-#     with TestClient(app) as test_client:
-#         yield test_client
+@pytest.fixture(scope="function")
+def cognite_client():
+    with memory_cognite_client() as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+def fastapi_client(cognite_client):
+    with TestClient(app) as test_client:
+        yield test_client
