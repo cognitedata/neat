c31011534cf90d3e5eb8aadfdc1e8fbacc0ef8c4
nikokaoja <nikola.vasiljevic@cognite.com>
2023-05-31 12:32:30 +0200

    imprved notebook

--- cognite/neat/core/extractors/graph_sheet_to_graph.py
+++ cognite/neat/core/extractors/graph_sheet_to_graph.py
@@ -2,88 +2,16 @@ import logging
 import warnings
 
 import pandas as pd
-from rdflib import RDF, Graph, Literal
+from rdflib import RDF, XSD, Literal, Namespace
 
 from cognite.neat.core.data_classes.transformation_rules import TransformationRules
 
 
-def sheet2graph(
-    graph_capturing_sheet: dict[str, pd.DataFrame], transformation_rule: TransformationRules, separator: str = ","
-):
-    """Converts a graph capturing sheet to an RDF graph
-
-    Parameters
-    ----------
-    graph_capturing_sheet : dict[str, pd.DataFrame]
-        Graph capturing sheet
-    transformation_rule : TransformationRules
-        Transformation rules
-    separator : str, optional
-        Multi value separator, by default ","
-    """
-    # Validation that everything is in order before proceeding
-    validate_if_graph_capturing_sheet_empty(graph_capturing_sheet)
-    validate_rules_graph_pair(graph_capturing_sheet, transformation_rule)
-
-    # get class property pairs
-    class_property_pairs = transformation_rule.get_class_property_pairs()
-
-    # Now create empty graph
-    graph = Graph()
-
-    # Add namespaces
-    for prefix, namespace in transformation_rule.prefixes.items():
-        graph.bind(prefix, namespace)
-    graph.bind(transformation_rule.metadata.prefix, transformation_rule.metadata.namespace)
-
-    # Add triples from the capturing sheet to the graph by iterating over the capturing sheet
-    # iterate over sheets
-    for sheet_name, df in graph_capturing_sheet.items():
-        # iterate over sheet rows
-        for _, row in df.iterrows():
-            if row.identifier is None:
-                msg = f"Missing identifier in sheet {sheet_name} at row {row.name}! Skipping..."
-                logging.warning(msg)
-                warnings.warn(
-                    msg,
-                    stacklevel=2,
-                )
-                continue
-
-            # iterate over sheet rows properties
-            for property_, value in row.to_dict().items():
-                if property_ == "identifier":
-                    graph.add(
-                        (
-                            transformation_rule.metadata.namespace[row.identifier],
-                            RDF.type,
-                            transformation_rule.metadata.namespace[sheet_name],
-                        )
-                    )
-
-                elif class_property_pairs[sheet_name][property_].property_type == "ObjectProperty" and value:
-                    for v in value.split(separator):
-                        graph.add(
-                            (
-                                transformation_rule.metadata.namespace[row.identifier],
-                                transformation_rule.metadata.namespace[property_],
-                                transformation_rule.metadata.namespace[v.strip()],
-                            )
-                        )
-                elif value:
-                    graph.add(
-                        (
-                            transformation_rule.metadata.namespace[row.identifier],
-                            transformation_rule.metadata.namespace[property_],
-                            Literal(value),
-                        )
-                    )
-
-    return graph
-
-
 def sheet2triples(
-    graph_capturing_sheet: dict[str, pd.DataFrame], transformation_rule: TransformationRules, separator: str = ","
+    graph_capturing_sheet: dict[str, pd.DataFrame],
+    transformation_rule: TransformationRules,
+    separator: str = ",",
+    namespace: str = None,
 ) -> list[tuple]:
     """Converts a graph capturing sheet to rdf triples
 
@@ -94,7 +22,9 @@ def sheet2triples(
     transformation_rule : TransformationRules
         Transformation rules
     separator : str, optional
-        Multi value separator, by default ","
+        Multi value separator at cell level, by default ","
+    namespace : str, optional
+        In case of a custom namespace, by default None meaning the namespace is taken from the transformation rules
     """
     # Validation that everything is in order before proceeding
     validate_if_graph_capturing_sheet_empty(graph_capturing_sheet)
@@ -103,13 +33,16 @@ def sheet2triples(
     # get class property pairs
     class_property_pairs = transformation_rule.get_class_property_pairs()
 
+    # namespace selection
+    if namespace is None:
+        instance_namespace = transformation_rule.metadata.namespace
+    else:
+        instance_namespace = Namespace(namespace)
+
+    model_namespace = Namespace(transformation_rule.metadata.namespace)
+
     # Now create empty graph
     triples = []
-
-    # # Add namespaces
-    # for prefix, namespace in transformation_rule.prefixes.items():
-    #     graph.bind(prefix, namespace)
-    # graph.bind(transformation_rule.metadata.prefix, transformation_rule.metadata.namespace)
 
     # Add triples from the capturing sheet to the graph by iterating over the capturing sheet
     # iterate over sheets
@@ -127,33 +60,39 @@ def sheet2triples(
 
             # iterate over sheet rows properties
             for property_, value in row.to_dict().items():
+                # Setting RDF type of the instance
                 if property_ == "identifier":
                     triples.append(
                         (
-                            transformation_rule.metadata.namespace[row.identifier],
+                            instance_namespace[row.identifier],
                             RDF.type,
-                            transformation_rule.metadata.namespace[sheet_name],
+                            model_namespace[sheet_name],
                         )
                     )
 
+                # Adding object properties
                 elif class_property_pairs[sheet_name][property_].property_type == "ObjectProperty" and value:
-                    for v in value.split(separator):
-                        triples.append(
-                            (
-                                transformation_rule.metadata.namespace[row.identifier],
-                                transformation_rule.metadata.namespace[property_],
-                                transformation_rule.metadata.namespace[v.strip()],
-                            )
+                    triples.extend(
+                        (
+                            instance_namespace[row.identifier],
+                            model_namespace[property_],
+                            instance_namespace[v.strip()],
                         )
+                        for v in value.split(separator)
+                    )
+
+                # Adding data properties
+                # TODO: Add support for datatype
                 elif value:
                     triples.append(
                         (
-                            transformation_rule.metadata.namespace[row.identifier],
-                            transformation_rule.metadata.namespace[property_],
-                            Literal(value),
+                            instance_namespace[row.identifier],
+                            model_namespace[property_],
+                            Literal(
+                                value, datatype=XSD[class_property_pairs[sheet_name][property_].expected_value_type]
+                            ),
                         )
                     )
-
     return triples
 
 
