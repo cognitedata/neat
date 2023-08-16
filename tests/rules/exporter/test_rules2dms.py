3cb648d5b5fb48df3d2109919dc7cc7336d5805f
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2023-08-16 08:22:17 +0200

    Update tests/rules/exporter/test_rules2dms.py
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- tests/rules/exporter/test_rules2dms.py
+++ tests/rules/exporter/test_rules2dms.py
@@ -8,8 +8,8 @@ def test_rules2dms(simple_rules):
 
     assert len(data_model.containers) == 4
     assert len(data_model.views) == 4
-    assert list(data_model.views.keys()) == ["CountryGroup", "Country", "PriceArea", "PriceAreaConnection"]
-    assert list(data_model.containers.keys()) == ["CountryGroup", "Country", "PriceArea", "PriceAreaConnection"]
+    assert list(data_model.views) == ["CountryGroup", "Country", "PriceArea", "PriceAreaConnection"]
+    assert list(data_model.containers) == ["CountryGroup", "Country", "PriceArea", "PriceAreaConnection"]
     assert data_model.version == "0_1"
     assert data_model.space == "playground"
     assert data_model.external_id == "neat"
