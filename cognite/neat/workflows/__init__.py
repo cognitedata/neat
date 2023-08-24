3ea3e5e731aefd6c7046cc771f4a9bd2d142b425
nikokaoja <nikola.vasiljevic@cognite.com>
2023-08-24 14:33:18 +0200

    linting

--- cognite/neat/workflows/__init__.py
+++ cognite/neat/workflows/__init__.py
@@ -1,5 +1,4 @@
 from cognite.neat.workflows.base import BaseWorkflow
-
 from cognite.neat.workflows.manager import WorkflowManager
 from cognite.neat.workflows.model import FlowMessage, WorkflowFullStateReport, WorkflowStepDefinition, WorkflowStepEvent
 
