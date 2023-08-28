fba652210cde4fae92c6f6522515ddac9677ee92
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2023-08-28 08:37:45 +0200

    Update cognite/neat/rules/exporter/rules2pydantic_models.py
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- cognite/neat/rules/exporter/rules2pydantic_models.py
+++ cognite/neat/rules/exporter/rules2pydantic_models.py
@@ -231,8 +231,7 @@ def edges_one_to_one(self) -> list[str]:
     return [
         field
         for field in self.model_fields_set
-        if self.model_fields[field].json_schema_extra
-        and self.model_fields[field].json_schema_extra.get("property_type", "") == "EdgeOneToOne"
+        if (schema := self.model_fields[field].json_schema_extra) and schema.get("property_type") == "EdgeOneToOne"
     ]
 
 
