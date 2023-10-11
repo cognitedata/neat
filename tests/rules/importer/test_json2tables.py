e20dc972b6a2bc0fd602b5812f60ffbf46f7bb3f
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-10-11 18:09:54 +0200

    tests: Better orged test (#138)

--- tests/rules/importer/test_json2tables.py
+++ tests/rules/importer/test_json2tables.py
@@ -5,21 +5,21 @@ from tests import data
 def test_json2rules_power_grid() -> None:
     # Arrange
     json_importer = importer.JSONImporter(data.POWER_GRID_JSON)
-
-    # Act
-    rules = json_importer.to_rules()
-
-    # Assert
-    assert not (
-        missing := set(rules.classes) - {"GeographicalRegion", "SubGraphicalRegion", "Terminal", "Substation"}
-    ), f"JSONImporter did not find classes {missing}"
-    properties_by_class = rules.properties.groupby("class_id")
     expected_properties = {
         "GeographicalRegion": {"name"},
         "SubGraphicalRegion": {"name", "parent"},
         "Substation": {"name", "parent"},
         "Terminal": {"name", "aliasName", "parent"},
     }
+
+    # Act
+    rules = json_importer.to_rules()
+
+    # Assert
+    assert not (
+        missing := set(rules.classes) - set(expected_properties)
+    ), f"JSONImporter did not find classes {missing}"
+    properties_by_class = rules.properties.groupby("class_id")
     for class_name, expected in expected_properties.items():
         assert not (
             missing := expected - {prop.property_name for prop in properties_by_class.get(class_name, []).values()}
