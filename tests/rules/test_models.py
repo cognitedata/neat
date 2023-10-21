41419cc792ed3545eb735425fea09cec5f153cc1
anders-albert <anders0albert@gmail.com>
2023-10-21 16:39:20 +0200

    tests: Added failing tests

---
+++ tests/rules/test_models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from rdflib import Namespace
+
+from cognite.neat.rules.models import Classes, Metadata, Properties, TransformationRules
+
+
+def tests_create_empty_rules():
+    metadata = Metadata(
+        title="Dummy Title",
+        description="A description",
+        version="0_1",
+        creator="Cognite",
+        created=datetime.utcnow(),
+        namespace=Namespace("http://purl.org/cognite/neat#"),
+        prefix="neat",
+        cdf_space_name="MySpace",
+        data_model_name="MyDataModel",
+    )
+    classes = Classes()
+    properties = Properties()
+
+    # Act
+    rules = TransformationRules(metadata=metadata, classes=classes, properties=properties, prefixes={}, instances=[])
+
+    assert rules.metadata.title == "Dummy Title"
