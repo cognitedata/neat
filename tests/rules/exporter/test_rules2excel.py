6a1ad885944dedc099d0039bb14926f3d3cae761
nikokaoja <nikola.vasiljevic@cognite.com>
2023-10-22 23:51:40 +0200

    fixing failing tests

--- tests/rules/exporter/test_rules2excel.py
+++ tests/rules/exporter/test_rules2excel.py
@@ -4,6 +4,6 @@ from cognite.neat.rules.exporter import ExcelExporter
 def test_rules2excel(simple_rules):
     exporter = ExcelExporter(rules=simple_rules, filepath=None)
 
-    assert exporter.data.sheetnames == ["Metadata", "Classes", "Properties"]
+    assert exporter.data.sheetnames == ["Metadata", "Classes", "Properties", "Prefixes"]
     assert exporter.class_counter == len(simple_rules.classes)
     assert exporter.property_counter == len(simple_rules.properties)
