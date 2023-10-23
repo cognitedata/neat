4b35ebee41cfd7f67e6d198cc96ef60a7317ed01
nikokaoja <sidarta83@gmail.com>
2023-10-23 21:06:52 +0200

    update

--- cognite/neat/rules/exporter/_base.py
+++ cognite/neat/rules/exporter/_base.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import cast
-from cognite.neat.rules.models.raw_rules import RawRules
 
+from cognite.neat.rules.models.raw_rules import RawRules
 from cognite.neat.rules.models.rules import Rules
 
 
