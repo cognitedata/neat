1935cad9c42437095c0925958626272d03d3212c
nikokaoja <sidarta83@gmail.com>
2023-10-23 22:00:05 +0200

    updated tests

--- tests/rules/exporter/test_rules2ontology.py
+++ tests/rules/exporter/test_rules2ontology.py
@@ -1,11 +1,12 @@
+import tempfile
 from copy import deepcopy
 from pathlib import Path
-import tempfile
+
 import pytest
+from rdflib import Graph
 
 from cognite.neat.rules.exceptions import PropertiesDefinedMultipleTimes
 from cognite.neat.rules.exporter import OWLExporter, SemanticDataModelExporter, SHACLExporter
-from rdflib import Graph
 
 
 def test_rules2owl(transformation_rules):
@@ -16,13 +17,12 @@ def test_rules2owl(transformation_rules):
     graph.bind("owl", "http://www.w3.org/2002/07/owl#")
     graph.bind("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
-
     # we have 7 classes in the ontology
     assert len(graph.query("SELECT ?s WHERE {?s rdf:type owl:Class Filter (!isBlank(?s))}")) == 7
 
 
 def test_rules2shacl(transformation_rules):
-    file = tempfile.NamedTemporaryFile(suffix=".ttl")    
+    file = tempfile.NamedTemporaryFile(suffix=".ttl")
     SHACLExporter(rules=transformation_rules, filepath=file.name).export()
     graph = Graph().parse(Path(file.name))
     graph.bind("owl", "http://www.w3.org/2002/07/owl#")
@@ -39,10 +39,7 @@ def test_rules2semantic_model(transformation_rules):
     graph.bind("owl", "http://www.w3.org/2002/07/owl#")
     graph.bind("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
-
-    assert (
-        len(graph.query("SELECT ?s WHERE {?s rdf:type sh:NodeShape Filter (!isBlank(?s))}")) == 6
-    )
+    assert len(graph.query("SELECT ?s WHERE {?s rdf:type sh:NodeShape Filter (!isBlank(?s))}")) == 6
     assert len(graph.query("SELECT ?s WHERE {?s rdf:type owl:Class Filter (!isBlank(?s))}")) == 7
 
 
