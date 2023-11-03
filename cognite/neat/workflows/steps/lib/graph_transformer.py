82b0b3e04098ab39fe7309a66de0565c2d739cba
alivinco <aleksandrs.livincovs@gmail.com>
2023-11-03 17:17:34 +0100

    fix and docs update

--- cognite/neat/workflows/steps/lib/graph_transformer.py
+++ cognite/neat/workflows/steps/lib/graph_transformer.py
@@ -1,10 +1,11 @@
+from typing import ClassVar
 from cognite.client import CogniteClient
 
 from cognite.neat.graph.transformations.transformer import RuleProcessingReport, domain2app_knowledge_graph
 from cognite.neat.rules.exporter.rules2triples import get_instances_as_triples
 from cognite.neat.workflows.model import FlowMessage
 from cognite.neat.workflows.steps.data_contracts import RulesData, SolutionGraph, SourceGraph
-from cognite.neat.workflows.steps.step_model import Step
+from cognite.neat.workflows.steps.step_model import Configurable, Step
 
 __all__ = ["TransformSourceToSolutionGraph"]
 
@@ -18,6 +19,14 @@ class TransformSourceToSolutionGraph(Step):
 
     description = "The step transforms source graph to solution graph"
     category = CATEGORY
+    configurables: ClassVar[list[Configurable]] = [
+        Configurable(
+            name="cdf_lookup_database",
+            value="",
+            label="Name of the CDF raw database to use for data lookup (rawlookup rules).\
+            Applicable only for transformations with rawlookup rules.",
+        ),
+    ]
 
     def run(  # type: ignore[override, syntax]
         self,
@@ -28,6 +37,7 @@ class TransformSourceToSolutionGraph(Step):
     ) -> FlowMessage:
         report = RuleProcessingReport()
         # run transformation and generate new graph
+        cdf_lookup_database = self.configs.get("cdf_lookup_database", "")
         solution_graph.graph.set_graph(
             domain2app_knowledge_graph(
                 source_graph.graph.get_graph(),
@@ -35,7 +45,7 @@ class TransformSourceToSolutionGraph(Step):
                 app_instance_graph=solution_graph.graph.get_graph(),
                 extra_triples=get_instances_as_triples(transformation_rules.rules),
                 client=cdf_client,
-                cdf_lookup_database=None,  # change this accordingly!
+                cdf_lookup_database=cdf_lookup_database,  # change this accordingly!
                 processing_report=report,
             )
         )
