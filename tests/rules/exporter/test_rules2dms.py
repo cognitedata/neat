af76a8a7e87f50a66e7ae93aca3d5752609335c1
nikokaoja <nikola.vasiljevic@cognite.com>
2024-01-02 14:52:46 +0100

    added multi space tests

--- tests/rules/exporter/test_rules2dms.py
+++ tests/rules/exporter/test_rules2dms.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
 import pytest
+from rdflib import Namespace
 
 from cognite.neat.rules.exceptions import EntitiesContainNonDMSCompliantCharacters
 from cognite.neat.rules.exporter._rules2dms import DataModel
+from cognite.neat.rules.models._base import ParentClass
+from cognite.neat.rules.models.rules import Class, Metadata, Property, Rules
 
 
-def test_rules2dms(simple_rules):
+def test_rules2dms_single_space(simple_rules):
     data_model = DataModel.from_rules(rules=simple_rules)
 
     assert len(data_model.containers) == 4
@@ -24,6 +29,49 @@ def test_rules2dms(simple_rules):
     assert data_model.version == "0.1"
     assert data_model.space == "neat"
     assert data_model.external_id == "playground_model"
+
+
+def test_rules2dms_multi_space():
+    metadata = Metadata(
+        name="Dummy Data Model",
+        description="A description",
+        version="0.1",
+        creator="Cognite",
+        created=datetime.utcnow(),
+        namespace=Namespace("http://purl.org/cognite/neat#"),
+        prefix="neat",
+    )
+    classes = {
+        "DummyClass": Class(class_id="DummyClass", description="A description", parent_class="mega:DummyClass"),
+        "EmptyClass": Class(class_id="EmptyClass"),
+    }
+    properties = {
+        "dummyProperty": Property(
+            class_id="DummyClass2",
+            property_id="dummyProperty",
+            expected_value_type="string",
+            max_count=1,
+            container="outerSpace:DummyClass",
+        )
+    }
+
+    # Act
+    rules = Rules(metadata=metadata, classes=classes, properties=properties, prefixes={}, instances=[])
+
+    data_model = DataModel.from_rules(rules=rules)
+    assert len(data_model.containers) == 1
+    assert len(data_model.views) == 2
+    assert list(data_model.views.keys()) == [
+        "neat:DummyClass",
+        "neat:DummyClass2",
+    ]
+    assert list(data_model.containers.keys()) == [
+        "outerSpace:DummyClass",
+    ]
+
+    assert data_model.views["neat:DummyClass"].implements == [
+        ParentClass.from_string(entity_string="mega:DummyClass(version=0.1)").view_id
+    ]
 
 
 def test_raise_error10(transformation_rules):
