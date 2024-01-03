815600c78dbb695e328bf9ccbd1971fb788a3538
anders-albert <anders0albert@gmail.com>
2024-01-03 16:00:37 +0100

    refactor: cleanup namespace

--- cognite/neat/graph/stores/__init__.py
+++ cognite/neat/graph/stores/__init__.py
@@ -3,9 +3,11 @@ from ._graphdb_store import GraphDBStore
 from ._memory_store import MemoryStore
 from ._oxigraph_store import OxiGraphStore
 
-STORE_BY_TYPE: dict[str, type[NeatGraphStoreBase]] = {
-    store.rdf_store_type: store for store in NeatGraphStoreBase.__subclasses__()  # type: ignore[type-abstract]
-}
+STORE_BY_TYPE: dict[str, type[NeatGraphStoreBase]] = {}
+for store in NeatGraphStoreBase.__subclasses__():
+    STORE_BY_TYPE[store.rdf_store_type] = store  # type: ignore[type-abstract]
+
+del store  # Cleanup namespace
 STORE_BY_TYPE["sparql"] = MemoryStore
 AVAILABLE_STORES = set(STORE_BY_TYPE.keys())
 
