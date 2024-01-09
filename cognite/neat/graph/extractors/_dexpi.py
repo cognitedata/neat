79c42a832a08e2f7ec5f6bec90a951608166412e
nikokaoja <nikola.vasiljevic@cognite.com>
2024-01-09 11:05:07 +0100

    refactor dexpi extractor

--- cognite/neat/graph/extractors/_dexpi.py
+++ cognite/neat/graph/extractors/_dexpi.py
@@ -1,6 +1,5 @@
 import re
 import xml.etree.ElementTree as ET
-from collections.abc import Generator
 from pathlib import Path
 from xml.etree.ElementTree import Element
 
@@ -9,6 +8,7 @@ from rdflib import OWL, RDF, RDFS, SKOS, XSD, Literal, Namespace, URIRef
 from cognite.neat.graph.models import Triple
 from cognite.neat.rules.models._base import ALLOWED_PATTERN
 from cognite.neat.utils.utils import get_namespace, remove_namespace
+from cognite.neat.utils.xml import get_children, iterate_tree
 
 from ._base import BaseExtractor
 
@@ -52,47 +52,14 @@ class DexpiXML(BaseExtractor):
 
         root = ET.parse(self.filepath).getroot()
 
-        nodes, edges = extract_nodes_and_edges(root)
+        nodes, edges = _extract_nodes_and_edges(root)
 
-        return to_triples(nodes, edges, base_namespace=self.namespace)
+        self.nodes = nodes
 
-
-def iterate_tree(node: Element) -> Generator:
-    """Iterate over all elements in an XML tree.
-
-    Args:
-        node: XML tree to iterate over.
-
-    Returns:
-        Generator of XML elements.
-    """
-    yield node
-    for child in node:
-        yield from iterate_tree(child)
+        return _to_triples(nodes, edges, base_namespace=self.namespace)
 
 
-def get_children(element: Element, child_tag: str, no_children: int = -1) -> list[Element]:
-    """Get children of an XML element.
-
-    Args:
-        element: XML element to get children from.
-        child_tag: Tag of the children to get.
-        no_children: Max number of children to get. Defaults to -1 (all).
-
-    Returns:
-        List of XML elements if no_children > 1, otherwise XML element.
-    """
-    children = []
-    for child in element:
-        if child.tag == child_tag:
-            if no_children == 1:
-                return [child]
-            else:
-                children.append(child)
-    return children
-
-
-def extract_nodes_and_edges(root: Element) -> tuple[dict, dict]:
+def _extract_nodes_and_edges(root: Element) -> tuple[dict, dict]:
     """Extract nodes and edges from an XML tree.
 
     Args:
@@ -110,63 +77,94 @@ def extract_nodes_and_edges(root: Element) -> tuple[dict, dict]:
             and element.attrib["ComponentClass"] != "Label"
             and "ID" in element.attrib
         ):
+            id_ = element.attrib["ID"]
+
             # add header that contains things such as type
-            nodes[element.attrib["ID"]] = {"header": element.attrib}
-            nodes[element.attrib["ID"]]["header"]["tag"] = str(element.tag)
+            nodes[id_] = {"header": element.attrib, "attributes": {}}
+            nodes[id_]["header"]["tag"] = str(element.tag)
 
-            # add generic attributes
-            if gen_atribute := get_children(element, "GenericAttributes", 1):
-                if grandchildren := get_children(gen_atribute[0], "GenericAttribute"):
-                    for grandchild in grandchildren:
-                        if grandchild.attrib["AttributeURI"] not in nodes[element.attrib["ID"]]:
-                            nodes[element.attrib["ID"]][grandchild.attrib["AttributeURI"]] = [grandchild.attrib]
-                        else:
-                            nodes[element.attrib["ID"]][grandchild.attrib["AttributeURI"]].append(grandchild.attrib)
+            # add human readable label of node (if exists)
+            nodes = _add_node_label(id_, nodes, element)
 
-            # add label which is in Label part of the xml
-            if label_element := get_children(element, "Label", 1):
-                if text_element := get_children(label_element[0], "Text", 1):
-                    if "String" in text_element[0].attrib:
-                        nodes[element.attrib["ID"]]["header"]["label"] = text_element[0].attrib["String"]
+            # add generic attributes of node
+            nodes = _add_node_generic_attributes(id_, nodes, element)
 
             # edges scenario: <Connection> tag
-            if connections := get_children(element, "Connection"):
-                for connection in connections:
-                    if "FromID" in connection.attrib and "ToID" in connection.attrib:
-                        edges["connections"].add((connection.attrib["FromID"], "connection", connection.attrib["ToID"]))
+            edges = _add_connection_edge(edges, element)
 
             # edges scenario: <Association> tag
-            if associations := get_children(element, "Association"):
-                for association in associations:
-                    if "Type" in association.attrib and "ItemID" in association.attrib:
-                        association_type = "".join(
-                            [
-                                word.capitalize() if i != 0 else word
-                                for i, word in enumerate(association.attrib["Type"].split(" "))
-                            ]
-                        )
-                        edges["associations"].add(
-                            (element.attrib["ID"], f"{association_type}", association.attrib["ItemID"])
-                        )
+            edges = _add_association_edge(id_, edges, element)
 
             # edge scenario: children elements of the element
-            for child in element:
-                if "ID" in child.attrib and child.tag != "Label":
-                    edges["children"].add((element.attrib["ID"], child.tag, child.attrib["ID"]))
+            edges = _add_child_edge(id_, edges, element)
+
     return nodes, edges
 
 
-def get_datatype_triples(base_namespace: Namespace, unit_definition: dict):
-    return {
-        (base_namespace[unit_definition["Units"]], RDF.type, RDFS.Datatype),
-        (base_namespace[unit_definition["Units"]], RDFS.label, Literal(unit_definition["Units"])),
-        (base_namespace[unit_definition["Units"]], OWL.equivalentClass, XSD[unit_definition["Format"]]),
-        (base_namespace[unit_definition["Units"]], SKOS.exactMatch, URIRef(unit_definition["UnitsURI"])),
-    }
+def _add_node_label(id_: str, nodes: dict, element: Element) -> dict:
+    if children := get_children(element, "Label", 1):
+        if grandchildren := get_children(children[0], "Text", 1):
+            if "String" in grandchildren[0].attrib:
+                nodes[id_]["header"]["label"] = grandchildren[0].attrib["String"]
+
+    return nodes
 
 
-def to_triples(nodes: dict, edges: dict, base_namespace: Namespace) -> set[Triple]:
-    """Convert nodes and edges to triples.
+def _add_node_generic_attributes(id_: str, nodes: dict, element: Element) -> dict:
+    if children := get_children(element, "GenericAttributes", 1):
+        if grandchildren := get_children(children[0], "GenericAttribute"):
+            for generic_attribute in grandchildren:
+                if generic_attribute.attrib["AttributeURI"] not in nodes[id_]:
+                    nodes[id_]["attributes"][generic_attribute.attrib["AttributeURI"]] = [generic_attribute.attrib]
+                else:
+                    nodes[id_]["attributes"][generic_attribute.attrib["AttributeURI"]].append(generic_attribute.attrib)
+
+    return nodes
+
+
+def _add_connection_edge(edges: dict, element: Element) -> dict:
+    if "connections" not in edges:
+        edges["connections"] = set()
+
+    if connections := get_children(element, "Connection"):
+        for connection in connections:
+            if "FromID" in connection.attrib and "ToID" in connection.attrib:
+                edges["connections"].add((connection.attrib["FromID"], "connection", connection.attrib["ToID"]))
+
+    return edges
+
+
+def _add_association_edge(id_: str, edges: dict, element: Element) -> dict:
+    if "associations" not in edges:
+        edges["associations"] = set()
+
+    if associations := get_children(element, "Association"):
+        for association in associations:
+            if "Type" in association.attrib and "ItemID" in association.attrib:
+                association_type = "".join(
+                    [
+                        word.capitalize() if i != 0 else word
+                        for i, word in enumerate(association.attrib["Type"].split(" "))
+                    ]
+                )
+                edges["associations"].add((id_, f"{association_type}", association.attrib["ItemID"]))
+
+    return edges
+
+
+def _add_child_edge(id_: str, edges: dict, element: Element) -> dict:
+    if "children" not in edges:
+        edges["children"] = set()
+
+    for child in element:
+        if "ID" in child.attrib and child.tag != "Label":
+            edges["children"].add((id_, child.tag, child.attrib["ID"]))
+
+    return edges
+
+
+def _to_triples(nodes: dict, edges: dict, base_namespace: Namespace) -> set[Triple]:
+    """Convert nodes and edges to subject-predicate-object triples.
 
     Args:
         nodes: Nodes to convert to triples.
@@ -178,103 +176,134 @@ def to_triples(nodes: dict, edges: dict, base_namespace: Namespace) -> set[Tripl
     triples: set[Triple] = set()
 
     # Adding nodes and nodes attributes
-    for node_id, node_attributes in nodes.items():
-        node_uri = URIRef(base_namespace + node_id)
+    for id_, node in nodes.items():
+        uri = URIRef(base_namespace + id_)
 
-        triples.add((node_uri, RDF.type, URIRef(node_attributes["header"]["ComponentClassURI"])))
-        triples.add((node_uri, _DEXPI_PREFIXES["dexpi"].tag, Literal(node_attributes["header"]["tag"])))
+        # add header attribute triples
+        triples = _add_header_triples(uri, triples, node["header"], "ComponentClassURI", RDF.type)
+        triples = _add_header_triples(uri, triples, node["header"], "label", RDFS.label)
+        triples = _add_header_triples(uri, triples, node["header"], "tag")
+        triples = _add_header_triples(uri, triples, node["header"], "ComponentClass")
+        triples = _add_header_triples(uri, triples, node["header"], "ComponentName")
 
-        if "ComponentClass" in node_attributes["header"]:
-            triples.add(
-                (
-                    node_uri,
-                    _DEXPI_PREFIXES["dexpi"].ComponentClass,
-                    Literal(node_attributes["header"]["ComponentClass"]),
-                )
-            )
+        # add generic attribute triples
+        for attribute, values in node["attributes"].items():
+            attribute_uri, triples = _to_compliant_attribute_uri(URIRef(attribute), triples)
 
-        if "ComponentName" in node_attributes["header"]:
-            triples.add(
-                (node_uri, _DEXPI_PREFIXES["dexpi"].ComponentName, Literal(node_attributes["header"]["ComponentName"]))
-            )
+            for value in values:
+                triples = _add_attribute_triples(uri, triples, attribute_uri, value)
 
-        if "label" in node_attributes["header"]:
-            triples.add((node_uri, RDFS.label, Literal(node_attributes["header"]["label"])))
-
-        for attribute, values in node_attributes.items():
-            if attribute != "header":
-                # attribute contains illegal characters
-
-                namespace = get_namespace(attribute)
-                attribute_name = remove_namespace(attribute)
-                compliant_attribute_name = re.sub(ALLOWED_PATTERN, "", attribute_name)
-
-                if attribute_name != compliant_attribute_name:
-                    attribute = f"{namespace}{compliant_attribute_name}"
-                    triples.add(
-                        (
-                            URIRef(attribute),
-                            SKOS.exactMatch,
-                            URIRef(f"{namespace}{attribute_name}"),
-                        )
-                    )
-                    triples.add(
-                        (
-                            URIRef(attribute),
-                            RDFS.comment,
-                            Literal("Modified property URI to be compliant with NEAT internal representation"),
-                        )
-                    )
-
-                for value in values:
-                    if "Value" not in value or "Format" not in value:
-                        continue
-                    if "Units" in value:
-                        triples.update(get_datatype_triples(_DEXPI_PREFIXES["dexpi"], value))
-                        triples.add(
-                            (
-                                node_uri,
-                                URIRef(attribute),
-                                Literal(value["Value"], datatype=_DEXPI_PREFIXES["dexpi"][value["Units"]]),
-                            )
-                        )
-
-                    elif "Language" in value and "Value" in value:
-                        triples.add((node_uri, URIRef(attribute), Literal(value["Value"], lang=value["Language"])))
-                    elif "ValueURI" in value:
-                        triples.add(
-                            (node_uri, URIRef(attribute), Literal(value["ValueURI"], datatype=XSD[value["Format"]]))
-                        )
-                    elif value["Format"].lower() != "string":
-                        triples.add(
-                            (node_uri, URIRef(attribute), Literal(value["Value"], datatype=XSD[value["Format"]]))
-                        )
-                    else:
-                        triples.add((node_uri, URIRef(attribute), Literal(value["Value"])))
-
-    # Adding Edges: type connections
-    for subject, predicate, object in edges["connections"]:
-        triples.add(
-            (URIRef(base_namespace + subject), _DEXPI_PREFIXES["dexpi"][predicate], URIRef(base_namespace + object))
-        )
-
-    # Adding Edges: type associations
-    for subject, predicate, object in edges["associations"]:
-        triples.add(
-            (
-                URIRef(base_namespace + subject),
-                _DEXPI_PREFIXES["dexpi"][f"association/{predicate}"],
-                URIRef(base_namespace + object),
-            )
-        )
-    # Adding Edges: type children
-    for subject, predicate, object in edges["children"]:
-        triples.add(
-            (
-                URIRef(base_namespace + subject),
-                _DEXPI_PREFIXES["dexpi"][f"children/has{predicate}"],
-                URIRef(base_namespace + object),
-            )
-        )
+    # Adding Edges
+    triples = _add_edge_triples(edges["connections"], triples, "connections", base_namespace)
+    triples = _add_edge_triples(edges["associations"], triples, "associations", base_namespace)
+    triples = _add_edge_triples(edges["children"], triples, "children", base_namespace)
 
     return triples
+
+
+def _add_edge_triples(
+    edges: set[tuple[str, str, str]], triples: set[Triple], edge_type: str, base_namespace: Namespace
+) -> set[Triple]:
+    """Convert edges to subject-predicate-object triples.
+
+    Args:
+        edges: Edges to convert to triples.
+
+    Returns:
+        List of triples.
+    """
+
+    for subject, predicate, object in edges:
+        if edge_type != "connections":
+            predicate_uri = _DEXPI_PREFIXES["dexpi"][f"{edge_type}/{predicate}"]
+        else:
+            predicate_uri = _DEXPI_PREFIXES["dexpi"][predicate]
+
+        triples.add((URIRef(base_namespace + subject), predicate_uri, URIRef(base_namespace + object)))
+
+    return triples
+
+
+def _add_header_triples(
+    uri: URIRef, triples: set[Triple], attributes: dict, attribute: str, attribute_uri: None | URIRef = None
+) -> set[Triple]:
+    if attribute not in attributes:
+        return triples
+
+    attribute_uri = attribute_uri or _DEXPI_PREFIXES["dexpi"][attribute]
+
+    if attribute in attributes and attribute_uri == RDF.type:
+        triples.add((uri, attribute_uri, URIRef(attributes[attribute])))
+    else:
+        triples.add((uri, attribute_uri, Literal(attributes[attribute])))
+
+    return triples
+
+
+def _to_compliant_attribute_uri(attribute_uri: URIRef, triples: set[Triple]) -> tuple[URIRef, set[Triple]]:
+    namespace = get_namespace(attribute_uri)
+    id_ = remove_namespace(attribute_uri)
+    compliant_id = re.sub(ALLOWED_PATTERN, "", id_)
+    compliant_attribute_uri = URIRef(f"{namespace}{compliant_id}")
+
+    if attribute_uri != compliant_attribute_uri:
+        triples.add(
+            (
+                compliant_attribute_uri,
+                SKOS.exactMatch,
+                attribute_uri,
+            )
+        )
+        triples.add(
+            (
+                compliant_attribute_uri,
+                RDFS.comment,
+                Literal("Modified property URI to be compliant with NEAT internal representation"),
+            )
+        )
+
+    return compliant_attribute_uri, triples
+
+
+def _add_attribute_triples(uri: URIRef, triples: set[Triple], attribute_uri, value: dict) -> set[Triple]:
+    # case: when Value or Format is not present we skip the attribute since it is not formatted correctly
+    if "Value" not in value or "Format" not in value:
+        return triples
+
+    # case: when unit is present we create a special datatype for it
+    if "Units" in value:
+        triples.update(_generate_datatype_triples(_DEXPI_PREFIXES["dexpi"], value))
+        triples.add(
+            (
+                uri,
+                attribute_uri,
+                Literal(value["Value"], datatype=_DEXPI_PREFIXES["dexpi"][value["Units"]]),
+            )
+        )
+
+    # case: when language is present we create add language tag to the literal
+    elif "Language" in value and "Value" in value:
+        triples.add((uri, attribute_uri, Literal(value["Value"], lang=value["Language"])))
+
+    # case: when ValueURI is present we use it instead of Value
+    elif "ValueURI" in value:
+        triples.add((uri, attribute_uri, Literal(value["ValueURI"], datatype=XSD[value["Format"]])))
+
+    # case: when Format is not string we make sure to add the datatype
+    elif value["Format"].lower() != "string":
+        triples.add((uri, attribute_uri, Literal(value["Value"], datatype=XSD[value["Format"]])))
+
+    # case: when Format is string we add the literal without datatype (easier to read triples, less noise)
+    else:
+        triples.add((uri, attribute_uri, Literal(value["Value"])))
+
+    return triples
+
+
+def _generate_datatype_triples(base_namespace: Namespace, unit_definition: dict) -> set[Triple]:
+    return {
+        (base_namespace[unit_definition["Units"]], RDF.type, RDFS.Datatype),
+        (base_namespace[unit_definition["Units"]], RDFS.label, Literal(unit_definition["Units"])),
+        (base_namespace[unit_definition["Units"]], OWL.equivalentClass, XSD[unit_definition["Format"]]),
+        (base_namespace[unit_definition["Units"]], SKOS.exactMatch, URIRef(unit_definition["UnitsURI"])),
+    }
