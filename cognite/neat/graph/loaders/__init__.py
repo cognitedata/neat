88a75b8347e737ce9f54fe8ae6639cc21f69d95a
nikokaoja <nikola.vasiljevic@cognite.com>
2024-01-11 14:11:20 +0100

    final touches

--- cognite/neat/graph/loaders/__init__.py
+++ cognite/neat/graph/loaders/__init__.py
@@ -2,7 +2,7 @@ from cognite.neat.graph.loaders.core.labels import upload_labels
 
 from .core.rdf_to_assets import categorize_assets, rdf2assets, upload_assets
 from .core.rdf_to_relationships import categorize_relationships, rdf2relationships, upload_relationships
-from .rdf_to_dms import rdf2nodes_and_edges
+from .rdf_to_dms import rdf2nodes_and_edges, upload_edges, upload_nodes
 
 __all__ = [
     "rdf2relationships",
@@ -13,4 +13,6 @@ __all__ = [
     "categorize_relationships",
     "upload_relationships",
     "upload_labels",
+    "upload_nodes",
+    "upload_edges",
 ]
