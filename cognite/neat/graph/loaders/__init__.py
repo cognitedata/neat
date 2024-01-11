1d317c451c61d1ff479c0541b2d11a444199d283
nikokaoja <nikola.vasiljevic@cognite.com>
2024-01-11 13:09:51 +0100

    fix issue with optional edges

--- cognite/neat/graph/loaders/__init__.py
+++ cognite/neat/graph/loaders/__init__.py
@@ -2,12 +2,14 @@ from cognite.neat.graph.loaders.core.labels import upload_labels
 
 from .core.rdf_to_assets import categorize_assets, rdf2assets, upload_assets
 from .core.rdf_to_relationships import categorize_relationships, rdf2relationships, upload_relationships
+from .rdf_to_dms import rdf2nodes_and_edges
 
 __all__ = [
     "rdf2relationships",
     "categorize_assets",
     "upload_assets",
     "rdf2assets",
+    "rdf2nodes_and_edges",
     "categorize_relationships",
     "upload_relationships",
     "upload_labels",
