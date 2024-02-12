49b7209b2fd180d26692d89c32239d841ff0acaa
nikokaoja <nikola.vasiljevic@cognite.com>
2024-02-12 14:52:33 +0100

    commit of new classes

--- cognite/neat/rules/models/_rules/information_rules.py
+++ cognite/neat/rules/models/_rules/information_rules.py
@@ -1,19 +1,89 @@
 from datetime import datetime
+import re
+import warnings
+from pydantic import Field, TypeAdapter, field_validator, HttpUrl
 
 from rdflib import Namespace
 
-from .base import Prefix, RoleTypes, RuleModel
+from cognite.neat.rules import exceptions
+from .base import Prefix, RoleTypes, RuleModel, skip_field_validator
+from .base import version_compliance_regex, prefix_compliance_regex, more_than_one_none_alphanumerics_regex
 from .domain_rules import DomainMetadata
 
 
 class InformationArchitectMetadata(DomainMetadata):
+
     role: RoleTypes = RoleTypes.information_architect
-    prefix: Prefix
-    namespace: Namespace
-    version: str
-    contributor: str | list[str]
-    created: datetime
-    updated: datetime
+    prefix: Prefix = Field(
+        description="This is used as prefix for generation of RDF OWL/SHACL data model representation",
+    )
+    namespace: Namespace = Field(
+        description="This is used as RDF namespace for generation of RDF OWL/SHACL data model representation "
+        "and/or for generation of RDF graphs.",
+        min_length=1,
+        max_length=2048,
+    )
+
+    name: str = Field(
+        alias="title",
+        description="Human readable name of the data model",
+        min_length=1,
+        max_length=255,
+    )
+
+    version: str | None = Field(
+        description="Data model version",
+        min_length=1,
+        max_length=43,
+    )
+
+    contributor: str | list[str] = Field(
+        description=(
+            "List of contributors to the data model creation, "
+            "typically information architects are considered as contributors."
+        ),
+    )
+
+    created: datetime = Field(
+        description=("Date of the data model creation"),
+    )
+
+    updated: datetime = Field(
+        description=("Date of the data model update"),
+    )
+
+    @field_validator("version")
+    def is_version_compliant(cls, value):
+        if not re.match(version_compliance_regex, value):
+            raise exceptions.VersionRegexViolation(value, version_compliance_regex).to_pydantic_custom_error()
+        else:
+            return value
+
+    @field_validator("prefix")
+    def is_prefix_compliant(cls, value):
+
+        if not re.match(prefix_compliance_regex, value):
+            raise exceptions.PrefixesRegexViolation([value], prefix_compliance_regex).to_pydantic_custom_error()
+        else:
+            return value
+
+    @field_validator("creator", "contributor", mode="before")
+    def to_list_if_comma(cls, value, values):
+        if isinstance(value, str):
+            if value:
+                return value.replace(", ", ",").split(",")
+            if cls.model_fields[values.field_name].default is None:
+                return None
+        return value
+
+    @field_validator("namespace", mode="before")
+    def fix_namespace_ending(cls, value):
+        if value.endswith("#") or value.endswith("/"):
+            return Namespace(TypeAdapter(HttpUrl).validate_python(value))
+        warnings.warn(
+            exceptions.NamespaceEndingFixed(value).message, category=exceptions.NamespaceEndingFixed, stacklevel=2
+        )
+        return Namespace(TypeAdapter(HttpUrl).validate_python(f"{value}#"))
 
     @classmethod
     def from_domain_expert_metadata(
@@ -25,6 +95,7 @@ class InformationArchitectMetadata(DomainMetadata):
         contributor: str | list[str] | None = None,
         created: datetime | None = None,
         updated: datetime | None = None,
+        name: str | None = None,
     ):
         metadata_as_dict = metadata.model_dump()
         metadata_as_dict["prefix"] = prefix or metadata.prefix or "neat"
@@ -33,6 +104,7 @@ class InformationArchitectMetadata(DomainMetadata):
         metadata_as_dict["contributor"] = contributor or metadata.contributor or "Cognite"
         metadata_as_dict["created"] = created or metadata.created or datetime.utcnow()
         metadata_as_dict["updated"] = updated or metadata.updated or datetime.utcnow()
+        metadata_as_dict["name"] = name or metadata.name or "NEAT Data Model"
         return cls(**metadata_as_dict)
 
 
