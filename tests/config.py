17b170944c4faae2ca5308b843e6dd2fa6c7be74
nikokaoja <nikola.vasiljevic@cognite.com>
2024-02-28 10:34:30 +0100

    fix tests

--- tests/config.py
+++ tests/config.py
@@ -5,8 +5,7 @@ ROOT = TEST_FOLDER.parent
 PACKAGE_DIRECTORY = ROOT / "cognite" / "neat"
 
 DOC_FOLDER = ROOT / "docs"
-DOC_TUTORIAL = DOC_FOLDER / "tutorial"
-DOC_KNOWLEDGE_ACQUISITION_TUTORIAL = DOC_TUTORIAL / "use-cases" / "spreadsheets"
+DOC_KNOWLEDGE_ACQUISITION_TUTORIAL = DOC_FOLDER / "data-modeling-lifecycle" / "spreadsheets"
 DATA_FOLDER = TEST_FOLDER / "data"
 PYPROJECT_TOML = ROOT / "pyproject.toml"
 
