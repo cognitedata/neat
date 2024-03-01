9b97f22fd551edef64bdab613be04ec3e9dc4dde
nikokaoja <nikola.vasiljevic@cognite.com>
2024-03-01 17:19:25 +0100

    final msg

--- tests/tests_unit/rules/conftest.py
+++ tests/tests_unit/rules/conftest.py
@@ -3,11 +3,9 @@ from typing import Any
 import pandas as pd
 import pytest
 
-from cognite.neat.rules.models._rules.dms_architect_rules import (
-    DMSRules,
-)
+from cognite.neat.rules.models._rules import DMSRules, InformationRules
+from cognite.neat.utils.spreadsheet import read_spreadsheet
 from tests.config import DOC_KNOWLEDGE_ACQUISITION_TUTORIAL
-from tests.tests_unit.rules.test_models.utils import read_spreadsheet
 
 
 @pytest.fixture(scope="session")
@@ -16,12 +14,28 @@ def alice_spreadsheet() -> dict[str, dict[str, Any]]:
     excel_file = pd.ExcelFile(filepath)
     return {
         "Metadata": dict(pd.read_excel(excel_file, "Metadata", header=None).values),
-        "Properties": read_spreadsheet(excel_file, "Properties", skiprows=1),
-        "Views": read_spreadsheet(excel_file, "Views", skiprows=1),
-        "Containers": read_spreadsheet(excel_file, "Containers", skiprows=1),
+        "Properties": read_spreadsheet(excel_file, "Properties", ["Property"]),
+        "Views": read_spreadsheet(excel_file, "Views", ["View"]),
+        "Containers": read_spreadsheet(excel_file, "Containers", ["Container"]),
     }
 
 
 @pytest.fixture(scope="session")
 def alice_rules(alice_spreadsheet: dict[str, dict[str, Any]]) -> DMSRules:
     return DMSRules.model_validate(alice_spreadsheet)
+
+
+@pytest.fixture(scope="session")
+def david_spreadsheet() -> dict[str, dict[str, Any]]:
+    filepath = DOC_KNOWLEDGE_ACQUISITION_TUTORIAL / "information-architect-david.xlsx"
+    excel_file = pd.ExcelFile(filepath)
+    return {
+        "Metadata": dict(pd.read_excel(excel_file, "Metadata", header=None).values),
+        "Properties": read_spreadsheet(excel_file, "Properties", ["Property"]),
+        "Classes": read_spreadsheet(excel_file, "Classes", ["Class"]),
+    }
+
+
+@pytest.fixture(scope="session")
+def david_rules(david_spreadsheet: dict[str, dict[str, Any]]) -> InformationRules:
+    return InformationRules.model_validate(david_spreadsheet)
