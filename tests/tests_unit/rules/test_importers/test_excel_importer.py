a5f515a05bde1f5099407b04d1296a91653b1916
anders-albert <anders0albert@gmail.com>
2024-03-03 08:06:53 +0100

    tests; generalized tests

--- tests/tests_unit/rules/test_importers/test_excel_importer.py
+++ tests/tests_unit/rules/test_importers/test_excel_importer.py
@@ -5,6 +5,7 @@ from pydantic.version import VERSION
 
 from cognite.neat.rules.importers import ExcelImporter
 from cognite.neat.rules.importers import _models as issue_cls
+from cognite.neat.rules.importers._models import IssueList
 from tests.config import DOC_KNOWLEDGE_ACQUISITION_TUTORIAL
 from tests.tests_unit.rules.test_importers.constants import DATA_DIR
 
@@ -16,20 +17,24 @@ def valid_rules_filepaths():
 def invalid_rules_filepaths():
     yield pytest.param(
         DOC_KNOWLEDGE_ACQUISITION_TUTORIAL / "not-existing.xlsx",
-        issue_cls.SpreadsheetNotFound("not-existing.xlsx"),
+        IssueList([issue_cls.SpreadsheetNotFound("not-existing.xlsx")]),
         id="Not existing file",
     )
     major, minor, *_ = VERSION.split(".")
 
     yield pytest.param(
         DATA_DIR / "invalid_dms_rules.xlsx",
-        issue_cls.InvalidPropertySpecification(
-            column="IsList",
-            row=4,
-            type="bool_parsing",
-            msg="Input should be a valid boolean, unable to interpret input",
-            input="Apple",
-            url=f"https://errors.pydantic.dev/{major}.{minor}/v/bool_parsing",
+        IssueList(
+            [
+                issue_cls.InvalidPropertySpecification(
+                    column="IsList",
+                    row=4,
+                    type="bool_parsing",
+                    msg="Input should be a valid boolean, unable to interpret input",
+                    input="Apple",
+                    url=f"https://errors.pydantic.dev/{major}.{minor}/v/bool_parsing",
+                )
+            ]
         ),
         id="Invalid property in Metadata sheet",
     )
@@ -44,11 +49,11 @@ class TestExcelImporter:
 
         assert rules
 
-    @pytest.mark.parametrize("filepath, expected_issue", invalid_rules_filepaths())
-    def test_import_invalid_rules_single_issue(self, filepath: Path, expected_issue: issue_cls.Error):
+    @pytest.mark.parametrize("filepath, expected_issues", invalid_rules_filepaths())
+    def test_import_invalid_rules_single_issue(self, filepath: Path, expected_issues: IssueList):
         importer = ExcelImporter(filepath)
 
         _, issues = importer.to_rules(errors="continue")
 
-        assert len(issues) == 1
-        assert issues[0] == expected_issue
+        assert len(issues) == len(expected_issues)
+        assert sorted(issues) == sorted(expected_issues)
