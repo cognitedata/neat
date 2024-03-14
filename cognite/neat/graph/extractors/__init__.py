1a5962af4b2728ca09d7e32b4b6deccf46a6b77d
nikokaoja <nikola.vasiljevic@cognite.com>
2024-03-14 11:20:11 +0100

    remove unecessary moduel

--- cognite/neat/graph/extractors/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._base import BaseExtractor
-from ._dexpi import DexpiXML
-from ._graph_capturing_sheet import GraphCapturingSheet
-from ._mock_graph_generator import MockGraphGenerator
-
-__all__ = ["BaseExtractor", "MockGraphGenerator", "GraphCapturingSheet", "DexpiXML"]
