8222d760e4ab245a6b14aab5d8032317edb8a108
anders-albert <anders0albert@gmail.com>
2024-03-18 19:00:30 +0100

    tests: added failing test

--- cognite/neat/rules/exporters/__init__.py
+++ cognite/neat/rules/exporters/__init__.py
@@ -1,6 +1,7 @@
 from ._rules2dms import CDFExporter, DMSExporter
 from ._rules2excel import ExcelExporter
 from ._rules2ontology import GraphExporter, OWLExporter, SemanticDataModelExporter, SHACLExporter
+from ._rules2yaml import YAMLExporter
 
 __all__ = [
     "DMSExporter",
@@ -10,4 +11,5 @@ __all__ = [
     "GraphExporter",
     "SHACLExporter",
     "ExcelExporter",
+    "YAMLExporter",
 ]
---
+++ tests/tests_unit/rules/test_exporters/test_rules2yaml.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from cognite.neat.rules.exporters import YAMLExporter
+from cognite.neat.rules.importers import YAMLImporter
+from cognite.neat.rules.models._rules import DMSRules
+
+
+class TestYAMLExporter:
+    def test_export_import_rules(self, alice_rules: DMSRules, tmp_path: Path) -> None:
+        exporter = YAMLExporter()
+        exporter.export_to_file(tmp_path, alice_rules)
+        importer = YAMLImporter.from_directory(tmp_path)
+
+        recreated_rules = importer.to_rules("raise")
+
+        assert alice_rules == recreated_rules
