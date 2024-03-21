5210318ec020b15544dd2dbd921cda40a400e946
anders-albert <anders0albert@gmail.com>
2024-03-21 18:42:41 +0100

    tests: added failing test

--- tests/tests_unit/rules/test_exporters/test_rules2yaml.py
+++ tests/tests_unit/rules/test_exporters/test_rules2yaml.py
@@ -14,3 +14,12 @@ class TestYAMLExporter:
         recreated_rules = importer.to_rules("raise")
 
         assert alice_rules == recreated_rules
+
+    def test_export_import_information_rules(self, david_rules: DMSRules, tmp_path: Path) -> None:
+        exporter = YAMLExporter(files="single", output="yaml")
+        exporter.export_to_file(tmp_path / "tmp.yaml", david_rules)
+        importer = YAMLImporter.from_file(tmp_path / "tmp.yaml")
+
+        recreated_rules = importer.to_rules("raise")
+
+        assert david_rules == recreated_rules
