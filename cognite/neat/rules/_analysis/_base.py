1188e1cfefcd4fbb7f6976c1f8c102914cbecfa6
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2024-04-04 13:15:11 +0200

    Update cognite/neat/rules/_analysis/_base.py
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- cognite/neat/rules/_analysis/_base.py
+++ cognite/neat/rules/_analysis/_base.py
@@ -4,7 +4,11 @@ from cognite.neat.rules._shared import Rules
 from cognite.neat.rules.models._rules._types import ClassEntity
 
 
-class BaseAnalysis(ABC):
+from typing import TypeVar
+
+T_Rules = TypeVar("T_Rules", bound=Rules)
+
+class BaseAnalysis(ABC, Generic[T_Rules]):
     @abstractmethod
-    def subset_rules(self, desired_classes: set[ClassEntity]) -> Rules:
+    def subset_rules(self, desired_classes: set[ClassEntity]) -> T_Rules:
         raise NotImplementedError()
