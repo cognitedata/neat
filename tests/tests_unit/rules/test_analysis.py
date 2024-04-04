754e0f38bab2c1af2ebb12c3fa7d0d4d0d243ac4
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2024-04-04 13:56:23 +0200

    created analysis module to bundle up various methods (#359)
    Moved set of methods that were in _analysis module to be class methods.
    Renamed methods (usually dropped "to" and "get")
    
    This is prep for adding methods to the analysis class that will analyze rules to detect modeling scenarion.

--- tests/tests_unit/rules/test_analysis.py
+++ tests/tests_unit/rules/test_analysis.py
@@ -1,32 +1,62 @@
-from cognite.neat.rules._analysis import (
-    get_class_linkage,
-    get_connected_classes,
-    get_defined_classes,
-    get_disconnected_classes,
-    get_symmetric_pairs,
-)
+from cognite.neat.rules._analysis._information_rules import InformationArchitectRulesAnalysis
 from cognite.neat.rules.models._rules import InformationRules
 from cognite.neat.rules.models._rules._types import ClassEntity
 
 
 class TestRulesAnalysis:
+    def test_class_parent_pairs(self, david_rules: InformationRules) -> None:
+        assert len(InformationArchitectRulesAnalysis(david_rules).class_parent_pairs()) == 26
+
+    def test_classes_with_properties(self, david_rules: InformationRules) -> None:
+        assert len(InformationArchitectRulesAnalysis(david_rules).classes_with_properties()) == 20
+
+    def test_class_property_pairs(self, david_rules: InformationRules) -> None:
+        assert len(InformationArchitectRulesAnalysis(david_rules).class_property_pairs()) == 20
+
     def test_defined_classes(self, david_rules: InformationRules) -> None:
-        assert len(get_defined_classes(david_rules, consider_inheritance=False)) == 20
-        assert len(get_defined_classes(david_rules, consider_inheritance=True)) == 26
+        assert len(InformationArchitectRulesAnalysis(david_rules).defined_classes(consider_inheritance=False)) == 20
+        assert len(InformationArchitectRulesAnalysis(david_rules).defined_classes(consider_inheritance=True)) == 26
 
     def test_disconnected_classes(self, david_rules: InformationRules) -> None:
-        assert get_disconnected_classes(david_rules, consider_inheritance=False) == {
+        assert InformationArchitectRulesAnalysis(david_rules).disconnected_classes(consider_inheritance=False) == {
             ClassEntity.from_raw("power:GeoLocation")
         }
 
     def test_connected_classes(self, david_rules: InformationRules) -> None:
-        assert len(get_connected_classes(david_rules, consider_inheritance=False)) == 24
-        assert len(get_connected_classes(david_rules, consider_inheritance=True)) == 25
+        assert len(InformationArchitectRulesAnalysis(david_rules).connected_classes(consider_inheritance=False)) == 24
+        assert len(InformationArchitectRulesAnalysis(david_rules).connected_classes(consider_inheritance=True)) == 25
 
     def test_get_class_linkage(self, david_rules: InformationRules) -> None:
-        assert len(get_class_linkage(david_rules, consider_inheritance=False)) == 28
-        assert len(get_class_linkage(david_rules, consider_inheritance=True)) == 63
+        assert len(InformationArchitectRulesAnalysis(david_rules).class_linkage(consider_inheritance=False)) == 28
+        assert len(InformationArchitectRulesAnalysis(david_rules).class_linkage(consider_inheritance=True)) == 63
 
     def test_symmetric_pairs(self, david_rules: InformationRules) -> None:
-        assert len(get_symmetric_pairs(david_rules, consider_inheritance=True)) == 0
-        assert len(get_symmetric_pairs(david_rules, consider_inheritance=False)) == 0
+        assert (
+            len(
+                InformationArchitectRulesAnalysis(david_rules).symmetrically_connected_classes(
+                    consider_inheritance=True
+                )
+            )
+            == 0
+        )
+        assert (
+            len(
+                InformationArchitectRulesAnalysis(david_rules).symmetrically_connected_classes(
+                    consider_inheritance=False
+                )
+            )
+            == 0
+        )
+
+    def test_subset_rules(self, david_rules: InformationRules) -> None:
+        assert InformationArchitectRulesAnalysis(david_rules).subset_rules(
+            {ClassEntity.from_raw("power:GeoLocation")}
+        ).classes.data[0].class_ == ClassEntity.from_raw("power:GeoLocation")
+        assert (
+            len(
+                InformationArchitectRulesAnalysis(david_rules)
+                .subset_rules({ClassEntity.from_raw("power:GeoLocation")})
+                .classes.data
+            )
+            == 1
+        )
