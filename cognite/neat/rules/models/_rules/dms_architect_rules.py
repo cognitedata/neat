85a963032f109806076e37140671e859d9fc7788
nikokaoja <nikola.vasiljevic@cognite.com>
2024-04-04 20:02:12 +0200

    add ReferenceType

--- cognite/neat/rules/models/_rules/dms_architect_rules.py
+++ cognite/neat/rules/models/_rules/dms_architect_rules.py
@@ -160,7 +160,7 @@ class DMSProperty(SheetEntity):
     nullable: bool | None = Field(default=None, alias="Nullable")
     is_list: bool | None = Field(default=None, alias="IsList")
     default: str | int | dict | None = Field(None, alias="Default")
-    reference: ReferenceType = Field(None, alias="Reference")
+    reference: ReferenceType = Field(default=None, alias="Reference")
     container: ContainerType | None = Field(None, alias="Container")
     container_property: str | None = Field(None, alias="ContainerProperty")
     view: ViewType = Field(alias="View")
