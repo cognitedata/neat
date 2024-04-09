cc90478ad26acb483535a12cf7da8effc7cca469
anders-albert <anders0albert@gmail.com>
2024-04-09 16:14:04 +0200

    refactor: sort filters

--- cognite/neat/rules/models/_rules/dms_architect_rules.py
+++ cognite/neat/rules/models/_rules/dms_architect_rules.py
@@ -796,7 +796,7 @@ class _DMSExporter:
         parent_views = {parent for view in views for parent in view.implements or []}
         node_type_flag = False
         for view in views:
-            ref_containers = view.referenced_containers()
+            ref_containers = sorted(view.referenced_containers(), key=lambda c: c.as_tuple())
             has_data = dm.filters.HasData(containers=list(ref_containers)) if ref_containers else None
             node_type = dm.filters.Equals(["node", "type"], {"space": view.space, "externalId": view.external_id})
             if view.as_id() in parent_views:
