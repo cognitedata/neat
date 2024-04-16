d18feff74d7338ff4b917e8f0582dc71a1d4ce1a
anders-albert <anders0albert@gmail.com>
2024-04-16 16:42:08 +0200

    refactor; cleanup

--- tests/tests_integration/test_rules/test_importers/test_dms_importers.py
+++ tests/tests_integration/test_rules/test_importers/test_dms_importers.py
@@ -23,11 +23,8 @@ def alice_data_model_id(alice_rules: DMSRules) -> DataModelId:
 
 class TestDMSImporter:
     def test_import_from_cdf(self, cognite_client: CogniteClient, alice_data_model_id: DataModelId):
-        dms_exporter = DMSImporter.from_data_model_id(
-            cognite_client,
-            alice_data_model_id,
-        )
+        dms_exporter = DMSImporter.from_data_model_id(cognite_client, alice_data_model_id)
 
-        rules = dms_exporter.to_rules(errors="raise", role=RoleTypes.information_architect, is_reference=True)
+        rules = dms_exporter.to_rules(errors="raise", role=RoleTypes.information_architect)
 
         assert isinstance(rules, InformationRules)
