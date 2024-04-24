cff73a4477e796842bd630b7f8c27805c9accf64
nikokaoja <nikola.vasiljevic@cognite.com>
2024-04-24 10:35:03 +0200

    update of UI and new step added

--- cognite/neat/app/api/routers/core.py
+++ cognite/neat/app/api/routers/core.py
@@ -35,7 +35,7 @@ async def convert_data_model_to_rules(file: UploadFile):
 
     # load as OWL
     elif suffix in [".ttl", ".owl"]:
-        rules, issues = importers.OWLImporter(owl_filepath=temp_filepath).to_rules(role=RoleTypes.dms_architect)
+        rules, issues = importers.OWLImporter(filepath=temp_filepath).to_rules(role=RoleTypes.dms_architect)
 
     # load as YAML
     elif suffix in [".yml", ".yaml"]:
