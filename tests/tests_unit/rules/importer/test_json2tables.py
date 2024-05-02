387ded281019ff136d56d6da79dcd17a161edf9f
anders-albert <anders0albert@gmail.com>
2024-05-02 19:48:03 +0200

    refactor: run newest ruff

--- tests/tests_unit/rules/importer/test_json2tables.py
+++ tests/tests_unit/rules/importer/test_json2tables.py
@@ -16,11 +16,9 @@ def test_json2rules_power_grid() -> None:
     rules = json_importer.to_rules()
 
     # Assert
-    assert not (
-        missing := set(rules.classes) - set(expected_properties)
-    ), f"JSONImporter did not find classes {missing}"
+    missing = set(rules.classes) - set(expected_properties)
+    assert not missing, f"JSONImporter did not find classes {missing}"
     properties_by_class = rules.properties.groupby("class_id")
     for class_name, expected in expected_properties.items():
-        assert not (
-            missing := expected - {prop.property_name for prop in properties_by_class.get(class_name, []).values()}
-        ), f"JSONImporter did not find properties {missing} for class {class_name}"
+        missing = expected - {prop.property_name for prop in properties_by_class.get(class_name, []).values()}
+        assert not missing, f"JSONImporter did not find properties {missing} for class {class_name}"
