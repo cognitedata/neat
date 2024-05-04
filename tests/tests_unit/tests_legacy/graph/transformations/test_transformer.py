dddd414718f033caf6754e254c537f11f87d4da4
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-05-04 09:57:56 +0200

    [NEAT-219] Moved legacy tests 👴 (#428)
    * refactor: moved all tests into legacy
    
    * refactor: Moved ExcelImporter rules to the correct place

---
+++ tests/tests_unit/tests_legacy/graph/transformations/test_transformer.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from cognite.client.testing import monkeypatch_cognite_client
+from rdflib import Graph, Namespace
+
+from cognite.neat.legacy.graph.transformations.transformer import domain2app_knowledge_graph
+from cognite.neat.legacy.rules.models.rdfpath import TransformationRuleType
+from cognite.neat.legacy.rules.models.rules import Rules
+
+
+def test_domain2app_knowledge_graph(transformation_rules: Rules, source_knowledge_graph: Graph):
+    # Arrange
+    rules = transformation_rules
+    domain_graph = source_knowledge_graph
+
+    # Act
+    app_graph = domain2app_knowledge_graph(domain_graph, rules)
+
+    # Assert
+    res = app_graph.query(
+        """SELECT ?o WHERE {<http://purl.org/nordic44#_f17695aa-9aeb-11e5-91da-b8763fd99c5f> rdf:type ?o}"""
+    )
+    assert next(iter(res))[0] == Namespace("http://purl.org/cognite/tnt#").Substation
+
+
+def test_domain2app_knowledge_graph_raw_lookup(transformation_rules: Rules, source_knowledge_graph: Graph):
+    # Arrange
+    rules = transformation_rules.model_copy(deep=True)
+    rules.properties["row 18"].rule += " | TerminalName(NordicName, TNTName)"
+    rules.properties["row 18"].rule_type = TransformationRuleType.rawlookup
+    domain_graph = source_knowledge_graph
+    database_name = "look_up"
+
+    # Act
+    with monkeypatch_cognite_client() as client_mock:
+
+        def mock_retrieve(db_name, table_name, **_):
+            if db_name == database_name and table_name == "TerminalName":
+                return pd.DataFrame([{"NordicName": "ARENDAL 300 A T1", "TNTName": "Gjerstad"}])
+
+        client_mock.raw.rows.retrieve_dataframe = mock_retrieve
+        app_graph = domain2app_knowledge_graph(
+            domain_graph, rules, client=client_mock, cdf_lookup_database=database_name
+        )
+
+    Gjerstad_node = app_graph.query(
+        "SELECT ?o WHERE {<http://purl.org/nordic44#_2dd9019e-bdfb-11e5-94fa-c8f73332c8f4> "
+        "tnt:IdentifiedObject.name ?o}"
+    )
+
+    NaN_node = app_graph.query(
+        "SELECT ?o WHERE {<http://purl.org/nordic44#_2dd9040e-bdfb-11e5-94fa-c8f73332c8f4> "
+        "tnt:IdentifiedObject.name ?o}"
+    )
+    assert next(iter(Gjerstad_node))[0].value == "Gjerstad"
+    assert next(iter(NaN_node))[0].value == "NaN"
---
+++ tests/tests_unit/tests_legacy/tests_rules/exporter/test_rules2excel.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from cognite.neat.legacy.rules.exporters import ExcelExporter
+from cognite.neat.legacy.rules.importers import ExcelImporter
+
+
+def test_rules2excel(simple_rules, tmp_path: Path) -> None:
+    file = tmp_path / "rules.xlsx"
+
+    ExcelExporter(rules=simple_rules).export_to_file(filepath=file)
+
+    importer = ExcelImporter(filepath=file).to_rules()
+
+    assert len(importer.classes) == len(simple_rules.classes)
+    assert len(importer.properties) == len(simple_rules.properties)
+    assert importer.properties.model_dump() == simple_rules.properties.model_dump()
