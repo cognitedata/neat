407b3ec8d0ac6951174b914604524f8252c9443c
anders-albert <anders0albert@gmail.com>
2024-05-04 11:20:40 +0200

    tests: Setup correct conftest files

---
+++ tests/tests_unit/app/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+from cognite.neat.legacy.rules import importers
+from cognite.neat.legacy.rules.models.rules import Rules
+from tests import config as config
+
+
+@pytest.fixture(scope="session")
+def transformation_rules() -> Rules:
+    return importers.ExcelImporter(config.TNT_TRANSFORMATION_RULES).to_rules()
