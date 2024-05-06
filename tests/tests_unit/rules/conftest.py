a287056d0d937cc85b2e44e2214486efffe09fbe
anders-albert <anders0albert@gmail.com>
2024-05-06 16:07:56 +0200

    refactor: update remaining imports

--- tests/tests_unit/rules/conftest.py
+++ tests/tests_unit/rules/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 from cognite.neat.rules.importers import ExcelImporter
 from cognite.neat.rules.models import DMSRules, DomainRules, InformationRules, RoleTypes
-from cognite.neat.rules.models.dms import DMSRulesWrite
+from cognite.neat.rules.models.dms import DMSRulesInput
 from cognite.neat.utils.spreadsheet import read_individual_sheet
 from tests.config import DOC_RULES
 
@@ -24,7 +24,7 @@ def alice_spreadsheet() -> dict[str, dict[str, Any]]:
 
 @pytest.fixture(scope="session")
 def alice_rules(alice_spreadsheet: dict[str, dict[str, Any]]) -> DMSRules:
-    return DMSRulesWrite.load(alice_spreadsheet).as_rules()
+    return DMSRulesInput.load(alice_spreadsheet).as_rules()
 
 
 @pytest.fixture(scope="session")
