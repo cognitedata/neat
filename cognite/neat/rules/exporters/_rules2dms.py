cbcd2b8ac67c04d1fe6e2aaa462c33fe44a3cf69
anders-albert <anders0albert@gmail.com>
2024-05-07 12:15:14 +0200

    refactor: remove use of frozen ids in DMSExporter

--- cognite/neat/rules/exporters/_rules2dms.py
+++ cognite/neat/rules/exporters/_rules2dms.py
@@ -11,8 +11,8 @@ from cognite.client.exceptions import CogniteAPIError
 from cognite.neat.rules import issues
 from cognite.neat.rules._shared import Rules
 from cognite.neat.rules.issues import IssueList
-from cognite.neat.rules.models import ExtensionCategory, InformationRules, SheetList
-from cognite.neat.rules.models.dms import DMSContainer, DMSRules, DMSSchema, PipelineSchema
+from cognite.neat.rules.models import InformationRules
+from cognite.neat.rules.models.dms import DMSRules, DMSSchema, PipelineSchema
 from cognite.neat.utils.cdf_loaders import (
     ContainerLoader,
     DataModelingLoader,
@@ -114,47 +114,9 @@ class DMSExporter(CDFExporter[DMSSchema]):
             dms_rules = rules.as_dms_architect_rules()
         else:
             raise ValueError(f"{type(rules).__name__} cannot be exported to DMS")
-
-        is_solution_model = (
-            dms_rules.reference and dms_rules.metadata.external_id != dms_rules.reference.metadata.external_id
+        return dms_rules.as_schema(
+            include_ref=True, include_pipeline=self.export_pipeline, instance_space=self.instance_space
         )
-        is_new_model = dms_rules.reference is None
-        if is_new_model or is_solution_model:
-            return dms_rules.as_schema(False, self.export_pipeline, self.instance_space)
-
-        # This is an extension of an existing model.
-        reference_rules = cast(DMSRules, dms_rules.reference).model_copy(deep=True)
-        reference_schema = reference_rules.as_schema(include_ref=False, include_pipeline=self.export_pipeline)
-
-        # Todo Move this to an appropriate location
-        # Merging Reference with User Rules
-        combined_rules = dms_rules.model_copy(deep=True)
-        existing_containers = {container.class_ for container in combined_rules.containers or []}
-        if combined_rules.containers is None:
-            combined_rules.containers = SheetList[DMSContainer](data=[])
-        for container in reference_rules.containers or []:
-            if container.class_ not in existing_containers:
-                container.reference = None
-                combined_rules.containers.append(container)
-        existing_views = {view.class_ for view in combined_rules.views}
-        for view in reference_rules.views:
-            if view.class_ not in existing_views:
-                view.reference = None
-                combined_rules.views.append(view)
-        existing_properties = {(property_.class_, property_.property_) for property_ in combined_rules.properties}
-        for property_ in reference_rules.properties:
-            if (property_.class_, property_.property_) not in existing_properties:
-                property_.reference = None
-                combined_rules.properties.append(property_)
-
-        schema = combined_rules.as_schema(True, self.export_pipeline, self.instance_space)
-
-        if dms_rules.metadata.extension in (ExtensionCategory.addition, ExtensionCategory.reshape):
-            # We do not freeze views as they might be changed, even for addition,
-            # in case, for example, new properties are added. The validation will catch this.
-            schema.frozen_ids.update(set(reference_schema.containers.as_ids()))
-            schema.frozen_ids.update(set(reference_schema.node_types.as_ids()))
-        return schema
 
     def delete_from_cdf(self, rules: Rules, client: CogniteClient, dry_run: bool = False) -> Iterable[UploadResult]:
         schema, to_export = self._prepare_schema_and_exporters(rules, client)
@@ -163,8 +125,7 @@ class DMSExporter(CDFExporter[DMSSchema]):
         # as they are sorted in the order of creation and we need to delete them in reverse order
         for all_items, loader in reversed(to_export):
             all_item_ids = loader.get_ids(all_items)
-            skipped = sum(1 for item_id in all_item_ids if item_id in schema.frozen_ids)
-            item_ids = [item_id for item_id in all_item_ids if item_id not in schema.frozen_ids]
+            item_ids = [item_id for item_id in all_item_ids]
             cdf_items = loader.retrieve(item_ids)
             cdf_item_by_id = {loader.get_id(item): item for item in cdf_items}
             items = [item for item in all_items if loader.get_id(item) in item_ids]
@@ -198,7 +159,7 @@ class DMSExporter(CDFExporter[DMSSchema]):
             yield UploadResult(
                 name=loader.resource_name,
                 deleted=deleted,
-                skipped=skipped,
+                skipped=0,
                 failed_deleted=failed_deleted,
                 error_messages=error_messages,
             )
@@ -213,8 +174,7 @@ class DMSExporter(CDFExporter[DMSSchema]):
         for all_items, loader in to_export:
             issue_list = IssueList()
             all_item_ids = loader.get_ids(all_items)
-            skipped = sum(1 for item_id in all_item_ids if item_id in schema.frozen_ids)
-            item_ids = [item_id for item_id in all_item_ids if item_id not in schema.frozen_ids]
+            item_ids = [item_id for item_id in all_item_ids]
             cdf_items = loader.retrieve(item_ids)
             cdf_item_by_id = {loader.get_id(item): item for item in cdf_items}
             items = [item for item in all_items if loader.get_id(item) in item_ids]
@@ -241,6 +201,7 @@ class DMSExporter(CDFExporter[DMSSchema]):
 
             created = len(to_create)
             failed_created = 0
+            skipped = 0
 
             if self.existing_handling in ["update", "force"]:
                 changed = len(to_update)
