1a81800f363e324dac776d3bcef17237e4af54f1
nikokaoja <nikola.vasiljevic@cognite.com>
2024-05-30 09:48:11 +0200

    extended tests

--- tests/tests_unit/rules/test_importers/test_inference_importer.py
+++ tests/tests_unit/rules/test_importers/test_inference_importer.py
@@ -3,9 +3,18 @@ from cognite.neat.rules.importers import InferenceImporter
 
 
 def test_rdf_inference():
-    rules, _ = InferenceImporter.from_rdf_file(nordic44_knowledge_graph, max_number_of_instance=1).to_rules(
-        errors="continue"
-    )
+    rules, _ = InferenceImporter.from_rdf_file(
+        nordic44_knowledge_graph, max_number_of_instance=1, make_compliant=True
+    ).to_rules(errors="continue")
 
     assert len(rules.properties) == 296
     assert len(rules.classes) == 59
+
+    # make compliant will make sure that "." from the original property will be replace
+    assert rules.properties.data[0].property_ == "CurrentLimit_value"
+
+    # make compliant will do two things for this property:
+    # 1. redefinition of property will get new ID by adding "_(property number)", in this case 20
+    # 2. replace " " with "_"
+
+    assert rules.properties.data[19].property_ == "Terminal_ConductingEquipment_20"
