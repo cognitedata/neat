3f1bae808dd7c6f33a88397fd933dbd2399e1e80
anders-albert <anders0albert@gmail.com>
2024-06-03 10:57:41 +0200

    refactor: First draft implementation

--- cognite/neat/graph/loaders/_rdf2dms.py
+++ cognite/neat/graph/loaders/_rdf2dms.py
@@ -3,19 +3,21 @@ import warnings
 from collections import defaultdict
 from collections.abc import Iterable
 from pathlib import Path
-from typing import cast
+from typing import Any, cast
 
 import yaml
 from cognite.client import CogniteClient
 from cognite.client import data_modeling as dm
 from cognite.client.data_classes.data_modeling import ViewId
-from pydantic import HttpUrl, TypeAdapter, ValidationError
+from pydantic import HttpUrl, TypeAdapter, ValidationError, ValidationInfo, create_model, field_validator
+from pydantic.main import Model
 from rdflib.query import ResultRow
 from rdflib.term import URIRef
 
 from cognite.neat.graph.stores import NeatGraphStoreBase
 from cognite.neat.rules.issues import NeatValidationError
 from cognite.neat.rules.models import DMSRules
+from cognite.neat.rules.models.data_types import _DATA_TYPE_BY_DMS_TYPE
 
 from ._base import CDFLoader
 
@@ -55,13 +57,14 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
 
     def _load(self, stop_on_exception: bool = False) -> Iterable[dm.InstanceApply | NeatValidationError]:
         for view in self.data_model.views:
-            # Some tracking and creation of a structure to do validation
-            validation_structure = self._create_validation_structure(view)
+            view_id = view.as_id()
+            # Todo Some tracking and creation of a structure to do validation
+            validation_structure = self._create_validation_structure(view)  # type: ignore[var-annotated]
             uri_ref = self.class_by_view_id.get(view.as_id(), URIRef(f"{view.space}:{view.external_id}"))
             triples = self.graph_store.queries.list_instances_of_type(uri_ref)
             for identifier, properties in _triples2dictionary(triples).items():
                 try:
-                    yield self._create_instance(identifier, properties, validation_structure)
+                    yield self._create_instance(identifier, properties, validation_structure, view_id)
                 except Exception:
                     # Todo Convert to NeatValidationError
                     raise
@@ -90,11 +93,48 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
             else:
                 yaml.safe_dump(dumped, f, sort_keys=False)
 
-    def _create_validation_structure(self, view: dm.View) -> dict:
-        return {}
+    def _create_validation_structure(self, view: dm.View) -> type[Model]:
+        field_definitions: dict[str, tuple[type, Any]] = {}
+        for prop_name, prop in view.properties.items():
+            if not isinstance(prop, dm.MappedProperty):
+                continue
+            data_type = _DATA_TYPE_BY_DMS_TYPE.get(prop.type._type)
+            if not data_type:
+                # Todo warning
+                continue
+            python_type: Any = data_type.python
+            if prop.type.is_list:
+                python_type = list[python_type]
+            default_value: Any = prop.default_value
+            if prop.nullable:
+                python_type = python_type | None
+            else:
+                default_value = ...
 
-    def _create_instance(self, class_name: str, properties: dict, validation_structure: dict) -> dm.InstanceApply:
-        raise NotImplementedError()
+            field_definitions[prop_name] = (python_type, default_value)
+
+        def parse_list(cls, value: Any, info: ValidationInfo) -> list[str]:
+            if isinstance(value, list) and cls.model_fields[info.field_name].annotation is not list:
+                if len(value) == 1:
+                    return value[0]
+                raise ValueError(f"Got multiple values for {info.field_name}: {value}")
+            return value
+
+        validators: dict[str, classmethod] = {"parse_list": field_validator("*", mode="before")(parse_list)}  # type: ignore[dict-item,arg-type]
+
+        return create_model(view.external_id, __validators__=validators, **field_definitions)  # type: ignore[arg-type, call-overload]
+
+    def _create_instance(
+        self, identifier: str, properties: dict, validation_structure: type[Model], view_id: dm.ViewId
+    ) -> dm.InstanceApply:
+        created = validation_structure.model_validate(properties)
+
+        return dm.NodeApply(
+            space=self.data_model.space,
+            external_id=identifier,
+            # type=#RDF type
+            sources=[dm.NodeOrEdgeData(source=view_id, properties=dict(created.model_dump().items()))],
+        )
 
 
 def _triples2dictionary(
@@ -103,12 +143,12 @@ def _triples2dictionary(
     """Converts list of triples to dictionary"""
     values_by_property_by_identifier: dict[str, dict[str, list[str]]] = defaultdict(lambda: defaultdict(list))
     for triple in triples:
-        id_, property_, value = _remove_namespace(triple)
+        id_, property_, value = _remove_namespace(triple, special_separator=":")
         values_by_property_by_identifier[id_][property_].append(value)
     return values_by_property_by_identifier
 
 
-def _remove_namespace(URI: ResultRow, special_separator: str = "#_") -> list[str]:
+def _remove_namespace(URI: ResultRow, special_separator: str = "#") -> list[str]:
     """Removes namespace from URI
 
     Args
@@ -142,6 +182,10 @@ def _remove_namespace(URI: ResultRow, special_separator: str = "#_") -> list[str
             _ = TypeAdapter(HttpUrl).validate_python(u)
             output.append(u.split(special_separator if special_separator in u else "#" if "#" in u else "/")[-1])
         except ValidationError:
-            output.append(str(u))
+            u_str = str(u)
+            if special_separator in u_str:
+                output.append(u_str.split(special_separator)[-1])
+            else:
+                output.append(u_str)
 
     return output
