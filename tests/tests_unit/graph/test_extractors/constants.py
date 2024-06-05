8fce5816be2c5df92e0b94e128dfea33f3631623
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2024-06-05 18:00:24 +0200

    added relationship extractor (#478)

--- tests/tests_unit/graph/test_extractors/constants.py
+++ tests/tests_unit/graph/test_extractors/constants.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 ASSET_EXTRACTOR_DATA = Path(__file__).parent / "asset_extractor_data"
+RELATIONSHIP_EXTRACTOR_DATA = Path(__file__).parent / "relationship_extractor_data"
