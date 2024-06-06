a7af0c62854cc79a289778f1e1edac5845bfaf96
nikokaoja <nikola.vasiljevic@cognite.com>
2024-06-06 14:37:21 +0200

    added events extractor

--- cognite/neat/graph/extractors/__init__.py
+++ cognite/neat/graph/extractors/__init__.py
@@ -1,4 +1,5 @@
 from ._classic_cdf._assets import AssetsExtractor
+from ._classic_cdf._events import EventsExtractor
 from ._classic_cdf._relationships import RelationshipsExtractor
 from ._classic_cdf._sequences import SequencesExtractor
 from ._classic_cdf._timeseries import TimeSeriesExtractor
@@ -10,4 +11,5 @@ __all__ = [
     "RelationshipsExtractor",
     "TimeSeriesExtractor",
     "SequencesExtractor",
+    "EventsExtractor",
 ]
