f721131eb7c408e309708508466427c5b94164b2
nikokaoja <nikola.vasiljevic@cognite.com>
2024-06-19 11:03:29 +0200

    added warning test

--- tests/tests_unit/graph/test_transformers/test_asset_ts_connector_transformer.py
+++ tests/tests_unit/graph/test_transformers/test_asset_ts_connector_transformer.py
@@ -1,3 +1,5 @@
+import pytest
+
 from cognite.neat.constants import DEFAULT_NAMESPACE
 from cognite.neat.graph import extractors, transformers
 from cognite.neat.graph.stores import NeatGraphStore
@@ -26,3 +28,25 @@ def test_asset_ts_connector_transformer():
         DEFAULT_NAMESPACE.TimeSeries_7075848883606566,
         DEFAULT_NAMESPACE.TimeSeries_7015848883606566,
     }
+
+
+def test_asset_ts_connector_transformer_warning():
+    store = NeatGraphStore.from_memory_store()
+
+    with pytest.warns(
+        UserWarning,
+        match="Cannot transform graph store with AssetTimeSeriesConnector, missing one or more required change",
+    ):
+        store.transform(transformers.AssetTimeSeriesConnector())
+
+    # Extract assets
+    store.write(extractors.AssetsExtractor.from_file(CLASSIC_CDF_EXTRACTOR_DATA / "assets.yaml"))
+
+    # Extract time series
+    store.write(extractors.TimeSeriesExtractor.from_file(CLASSIC_CDF_EXTRACTOR_DATA / "timeseries.yaml"))
+
+    # Connect assets and time series
+    store.transform(transformers.AssetTimeSeriesConnector())
+
+    with pytest.warns(UserWarning, match="Cannot transform graph store with AssetTimeSeriesConnector, already applied"):
+        store.transform(transformers.AssetTimeSeriesConnector())
