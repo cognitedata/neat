089c2f0f5652d860ebce3bba1df0874bab828c43
nikokaoja <nikola.vasiljevic@cognite.com>
2024-06-21 14:29:17 +0200

    update code for readability

--- cognite/neat/graph/queries/_construct.py
+++ cognite/neat/graph/queries/_construct.py
@@ -17,6 +17,11 @@ from cognite.neat.utils.utils import most_occurring_element
 
 from ._shared import Triple, hop2property_path
 
+_QUERY_TEMPLATE = """CONSTRUCT {{ {graph_template} }}
+                     WHERE {{     {graph_pattern}
+                                  {filter}
+                     }} ORDER BY ?instance"""
+
 
 def build_construct_query(
     class_: ClassEntity,
@@ -35,9 +40,14 @@ def build_construct_query(
         class_instances : List of class instances to filter by. Defaults to None (no filter, return all instances).
 
     Returns:
-        str: The CONSTRUCT query.
+        str: CONSTRUCT query.
 
-    Notes:
+    !!! note "On CONSTRUCT Query"
+        CONSTRUCT query is composed of two parts: graph template and graph pattern.
+        Graph template is used the shape of instance acquired using graph pattern.
+        This allows us to create a new graph with the new shape without actually modifying
+        the original graph, or creating new instances.
+
         The CONSTRUCT query is far less forgiving than the SELECT query. It will not return
         anything if one of the properties that define the "shape" of the class instance is missing.
         This is the reason why there is an option to make all properties optional, so that
@@ -48,57 +58,60 @@ def build_construct_query(
         .class_property_pairs(only_rdfpath=True, consider_inheritance=True)
         .get(class_, None)
     ):
-        query_template = "CONSTRUCT {graph_template\n}\n\nWHERE {graph_pattern\ninsert_filter} ORDER BY ?instance"
-        query_template = add_filter(class_instances, query_template)
-
         templates, patterns = to_construct_triples(
             graph, list(transformations.values()), rules.prefixes, properties_optional
         )
 
-        graph_template = "\n           ".join(triples2sparql_statement(templates))
-        graph_pattern = "\n       ".join(triples2sparql_statement(patterns))
+        return _QUERY_TEMPLATE.format(
+            graph_template="\n".join(triples2sparql_statement(templates)),
+            graph_pattern="\n".join(triples2sparql_statement(patterns)),
+            filter="" if not class_instances else add_filter(class_instances),
+        )
 
-        return query_template.replace("graph_template", graph_template).replace("graph_pattern", graph_pattern)
     else:
         return None
 
 
-def add_filter(class_instances, query_template):
-    if class_instances:
-        class_instances_formatted = [f"<{instance}>" for instance in class_instances]
-        query_template = query_template.replace(
-            "insert_filter", f"\n\nFILTER (?instance IN ({', '.join(class_instances_formatted)}))"
-        )
-    else:
-        query_template = query_template.replace("insert_filter", "")
-    return query_template
+def add_filter(class_instances: list[URIRef]):
+    class_instances_formatted = [f"<{instance}>" for instance in class_instances]
+    return f"FILTER (?instance IN ({', '.join(class_instances_formatted)}))"
 
 
 def to_construct_triples(
     graph: Graph, transformations: list[InformationProperty], prefixes: dict, properties_optional: bool = True
 ) -> tuple[list[Triple], list[Triple]]:
-    """Converts class definition to CONSTRUCT triples which are used to generate CONSTRUCT query
+    """Converts transformations of a class to CONSTRUCT triples which are used to generate CONSTRUCT query
 
     Args:
-        graph: Graph containing instances of classes
-        transformations : List of transformations to apply
+        graph: Graph containing instances of classes (used for property inference for hops)
+        transformations : List of transformations to use to form triples
         prefixes : Dictionary of prefixes for namespaces
         properties_optional : Flag indicating if properties should be optional. Defaults to True.
 
     Returns:
         tuple: Tuple of triples that define graph template and graph pattern parts of CONSTRUCT query
+
+
+    !!! note "Purely inherited transformations"
+        Assumption that neat makes is that in case of purely inherited transformations
+        we will type instance with class to which transformation belongs to.
+
+        Otherwise we will type instance with class that is most occurring in non-inherited
+        transformations.
+
     """
     # TODO: Add handling of UNIONs in rules
 
     templates = []
     patterns = []
-    class_ids = []
+    non_inherited_starting_rdf_types = []
 
     for transformation in transformations:
         traversal = cast(RDFPath, transformation.transformation).traversal
 
+        # keeping track of starting rdf types of non-inherited transformations/properties
         if isinstance(traversal, Traversal) and not transformation.inherited:
-            class_ids.append(traversal.class_.id)
+            non_inherited_starting_rdf_types.append(traversal.class_.id)
 
         graph_template_triple = Triple(
             subject="?instance",
@@ -108,13 +121,13 @@ def to_construct_triples(
         )
         templates.append(graph_template_triple)
 
-        # AllReferences should not be "optional" since we are creating their values
-        # by binding them to certain property
+        # use case AllReferences: binding instance to certain rdf property
         if isinstance(traversal, AllReferences):
             graph_pattern_triple = Triple(
                 subject="BIND(?instance", predicate="AS", object=f"{graph_template_triple.object})", optional=False
             )
 
+        # use case SingleProperty: simple property traversal
         elif isinstance(traversal, SingleProperty):
             graph_pattern_triple = Triple(
                 subject=graph_template_triple.subject,
@@ -123,6 +136,8 @@ def to_construct_triples(
                 optional=True if properties_optional else not transformation.is_mandatory,
             )
 
+        # use case Hop: property traversal with multiple hops turned into property path
+        # see: https://www.oxfordsemantic.tech/faqs/what-is-a-property-path
         elif isinstance(traversal, Hop):
             graph_pattern_triple = Triple(
                 subject="?instance",
@@ -130,16 +145,30 @@ def to_construct_triples(
                 object=graph_template_triple.object,
                 optional=True if properties_optional else not transformation.is_mandatory,
             )
+
+        # other type of rdfpaths are skipped
         else:
             continue
 
         patterns.append(graph_pattern_triple)
 
-    # add first triple for graph pattern stating type of object
-    # we use most occurring here to pull out most occurring class id as type
-    # this is safeguard in case there are multiple classes in the graph pattern
+    # Add first triple for graph pattern stating type of object
+    # we use most occurring here to pull out most occurring rdf type of the starting
+    # node of the transformation, or the class itself to which the transformation is
+    # defined for.
+    # This is safeguard in case there are multiple classes in the graph pattern
     patterns.insert(
-        0, Triple(subject="?instance", predicate="a", object=most_occurring_element(class_ids), optional=False)
+        0,
+        Triple(
+            subject="?instance",
+            predicate="a",
+            object=(
+                most_occurring_element(non_inherited_starting_rdf_types)
+                if non_inherited_starting_rdf_types
+                else str(transformation.class_)
+            ),
+            optional=False,
+        ),
     )
 
     return templates, patterns
