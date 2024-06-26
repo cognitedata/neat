44aec004a61d2fe01cf6c7f78e632acd7fca385d
anders-albert <anders0albert@gmail.com>
2024-06-26 17:16:45 +0200

    refactor: started on neat client

---
+++ cognite/neat/utils/auth.py
@@ -0,0 +1,13 @@
+class NeatClient:
+    @staticmethod
+    def _is_notebook() -> bool:
+        try:
+            shell = get_ipython().__class__.__name__  # type: ignore[name-defined]
+            if shell == "ZMQInteractiveShell":
+                return True  # Jupyter notebook or qtconsole
+            elif shell == "TerminalInteractiveShell":
+                return False  # Terminal running IPython
+            else:
+                return False  # Other type (?)
+        except NameError:
+            return False  # Probably standard Python interpreter
