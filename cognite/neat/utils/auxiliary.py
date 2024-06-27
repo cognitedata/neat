ae371f96029a9f8048e307d31ab5662d181a9704
anders-albert <anders0albert@gmail.com>
2024-06-27 11:12:01 +0200

    refactor: added helper to get classmethods

--- cognite/neat/utils/auxiliary.py
+++ cognite/neat/utils/auxiliary.py
@@ -1,4 +1,6 @@
 import importlib
+import inspect
+from collections.abc import Callable
 from types import ModuleType
 
 from cognite.neat.exceptions import NeatImportError
@@ -9,3 +11,9 @@ def local_import(module: str, extra: str) -> ModuleType:
         return importlib.import_module(module)
     except ImportError as e:
         raise NeatImportError(module.split(".")[0], extra) from e
+
+
+def get_classmethods(cls: type) -> list[Callable]:
+    return [
+        func for _, func in inspect.getmembers(cls, lambda x: inspect.ismethod(x) and not x.__name__.startswith("_"))
+    ]
