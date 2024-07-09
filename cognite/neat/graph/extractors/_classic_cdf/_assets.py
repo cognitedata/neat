a44d890c57c5f331c955fcab43d74d7a3d15c56e
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-07-09 13:10:10 +0200

    [NEAT-310]🏰Large Example (#533)
    * docs; ellaborated tutorial
    
    * refactor: more detailed feedback
    
    * docs: Finished tutotrial
    
    * tests: updated test
    
    * refactor; skip namespace
    
    * tests: Shell + failing test
    
    * feat: Added option for setting type in Asset Extractor
    
    * build; bump
    
    * refactor: prefix as a parameter in Inferrence
    
    * refactor: progress bar and handle spaces in type
    
    * refactor; support setting limit on asset extractor
    
    * refactor: fix progress bar
    
    * fix: bug in inference2rules
    
    * Update cognite/neat/rules/importers/_inference2rules.py
    
    * tests: updated tests

--- cognite/neat/graph/extractors/_classic_cdf/_assets.py
+++ cognite/neat/graph/extractors/_classic_cdf/_assets.py
@@ -1,10 +1,11 @@
+import re
 from collections.abc import Callable, Iterable
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import cast
 
 from cognite.client import CogniteClient
-from cognite.client.data_classes import Asset, AssetList
+from cognite.client.data_classes import Asset, AssetFilter, AssetList
 from rdflib import RDF, Literal, Namespace
 
 from cognite.neat.constants import DEFAULT_NAMESPACE
@@ -21,17 +22,28 @@ class AssetsExtractor(BaseExtractor):
         namespace (Namespace, optional): The namespace to use. Defaults to DEFAULT_NAMESPACE.
         to_type (Callable[[Asset], str | None], optional): A function to convert an asset to a type. Defaults to None.
             If None or if the function returns None, the asset will be set to the default type "Asset".
+        total (int, optional): The total number of assets to load. If passed, you will get a progress bar if rich
+            is installed. Defaults to None.
+        limit (int, optional): The maximal number of assets to load. Defaults to None. This is typically used for
+            testing setup of the extractor. For example, if you are extracting 100 000 assets, you might want to
+            limit the extraction to 1000 assets to test the setup.
     """
+
+    _SPACE_PATTERN = re.compile(r"\s+")
 
     def __init__(
         self,
         assets: Iterable[Asset],
         namespace: Namespace | None = None,
         to_type: Callable[[Asset], str | None] | None = None,
+        total: int | None = None,
+        limit: int | None = None,
     ):
         self.namespace = namespace or DEFAULT_NAMESPACE
         self.assets = assets
         self.to_type = to_type
+        self.total = total
+        self.limit = min(limit, total) if limit and total else limit
 
     @classmethod
     def from_dataset(
@@ -40,8 +52,17 @@ class AssetsExtractor(BaseExtractor):
         data_set_external_id: str,
         namespace: Namespace | None = None,
         to_type: Callable[[Asset], str | None] | None = None,
+        limit: int | None = None,
     ):
-        return cls(cast(Iterable[Asset], client.assets(data_set_external_ids=data_set_external_id)), namespace, to_type)
+        total = client.assets.aggregate_count(filter=AssetFilter(data_set_ids=[{"externalId": data_set_external_id}]))
+
+        return cls(
+            cast(Iterable[Asset], client.assets(data_set_external_ids=data_set_external_id)),
+            namespace,
+            to_type,
+            total,
+            limit,
+        )
 
     @classmethod
     def from_hierarchy(
@@ -50,21 +71,45 @@ class AssetsExtractor(BaseExtractor):
         root_asset_external_id: str,
         namespace: Namespace | None = None,
         to_type: Callable[[Asset], str | None] | None = None,
+        limit: int | None = None,
     ):
+        total = client.assets.aggregate_count(
+            filter=AssetFilter(asset_subtree_ids=[{"externalId": root_asset_external_id}])
+        )
+
         return cls(
-            cast(Iterable[Asset], client.assets(asset_subtree_external_ids=root_asset_external_id)), namespace, to_type
+            cast(Iterable[Asset], client.assets(asset_subtree_external_ids=root_asset_external_id)),
+            namespace,
+            to_type,
+            total,
+            limit,
         )
 
     @classmethod
     def from_file(
-        cls, file_path: str, namespace: Namespace | None = None, to_type: Callable[[Asset], str] | None = None
+        cls,
+        file_path: str,
+        namespace: Namespace | None = None,
+        to_type: Callable[[Asset], str] | None = None,
+        limit: int | None = None,
     ):
-        return cls(AssetList.load(Path(file_path).read_text()), namespace, to_type)
+        return cls(AssetList.load(Path(file_path).read_text()), namespace, to_type, limit)
 
     def extract(self) -> Iterable[Triple]:
         """Extracts an asset with the given asset_id."""
-        for asset in self.assets:
+        if self.total:
+            try:
+                from rich.progress import track
+            except ModuleNotFoundError:
+                to_iterate = self.assets
+            else:
+                to_iterate = track(self.assets, total=self.limit or self.total, description="Extracting Assets")
+        else:
+            to_iterate = self.assets
+        for no, asset in enumerate(to_iterate):
             yield from self._asset2triples(asset, self.namespace)
+            if self.limit and no >= self.limit:
+                break
 
     def _asset2triples(self, asset: Asset, namespace: Namespace) -> list[Triple]:
         """Converts an asset to triples."""
@@ -74,6 +119,8 @@ class AssetsExtractor(BaseExtractor):
         type_ = "Asset"
         if self.to_type:
             type_ = self.to_type(asset) or type_
+        type_ = self._SPACE_PATTERN.sub("_", type_)
+
         triples: list[Triple] = [(id_, RDF.type, namespace[type_])]
 
         # Create attributes
