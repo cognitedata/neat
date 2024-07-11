6f944640fcd4802d6397c0fcf8135e62fcff0e99
anders-albert <anders0albert@gmail.com>
2024-07-11 07:41:54 +0200

    style: naming

--- tests/tests_unit/rules/test_models/test_dms_architect_rules.py
+++ tests/tests_unit/rules/test_models/test_dms_architect_rules.py
@@ -1552,14 +1552,14 @@ class TestDMSRules:
 
         assert sorted(actual_issues) == sorted(expected_issues)
 
-    def test_add_reference(self) -> None:
+    def test_create_reference(self) -> None:
         dms_rules = car.CAR_RULES.as_dms_architect_rules()
 
         dms_rules.create_reference(car.BASE_MODEL, {"Manufacturer": "Entity", "Color": "Entity"})
 
         schema = dms_rules.as_schema()
         view_by_external_id = {view.external_id: view for view in schema.views.values()}
-        # The Manufacturer and Color view only has one property (name) and this is
+        # The Manufacturer and Color view only has one property, name, and this is
         # now expected to point to the Entity container in the base model.
         manufacturer_view = view_by_external_id["Manufacturer"]
         assert manufacturer_view.referenced_containers() == {dm.ContainerId(car.BASE_MODEL.metadata.space, "Entity")}
