5d5cca87a6ba4f54fe1d4b77d7a05371ea8ae5b0
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-11 11:30:24 +0200

    update test name

--- tests/tests_unit/graph/test_loaders/test_dms_loader.py
+++ tests/tests_unit/graph/test_loaders/test_dms_loader.py
@@ -1,6 +1,3 @@
-from cognite.client.data_classes import AssetList
-from cognite.client.testing import monkeypatch_cognite_client
-
 from cognite.neat.graph.extractors import AssetsExtractor
 from cognite.neat.graph.loaders import DMSLoader
 from cognite.neat.graph.stores import NeatGraphStore
@@ -8,12 +5,7 @@ from cognite.neat.rules.importers import InferenceImporter
 from tests.config import CLASSIC_CDF_EXTRACTOR_DATA
 
 
-def test_asset_extractor_with_packed_metadata():
-    with monkeypatch_cognite_client() as client_mock:
-        assets = AssetList.load((CLASSIC_CDF_EXTRACTOR_DATA / "assets.yaml").read_text())
-        client_mock.assets.aggregate_count.return_value = len(assets)
-        client_mock.assets.return_value = assets
-
+def test_metadata_as_json_filed():
     store = NeatGraphStore.from_memory_store()
     store.write(AssetsExtractor.from_file(CLASSIC_CDF_EXTRACTOR_DATA / "assets.yaml", unpack_metadata=False))
 
@@ -27,4 +19,5 @@ def test_asset_extractor_with_packed_metadata():
     loader = DMSLoader.from_rules(dms_rules, store, dms_rules.metadata.space)
     instances = {instance.external_id: instance for instance in loader._load()}
 
+    # metadata not unpacked but kept as Json obj
     assert isinstance(instances["Asset_4288662884680989"].sources[0].properties["metadata"], dict)
