b7b55a0908ad7ddf5cdda025b910d4e8895bf467
anders-albert <anders0albert@gmail.com>
2024-07-16 16:24:06 +0200

    refactor: removed unused

--- cognite/neat/utils/auth.py
+++ cognite/neat/utils/auth.py
@@ -271,19 +271,6 @@ def _prompt_cluster_and_project() -> _EnvironmentVariables:
     return _EnvironmentVariables(cluster, project)
 
 
-def _is_notebook() -> bool:
-    try:
-        shell = get_ipython().__class__.__name__  # type: ignore[name-defined]
-        if shell == "ZMQInteractiveShell":
-            return True  # Jupyter notebook or qtconsole
-        elif shell == "TerminalInteractiveShell":
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False  # Probably standard Python interpreter
-
-
 def _repo_root() -> Path | None:
     with suppress(Exception):
         result = subprocess.run("git rev-parse --show-toplevel".split(), stdout=subprocess.PIPE)
