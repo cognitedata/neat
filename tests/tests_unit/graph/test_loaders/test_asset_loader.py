ffef120422938b0f84dcc7192b63055a36a462f6
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-18 13:58:39 +0200

    improve naming

--- tests/tests_unit/graph/test_loaders/test_asset_loader.py
+++ tests/tests_unit/graph/test_loaders/test_asset_loader.py
@@ -5,7 +5,7 @@ from cognite.neat.graph.extractors import RdfFileExtractor
 from cognite.neat.graph.issues.loader import InvalidInstanceError
 from cognite.neat.graph.loaders import AssetLoader
 from cognite.neat.graph.stores import NeatGraphStore
-from cognite.neat.graph.transformers import AddAllReferences
+from cognite.neat.graph.transformers import AddSelfReferenceProperty
 
 
 def test_generation_of_assets(asset_rules):
@@ -13,7 +13,7 @@ def test_generation_of_assets(asset_rules):
     asset_store.write(RdfFileExtractor(nordic44_knowledge_graph, base_uri=URIRef("http://purl.org/nordic44#")))
 
     asset_store.add_rules(asset_rules.as_information_rules())
-    asset_store.transform(AddAllReferences(rules=asset_store.rules))
+    asset_store.transform(AddSelfReferenceProperty(rules=asset_store.rules))
 
     loader = AssetLoader(asset_store, asset_rules, 1983)
     result = list(loader.load())
