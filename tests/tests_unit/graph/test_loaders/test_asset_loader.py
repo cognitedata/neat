9785828a3c91735c736690ec10a279478792963e
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-22 13:30:38 +0200

    fix tests

--- tests/tests_unit/graph/test_loaders/test_asset_loader.py
+++ tests/tests_unit/graph/test_loaders/test_asset_loader.py
@@ -7,8 +7,6 @@ from cognite.neat.graph.issues.loader import InvalidInstanceError
 from cognite.neat.graph.loaders import AssetLoader
 from cognite.neat.graph.stores import NeatGraphStore
 from cognite.neat.graph.transformers import AddSelfReferenceProperty
-from cognite.neat.rules.models.asset._rules import AssetClass
-from cognite.neat.rules.models.entities import AssetEntity, ClassEntity
 
 
 @pytest.fixture()
@@ -24,32 +22,6 @@ def asset_store(asset_rules) -> NeatGraphStore:
 
 class TestAssetLoader:
     def test_generation_of_assets_no_errors(self, asset_rules, asset_store):
-        loader = AssetLoader(asset_store, asset_rules, 1983)
-        result = list(loader.load())
-
-        assets = []
-        errors = []
-        for r in result:
-            if not isinstance(r, InvalidInstanceError):
-                assets.append(r)
-            else:
-                errors.append(r)
-
-        assert len(errors) == 0
-        assert len(assets) == 452
-
-    def test_generation_of_assets_with_orphanage_errors(self, asset_rules, asset_store):
-        asset_rules = asset_rules.model_copy(deep=True)
-
-        # a bit nasty update of rules to produce errors and orphanage
-        asset_rules.properties.data[0].transformation = asset_store.rules.properties.data[0].transformation
-        asset_rules.properties.data[1].transformation = asset_store.rules.properties.data[1].transformation
-        asset_rules.properties.data[3].value_type = ClassEntity(prefix="cim", suffix="ConnectivityNode")
-        asset_rules.properties.data[3].implementation = [AssetEntity(property="parentExternalId")]
-        asset_rules.classes.data += [AssetClass(class_=ClassEntity(prefix="cim", suffix="ConnectivityNode"))]
-
-        asset_store.add_rules(asset_rules.as_information_rules())
-
         loader = AssetLoader(asset_store, asset_rules, 1983, use_orphanage=True)
         result = list(loader.load())
 
@@ -61,7 +33,28 @@ class TestAssetLoader:
             else:
                 errors.append(r)
 
-        assert len(errors) == 541
-        assert len(assets) == 453
+        assert len(errors) == 0
+        assert len(assets) == 631
+
+    def test_generation_of_assets_with_orphanage_errors(self, asset_rules, asset_store):
+        asset_store.graph.remove(
+            (
+                URIRef("http://purl.org/nordic44#_f17695a9-9aeb-11e5-91da-b8763fd99c5f"),
+                None,
+                None,
+            )
+        )
+        loader = AssetLoader(asset_store, asset_rules, 1983, use_orphanage=True)
+        result = list(loader.load())
+
+        assets = []
+        errors = []
+        for r in result:
+            if not isinstance(r, InvalidInstanceError):
+                assets.append(r)
+            else:
+                errors.append(r)
+
+        assert len(errors) == 13
+        assert len(assets) == 630
         assert assets[0] == loader.orphanage
-        assert assets[1].parent_external_id == assets[0].external_id
