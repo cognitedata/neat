61c1d239ca4f08493de321534e71ede2c2f9359a
anders-albert <anders0albert@gmail.com>
2024-07-23 13:30:24 +0200

    refactor: created base clas for classic CDF extractors

--- cognite/neat/graph/extractors/_classic_cdf/_assets.py
+++ cognite/neat/graph/extractors/_classic_cdf/_assets.py
@@ -1,5 +1,3 @@
-import json
-import re
 from collections.abc import Callable, Iterable
 from datetime import datetime, timezone
 from pathlib import Path
@@ -9,17 +7,17 @@ from cognite.client import CogniteClient
 from cognite.client.data_classes import Asset, AssetFilter, AssetList
 from rdflib import RDF, Literal, Namespace
 
-from cognite.neat.constants import DEFAULT_NAMESPACE
-from cognite.neat.graph.extractors._base import BaseExtractor
 from cognite.neat.graph.models import Triple
-from cognite.neat.utils.auxiliary import create_sha256_hash, string_to_ideal_type
+from cognite.neat.utils.auxiliary import create_sha256_hash
+
+from ._base import ClassicCDFExtractor
 
 
-class AssetsExtractor(BaseExtractor):
+class AssetsExtractor(ClassicCDFExtractor[Asset]):
     """Extract data from Cognite Data Fusions Assets into Neat.
 
     Args:
-        assets (Iterable[Asset]): An iterable of assets.
+        items (Iterable[Asset]): An iterable of assets.
         namespace (Namespace, optional): The namespace to use. Defaults to DEFAULT_NAMESPACE.
         to_type (Callable[[Asset], str | None], optional): A function to convert an asset to a type. Defaults to None.
             If None or if the function returns None, the asset will be set to the default type "Asset".
@@ -34,25 +32,7 @@ class AssetsExtractor(BaseExtractor):
             metadata. Defaults to frozenset({"nan", "null", "none", ""}).
     """
 
-    _SPACE_PATTERN = re.compile(r"\s+")
-
-    def __init__(
-        self,
-        assets: Iterable[Asset],
-        namespace: Namespace | None = None,
-        to_type: Callable[[Asset], str | None] | None = None,
-        total: int | None = None,
-        limit: int | None = None,
-        unpack_metadata: bool = True,
-        skip_metadata_values: set[str] | frozenset[str] | None = frozenset({"nan", "null", "none", ""}),
-    ):
-        self.namespace = namespace or DEFAULT_NAMESPACE
-        self.assets = assets
-        self.to_type = to_type
-        self.total = total
-        self.limit = min(limit, total) if limit and total else limit
-        self.unpack_metadata = unpack_metadata
-        self.skip_metadata_values = skip_metadata_values
+    _default_rdf_type = "Asset"
 
     @classmethod
     def from_dataset(
@@ -121,35 +101,11 @@ class AssetsExtractor(BaseExtractor):
             unpack_metadata=unpack_metadata,
         )
 
-    def extract(self) -> Iterable[Triple]:
-        """Extracts an asset with the given asset_id."""
-        if self.total:
-            try:
-                from rich.progress import track
-            except ModuleNotFoundError:
-                to_iterate = self.assets
-            else:
-                to_iterate = track(
-                    self.assets,
-                    total=self.limit or self.total,
-                    description="Extracting Assets",
-                )
-        else:
-            to_iterate = self.assets
-        for no, asset in enumerate(to_iterate):
-            yield from self._asset2triples(asset)
-            if self.limit and no >= self.limit:
-                break
-
-    def _asset2triples(self, asset: Asset) -> list[Triple]:
+    def _item2triples(self, asset: Asset) -> list[Triple]:
         """Converts an asset to triples."""
         id_ = self.namespace[f"Asset_{asset.id}"]
 
-        # Set rdf type
-        type_ = "Asset"
-        if self.to_type:
-            type_ = self.to_type(asset) or type_
-        type_ = self._SPACE_PATTERN.sub("_", type_)
+        type_ = self._get_rdf_type(asset)
 
         triples: list[Triple] = [(id_, RDF.type, self.namespace[type_])]
 
@@ -195,20 +151,7 @@ class AssetsExtractor(BaseExtractor):
                 )
 
         if asset.metadata:
-            if self.unpack_metadata:
-                for key, value in asset.metadata.items():
-                    if value and (
-                        self.skip_metadata_values is None or value.casefold() not in self.skip_metadata_values
-                    ):
-                        triples.append(
-                            (
-                                id_,
-                                self.namespace[key],
-                                Literal(string_to_ideal_type(value)),
-                            )
-                        )
-            else:
-                triples.append((id_, self.namespace.metadata, Literal(json.dumps(asset.metadata))))
+            triples.extend(self._metadata_to_triples(id_, asset.metadata))
 
         # Create connections:
         if asset.parent_id:
---
+++ cognite/neat/graph/extractors/_classic_cdf/_base.py
@@ -0,0 +1,100 @@
+import json
+import re
+from abc import ABC, abstractmethod
+from collections.abc import Callable, Iterable
+from typing import Generic, TypeVar
+
+from cognite.client.data_classes._base import CogniteResource
+from rdflib import Literal, Namespace, URIRef
+
+from cognite.neat.constants import DEFAULT_NAMESPACE
+from cognite.neat.graph.extractors._base import BaseExtractor
+from cognite.neat.graph.models import Triple
+from cognite.neat.utils.auxiliary import string_to_ideal_type
+
+T_CogniteResource = TypeVar("T_CogniteResource", bound=CogniteResource)
+
+
+class ClassicCDFExtractor(BaseExtractor, ABC, Generic[T_CogniteResource]):
+    """This is the Base Extractor for all classic CDF resources.
+
+    A classic resource is recognized in that it has a metadata atribute of type dict[str, str].
+
+    Args:
+        items (Iterable[T_CogniteResource]): An iterable of classic resource.
+        namespace (Namespace, optional): The namespace to use. Defaults to DEFAULT_NAMESPACE.
+        to_type (Callable[[T_CogniteResource], str | None], optional): A function to convert an item to a type.
+            Defaults to None. If None or if the function returns None, the asset will be set to the default type.
+        total (int, optional): The total number of items to load. If passed, you will get a progress bar if rich
+            is installed. Defaults to None.
+        limit (int, optional): The maximal number of items to load. Defaults to None. This is typically used for
+            testing setup of the extractor. For example, if you are extracting 100 000 assets, you might want to
+            limit the extraction to 1000 assets to test the setup.
+        unpack_metadata (bool, optional): Whether to unpack metadata. Defaults to False, which yields the metadata as
+            a JSON string.
+        skip_metadata_values (set[str] | frozenset[str] | None, optional): If you are unpacking metadata, then
+           values in this set will be skipped.
+    """
+
+    _default_rdf_type: str
+    _SPACE_PATTERN = re.compile(r"\s+")
+
+    def __init__(
+        self,
+        items: Iterable[T_CogniteResource],
+        namespace: Namespace | None = None,
+        to_type: Callable[[T_CogniteResource], str | None] | None = None,
+        total: int | None = None,
+        limit: int | None = None,
+        unpack_metadata: bool = True,
+        skip_metadata_values: set[str] | frozenset[str] | None = frozenset({"nan", "null", "none", ""}),
+    ):
+        self.namespace = namespace or DEFAULT_NAMESPACE
+        self.items = items
+        self.to_type = to_type
+        self.total = total
+        self.limit = min(limit, total) if limit and total else limit
+        self.unpack_metadata = unpack_metadata
+        self.skip_metadata_values = skip_metadata_values
+
+    def extract(self) -> Iterable[Triple]:
+        """Extracts an asset with the given asset_id."""
+        if self.total:
+            try:
+                from rich.progress import track
+            except ModuleNotFoundError:
+                to_iterate = self.items
+            else:
+                to_iterate = track(
+                    self.items,
+                    total=self.limit or self.total,
+                    description=f"Extracting {type(self).__name__.removesuffix('Extractor')}",
+                )
+        else:
+            to_iterate = self.items
+        for no, asset in enumerate(to_iterate):
+            yield from self._item2triples(asset)
+            if self.limit and no >= self.limit:
+                break
+
+    @abstractmethod
+    def _item2triples(self, item: T_CogniteResource) -> list[Triple]:
+        raise NotImplementedError()
+
+    def _metadata_to_triples(self, id_: URIRef, metadata: dict[str, str]) -> Iterable[Triple]:
+        if self.unpack_metadata:
+            for key, value in metadata.items():
+                if value and (self.skip_metadata_values is None or value.casefold() not in self.skip_metadata_values):
+                    yield (
+                        id_,
+                        self.namespace[key],
+                        Literal(string_to_ideal_type(value)),
+                    )
+        else:
+            yield id_, self.namespace.metadata, Literal(json.dumps(metadata))
+
+    def _get_rdf_type(self, item: T_CogniteResource) -> str:
+        type_ = self._default_rdf_type
+        if self.to_type:
+            type_ = self.to_type(item) or type_
+        return self._SPACE_PATTERN.sub("_", type_)
