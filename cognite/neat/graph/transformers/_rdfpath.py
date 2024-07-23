e81c36e8c5b058c2c9bf53405e51bcb39d7891ce
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2024-07-23 18:21:27 +0200

    Update _rdfpath.py

--- cognite/neat/graph/transformers/_rdfpath.py
+++ cognite/neat/graph/transformers/_rdfpath.py
@@ -14,9 +14,12 @@ class ReduceHopTraversal(BaseTransformer):
 
 
 class AddSelfReferenceProperty(BaseTransformer):
-    description: str = "Adds property that contains id of reference to all references of given class in Rules"
+    description: str = (
+        "Adds property that contains id of reference to all references of given class in Rules"
+    )
     _use_only_once: bool = True
     _need_changes = frozenset({})
+    _ref_template: str = """SELECT ?s WHERE {{?s a <{type_}>}}"""
 
     def __init__(
         self,
@@ -30,9 +33,15 @@ class AddSelfReferenceProperty(BaseTransformer):
             prefix = property_.transformation.traversal.class_.prefix
             suffix = property_.transformation.traversal.class_.suffix
 
-            namespace = self.rules.prefixes[prefix] if prefix in self.rules.prefixes else self.rules.metadata.namespace
+            namespace = (
+                self.rules.prefixes[prefix]
+                if prefix in self.rules.prefixes
+                else self.rules.metadata.namespace
+            )
 
-            for reference in graph.subjects(RDF.type, namespace[suffix]):
+            for (reference,) in graph.query(
+                self._ref_template.format(type_=namespace[suffix])
+            ):
                 graph.add(
                     (
                         reference,
