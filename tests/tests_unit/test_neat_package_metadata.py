6f1ce69dcae7b3e0ae66d4956fc8aebb48f38a25
anders-albert <anders0albert@gmail.com>
2024-07-23 14:32:25 +0200

    tests: fix bad tests

--- tests/tests_unit/test_neat_package_metadata.py
+++ tests/tests_unit/test_neat_package_metadata.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from pathlib import Path
 
 from cognite import neat
 from tests.config import PYPROJECT_TOML, ROOT
@@ -35,6 +36,16 @@ def test_consistent_version_variables():
 
 
 def test_no_spaces_in_sub_folders() -> None:
-    name_by_location = {path: path.name for path in (ROOT / "cognite").rglob("*") if path.is_dir() and " " in path.name}
+    name_by_location: dict[Path, str] = {}
+    to_check = [ROOT / "cognite"]
+    while to_check:
+        current = to_check.pop()
+        for path in current.iterdir():
+            if path.is_dir():
+                if path.name not in {"node_modules"}:
+                    to_check.append(path)
+                if " " in path.name:
+                    name_by_location[path] = path.name
+                    raise ValueError(f"Subfolder with spaces found: {path}")
 
     assert not name_by_location, f"Subfolders with spaces found: {name_by_location}"
