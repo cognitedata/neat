228a7208f714b414c44b30add77a7cb896812214
nikokaoja <nikola.vasiljevic@cognite.com>
2024-07-24 14:30:20 +0200

    make joined rdf based importer module

--- cognite/neat/rules/importers/__init__.py
+++ cognite/neat/rules/importers/__init__.py
@@ -1,8 +1,8 @@
 from ._base import BaseImporter
 from ._dms2rules import DMSImporter
 from ._dtdl2rules import DTDLImporter
-from ._inference2rules import InferenceImporter
 from ._rdf._imf2rules import IMFImporter
+from ._rdf._inference2rules import InferenceImporter
 from ._rdf._owl2rules import OWLImporter
 from ._spreadsheet2rules import ExcelImporter, GoogleSheetImporter
 from ._yaml2rules import YAMLImporter
