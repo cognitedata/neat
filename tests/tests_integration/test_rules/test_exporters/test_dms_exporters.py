7d83b94feade87476eb8b22139fa1cdedd8735a0
anders-albert <anders0albert@gmail.com>
2024-09-08 10:05:51 +0200

    tests: updated tests

--- tests/tests_integration/test_rules/test_exporters/test_dms_exporters.py
+++ tests/tests_integration/test_rules/test_exporters/test_dms_exporters.py
@@ -327,9 +327,9 @@ class TestDMSExporters:
             if view["Implements"]:
                 view["Implements"] = view["Implements"].replace("power", new_enterprise_space)
         for container in itertools.chain(
-            dumped.get("Containers", []),
-            dumped["Last"].get("Containers", []),
-            dumped["Reference"].get("Containers", []),
+            dumped.get("Containers", []) or [],
+            dumped["Last"].get("Containers", []) or [],
+            dumped["Reference"].get("Containers", []) or [],
         ):
             if container["Reference"]:
                 container["Reference"] = container["Reference"].replace("power", new_enterprise_space)
