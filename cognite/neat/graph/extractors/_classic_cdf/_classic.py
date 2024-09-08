43c51f3cee72518e03a820254e998c8f600a3f57
anders-albert <anders0albert@gmail.com>
2024-09-08 17:48:29 +0200

    refactor: included target of relationships as well

--- cognite/neat/graph/extractors/_classic_cdf/_classic.py
+++ cognite/neat/graph/extractors/_classic_cdf/_classic.py
@@ -38,7 +38,8 @@ class ClassicExtractor(BaseExtractor):
         self._data_set_external_id = data_set_external_id
         self._namespace = namespace or DEFAULT_NAMESPACE
 
-        self._resource_external_ids_by_type: dict[str, set[str]] = defaultdict(set)
+        self._source_external_ids_by_type: dict[Prefix, set[str]] = defaultdict(set)
+        self._target_external_ids_by_type: dict[Prefix, set[str]] = defaultdict(set)
         self._labels: set[str] = set()
         self._data_set_ids: set[int] = set()
 
@@ -65,7 +66,7 @@ class ClassicExtractor(BaseExtractor):
 
             yield from self._extract_subextractor(extractor, resource_type)
 
-        for resource_type, source_external_ids in self._resource_external_ids_by_type.items():
+        for resource_type, source_external_ids in self._source_external_ids_by_type.items():
             to_iterate: Iterable = chunker(list(source_external_ids), chunk_size=1000)
             try:
                 from rich.progress import track
@@ -82,9 +83,40 @@ class ClassicExtractor(BaseExtractor):
                 relationship_iterator = self._client.relationships(
                     source_external_ids=list(chunk), source_types=[resource_type.removesuffix("_")]
                 )
-                yield from RelationshipsExtractor(
+                for triple in RelationshipsExtractor(
                     relationship_iterator, self._namespace, unpack_metadata=False
-                ).extract()
+                ).extract():
+                    if triple[1] == self._namespace.target_external_id:
+                        external_id = remove_namespace_from_uri(triple[2])
+                        if external_id not in self._source_external_ids_by_type[resource_type]:
+                            self._target_external_ids_by_type[resource_type].add(external_id)
+                    yield triple
+
+        for resource_type, target_external_ids in self._target_external_ids_by_type.items():
+            to_iterate = chunker(list(target_external_ids), chunk_size=1000)
+            try:
+                from rich.progress import track
+            except ModuleNotFoundError:
+                ...
+            else:
+                to_iterate = track(
+                    to_iterate,
+                    total=(len(target_external_ids) // 1000) + 1,
+                    description=f"Extracting Target {resource_type.removesuffix('_')}",
+                )
+
+            api, extractor_cls = {
+                Prefix.asset: (self._client.assets, AssetsExtractor),
+                Prefix.time_series: (self._client.time_series, TimeSeriesExtractor),
+                Prefix.sequence: (self._client.sequences, SequencesExtractor),
+                Prefix.event: (self._client.events, EventsExtractor),
+                Prefix.file: (self._client.files, FilesExtractor),
+            }[resource_type]
+
+            for chunk in to_iterate:
+                resource_iterator = api.retrieve_multiple(external_ids=list(chunk), ignore_unknown_ids=True)  # type: ignore[attr-defined]
+                extractor = extractor_cls(resource_iterator, self._namespace, unpack_metadata=False)
+                yield from self._extract_subextractor(extractor, resource_type)
 
         label_iterator = self._client.labels.retrieve(external_id=list(self._labels), ignore_unknown_ids=True)
         yield from LabelsExtractor(label_iterator, self._namespace, total=len(label_iterator)).extract()
@@ -93,10 +125,12 @@ class ClassicExtractor(BaseExtractor):
             data_set_iterator, self._namespace, total=len(data_set_iterator), unpack_metadata=False
         ).extract()
 
-    def _extract_subextractor(self, extractor: _ClassicCDFBaseExtractor, resource_type: str) -> Iterable[Triple]:
+    def _extract_subextractor(
+        self, extractor: _ClassicCDFBaseExtractor, resource_type: Prefix | None = None
+    ) -> Iterable[Triple]:
         for triple in extractor.extract():
-            if triple[1] == self._namespace.external_id:
-                self._resource_external_ids_by_type[resource_type].add(remove_namespace_from_uri(triple[2]))
+            if triple[1] == self._namespace.external_id and resource_type is not None:
+                self._source_external_ids_by_type[resource_type].add(remove_namespace_from_uri(triple[2]))
             elif triple[1] == self._namespace.label:
                 self._labels.add(remove_namespace_from_uri(triple[2]).removeprefix(Prefix.label))
             elif triple[1] == self._namespace.dataset:
