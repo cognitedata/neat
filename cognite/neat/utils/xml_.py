19dbfb325ba794d46c4a9fcc9926b5d7adc8a7ef
Juliamg <julia.graham@live.no>
2024-09-09 15:03:56 +0200

    fix get children method to only extract direct children

--- cognite/neat/utils/xml_.py
+++ cognite/neat/utils/xml_.py
@@ -20,7 +20,7 @@ def iterate_tree(node: Element, skip_root: bool = False) -> Generator:
 def get_children(
     element: Element, child_tag: str, ignore_namespace: bool = False, no_children: int = -1
 ) -> Element | list[Element]:
-    """Get children of an XML element.
+    """Get direct children of an XML element.
 
     Args:
         element: XML element to get children from.
@@ -32,7 +32,7 @@ def get_children(
         List of XML elements if no_children > 1, otherwise XML element.
     """
     if ignore_namespace:
-        children = element.findall(f".//{{*}}{child_tag}")
+        children = element.findall(f".{{*}}{child_tag}")
     else:
-        children = element.findall(f".//{child_tag}")
+        children = element.findall(f".{child_tag}")
     return children[:no_children] if no_children > 0 else children
