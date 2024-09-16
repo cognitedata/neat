b2d779a24f2c59a40077f47755e3cd6af778f91e
nikokaoja <nikola.vasiljevic@cognite.com>
2024-09-16 13:33:44 +0200

    add test

--- tests/tests_unit/graph/test_loaders/test_dms_loader.py
+++ tests/tests_unit/graph/test_loaders/test_dms_loader.py
@@ -1,9 +1,10 @@
-from cognite.neat.graph.extractors import AssetsExtractor
+from cognite.neat.graph.extractors import AssetsExtractor, RdfFileExtractor
 from cognite.neat.graph.loaders import DMSLoader
-from cognite.neat.rules.importers import InferenceImporter
+from cognite.neat.rules.examples import imf_info_rules
+from cognite.neat.rules.importers import ExcelImporter, InferenceImporter
 from cognite.neat.rules.transformers import ImporterPipeline, InformationToDMS
 from cognite.neat.store import NeatGraphStore
-from tests.config import CLASSIC_CDF_EXTRACTOR_DATA
+from tests.config import CLASSIC_CDF_EXTRACTOR_DATA, IMF_EXAMPLE
 
 
 def test_metadata_as_json_filed():
@@ -22,3 +23,21 @@ def test_metadata_as_json_filed():
 
     # metadata not unpacked but kept as Json obj
     assert isinstance(instances["Asset_4288662884680989"].sources[0].properties["metadata"], dict)
+
+
+def test_imf_attribute_nodes():
+    # this test also accounts for renaming of classes
+    # as well omitting to remove namespace from values if
+    # properties are not specified to be object properties
+
+    info_rules = ImporterPipeline.verify(ExcelImporter(imf_info_rules))
+    dms_rules = InformationToDMS().transform(info_rules).rules
+
+    store = NeatGraphStore.from_oxi_store(rules=info_rules)
+    store.write(RdfFileExtractor(IMF_EXAMPLE, mime_type="text/turtle"))
+
+    loader = DMSLoader.from_rules(dms_rules, store, instance_space="knowledge")
+    knowledge_nodes = list(loader._load())
+
+    assert len(knowledge_nodes) == 56
+    assert knowledge_nodes[0].sources[0].properties["predicate"].startswith("http://")
