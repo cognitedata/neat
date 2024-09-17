b4ed1ae7712978b6bd55c0d247ad45e2a43b6577
anders-albert <anders0albert@gmail.com>
2024-09-17 16:29:42 +0200

    tests: failing tests

--- tests/tests_unit/rules/test_models/test_base_rules.py
+++ tests/tests_unit/rules/test_models/test_base_rules.py
@@ -1,8 +1,8 @@
-from cognite.neat.rules.models.information import InformationMetadata
+from cognite.neat.rules.models.information import InformationClass, InformationInputClass, InformationMetadata
 
 
 class TestBaseRules:
-    def test_strip_whitespace(
+    def test_strip_whitespace_metadata(
         self,
     ) -> None:
         meta = InformationMetadata.model_validate(
@@ -26,3 +26,14 @@ class TestBaseRules:
         assert meta.role == "information architect"
         assert meta.data_model_type == "enterprise"
         assert meta.creator == ["Me", "Myself", "I"]
+
+    def test_strip_whitespace_input_class(self) -> None:
+        row = InformationInputClass(
+            class_="  My Class  ", name="  My Class Name  ", description="  My Class Description  "
+        )
+
+        class_ = InformationClass.model_validate(row.dump(default_prefix="neat"))
+
+        assert class_.class_.suffix == "MyClass"
+        assert class_.name == "My Class Name"
+        assert class_.description == "My Class Description"
