27982dfa52be1a278d9795d79c2a787d27785158
anders-albert <anders0albert@gmail.com>
2024-09-17 16:40:14 +0200

    fix: strip the enums

--- cognite/neat/rules/models/information/_rules.py
+++ cognite/neat/rules/models/information/_rules.py
@@ -98,15 +98,15 @@ class InformationMetadata(BaseMetadata):
             return self
         return self
 
-    @field_validator("schema_", mode="before")
+    @field_validator("schema_", mode="plain")
     def as_enum_schema(cls, value: str) -> SchemaCompleteness:
         return SchemaCompleteness(value.strip())
 
-    @field_validator("extension", mode="before")
+    @field_validator("extension", mode="plain")
     def as_enum_extension(cls, value: str) -> ExtensionCategory:
         return ExtensionCategory(value.strip())
 
-    @field_validator("data_model_type", mode="before")
+    @field_validator("data_model_type", mode="plain")
     def as_enum_model_type(cls, value: str) -> DataModelType:
         return DataModelType(value.strip())
 
