1c8f288142e67accae3a6b2bc294f4e7a839e889
anders-albert <anders0albert@gmail.com>
2024-09-17 16:15:47 +0200

    tests: added failing tests

---
+++ tests/tests_unit/rules/test_models/test_base_rules.py
@@ -0,0 +1,28 @@
+from cognite.neat.rules.models.information import InformationMetadata
+
+
+class TestBaseRules:
+    def test_strip_whitespace(
+        self,
+    ) -> None:
+        meta = InformationMetadata.model_validate(
+            {
+                "role": "  information architect  ",
+                "dataModelType": "  enterprise ",
+                "schema": "   partial   ",
+                "prefix": "  my-prefix  ",
+                "namespace": "http://purl.org/cognite/neat/",
+                "version": "  0.1.0  ",
+                "name": "  My Data Model  ",
+                "description": "  My Data Model Description  ",
+                "created": "  2022-07-01T00:00:00Z  ",
+                "updated": "  2022-07-01T00:00:00Z  ",
+                "creator": " Me, Myself, I  ",
+            }
+        )
+
+        assert meta.prefix == "my-prefix"
+        assert meta.schema_ == "partial"
+        assert meta.role == "information architect"
+        assert meta.dataModelType == "enterprise"
+        assert meta.creator == ["Me", "Myself", "I"]
