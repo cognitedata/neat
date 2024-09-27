9e35b988c2268f527741ae027e756a04d10fea46
Juliamg <julia.graham@live.no>
2024-09-27 11:05:29 +0200

    ResolveValues transformer

--- cognite/neat/graph/transformers/_prune_graph.py
+++ cognite/neat/graph/transformers/_prune_graph.py
@@ -1,14 +1,9 @@
-import dataclasses
-from typing import Tuple
+from rdflib import RDF, Graph, Namespace, URIRef
 
-from rdflib import Graph, URIRef, RDF
 from ._base import BaseTransformer
 
+IODD = Namespace("http://www.io-link.com/IODD/2010/10/")
 
-@dataclasses.dataclass
-class NodeMapping:
-    source_node: URIRef
-    destination_node: URIRef
 
 class ResolveValues(BaseTransformer):
     """
@@ -19,7 +14,8 @@ class ResolveValues(BaseTransformer):
         Ex. ResolveValues:
 
         Graph before pruning:
-        node(A, rdf:type(Pump)) -(predicate("vendor"))> node(B, rdf:type(TextObject)) -(predicate("value"))> Literal("CompanyX")
+        node(A, rdf:type(Pump)) -(predicate("vendor"))>
+                                node(B, rdf:type(TextObject)) -(predicate("value"))> Literal("CompanyX")
 
         Graph after resolving values between nodes rd:type(Pump) and rdf:type(Disc):
 
@@ -28,16 +24,40 @@ class ResolveValues(BaseTransformer):
     Args:
     resolve_connection: List of mappings between source and destination node.
     """
+
     description: str = "Prunes the graph of specified rdf types that do not have connections to other nodes."
 
     def __init__(
         self,
-        resolve_connection: list[NodeMapping]
+        source_node_type: URIRef,
+        destination_node_type: URIRef,
+        predicate_namespace: Namespace,
+        delete_edge_node: bool = True,
     ):
-        self.resolve_connection = resolve_connection
+        self.source_node_type = source_node_type
+        self.destination_node_type = destination_node_type
+        self.predicate_namespace = predicate_namespace
+        self.delete_edge_node = delete_edge_node
 
     def transform(self, graph: Graph) -> None:
-        ...
+        source_subjects = graph.subjects(object=self.source_node_type, predicate=RDF.type)
+        destination_subjects = [
+            subject for subject in graph.subjects(object=self.destination_node_type, predicate=RDF.type)
+        ]
+
+        triples_to_delete: list[tuple] = []
+
+        for subject in source_subjects:
+            for predicate, object in graph.predicate_objects(subject=subject, unique=False):
+                if object in destination_subjects:
+                    if value := graph.value(subject=object, predicate=self.predicate_namespace.value, object=None):
+                        # Create new connection from source subject to value
+                        graph.add((subject, predicate, value))
+                        triples_to_delete.append((object, RDF.type, self.destination_node_type))
+
+        if self.delete_edge_node:
+            for delete_triple in triples_to_delete:
+                graph.remove(triple=delete_triple)
 
 
 class PruneDanglingNodes(BaseTransformer):
@@ -59,23 +79,22 @@ class PruneDanglingNodes(BaseTransformer):
     Args:
         prune_type: list of RDF types to prune from the Graph
     """
+
     description: str = "Prunes the graph of specified rdf types that do not have connections to other nodes."
 
     def __init__(
         self,
         node_prune_types: list[URIRef],
     ):
-
         self.node_prune_types = node_prune_types
 
     def transform(self, graph: Graph) -> None:
-
-        triples_to_delete: list[Tuple] = []
+        triples_to_delete: list[tuple] = []
 
         for object_type in self.node_prune_types:
-            print("hei?")
             for object, predicate, subject in graph.triples(triple=(None, RDF.type, object_type)):
-                # Check if Node object has an edge pointing to it from another node in the Graph, i.e object is the subject of another triple
+                # Check if Node object has an edge pointing to it from another node in the Graph,
+                # i.e object is the subject of another triple
                 triple_checker = (None, None, object)
                 found = False
                 for _ in graph.triples(triple=triple_checker):
