8042d8d92e2f5175d74b9d98c80aeaa7424a9a6e
Juliamg <julia.graham@live.no>
2024-10-01 10:56:43 +0200

    rewrite pruner with sparql query

--- cognite/neat/graph/transformers/_prune_graph.py
+++ cognite/neat/graph/transformers/_prune_graph.py
@@ -6,9 +6,12 @@ from ._base import BaseTransformer
 
 class ResolveValues(BaseTransformer):
     """
-    Knowledge graph pruner and resolver. Will remove rdf triples from graph that does not have connections
-    to other nodes, and traverse graph for specified types to resolve the value in the final node and link it to
-    the source node.
+    Knowledge value resolver. Will traverse graph for specified source nodes that have a connection to
+    specified destination nodes, and then resolve the value property attatched to the destination node and link it
+    directly to the source node. The result is that the intermediate node to get to the value of the property can
+    be removed.
+    The user can also provide a flag to decide if the intermediate node should be removed or not after performing
+    the mapping from source node to the final property value.
 
         Ex. ResolveValues:
 
@@ -35,12 +38,12 @@ class ResolveValues(BaseTransformer):
         self,
         source_node_type: URIRef,
         destination_node_type: URIRef,
-        predicate_namespace: Namespace,
+        property_value: Namespace,
         delete_connecting_node: bool = True,
     ):
         self.source_node_type = source_node_type
         self.destination_node_type = destination_node_type
-        self.predicate_namespace = predicate_namespace
+        self.property_value = property_value
         self.delete_connecting_node = delete_connecting_node
 
     def transform(self, graph: Graph) -> None:
@@ -55,7 +58,7 @@ class ResolveValues(BaseTransformer):
         for subject in source_subjects:
             for predicate, object in graph.predicate_objects(subject=subject, unique=False):
                 if object in destination_subjects:
-                    if value := graph.value(subject=object, predicate=self.predicate_namespace.value):
+                    if value := graph.value(subject=object, predicate=self.property_value.value):
                         # Create new connection from source subject to value
                         graph.add((subject, predicate, value))
                         triples_to_delete.append((object, RDF.type, self.destination_node_type))
@@ -95,21 +98,17 @@ class PruneDanglingNodes(BaseTransformer):
         node_prune_types: list[URIRef],
     ):
         self.node_prune_types = node_prune_types
+        self._query_template = """
+                SELECT ?subject
+                WHERE {{
+                    ?subject a <{rdf_type}> .
+                    FILTER NOT EXISTS {{ ?s ?p ?subject }}
+                }}
+        """
 
     def transform(self, graph: Graph) -> None:
-        triples_to_delete: list[tuple[Node, Node, Node]] = []
-
         for object_type in self.node_prune_types:
-            for object, predicate, subject in graph.triples(triple=(None, RDF.type, object_type)):
-                # Check if Node object has an edge pointing to it from another node in the Graph,
-                # i.e. object is the subject of another triple
-                triple_checker = (None, None, object)
-                found = False
-                for _ in graph.triples(triple=triple_checker):
-                    found = True
-                    break
-                if not found:
-                    triples_to_delete.append((object, predicate, subject))
+            free_standing_nodes = list(graph.query(self._query_template.format(rdf_type=object_type)))
 
-        for delete_triple in triples_to_delete:
-            graph.remove(triple=delete_triple)
+            for node in free_standing_nodes:
+                graph.remove(triple=(node["subject"], RDF.type, object_type))
