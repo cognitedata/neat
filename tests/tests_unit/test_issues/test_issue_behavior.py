dff96f695425337932d4479e31ce6b191c442d29
anders-albert <anders0albert@gmail.com>
2024-10-19 12:48:59 +0200

    tests: failing tests

--- tests/tests_unit/test_issues/test_issue_behavior.py
+++ tests/tests_unit/test_issues/test_issue_behavior.py
@@ -1,25 +1,16 @@
-from contextlib import contextmanager
-
-import pytest
-
-from cognite.neat.issues.errors import NeatError, NeatValueError
-
-
-@contextmanager
-def _catch_error(errors: list[NeatError]) -> None:
-    try:
-        yield
-    except NeatError as e:
-        raise RuntimeError("This should not happen") from e
+from cognite.neat.issues import IssueList
+from cognite.neat.issues.errors import ResourceCreationError
+from cognite.neat.rules.transformers._verification import _handle_issues
 
 
 class TestIssues:
     def test_raise_issue_in_contextmanager(self) -> None:
         """Test that an issue is raised in the context manager."""
-        errors: list[NeatError] = []
-        with pytest.raises(RuntimeError) as _:
-            with _catch_error(errors):
-                raise NeatValueError("Test error")
+        my_error = ResourceCreationError(identifier="missing", resource_type="space", error="No CDF Connection")
+
+        errors = IssueList()
+        with _handle_issues(issues=errors):
+            raise my_error
 
         assert len(errors) == 1
-        assert str(errors[0]) == "Test error"
+        assert errors[0] == my_error
