5c054d594d9ae3e312e595a444991ea236743249
nikokaoja <nikola.vasiljevic@cognite.com>
2024-10-21 12:31:24 +0200

    added rdf based readers

--- cognite/neat/session/_read.py
+++ cognite/neat/session/_read.py
@@ -4,8 +4,9 @@ from typing import Any
 from cognite.client import CogniteClient
 
 from cognite.neat.issues import IssueList
+from cognite.neat.rules import importers
 from cognite.neat.rules._shared import ReadRules
-from cognite.neat.rules.importers import ExcelImporter
+from cognite.neat.store import NeatGraphStore
 
 from ._state import SessionState
 
@@ -16,25 +17,50 @@ class ReadAPI:
         self._verbose = verbose
         self.cdf = CDFReadAPI(state, client)
 
-    def excel(
-        self,
-        io: Any,
-    ) -> IssueList:
+    def excel(self, io: Any) -> IssueList:
+        filepath = self._return_filepath(io)
+        input_rules: ReadRules = importers.ExcelImporter(filepath).to_rules()
+        self._store_rules(io, input_rules, "Excel")
+        return input_rules.issues
+
+    def ontology(self, io: Any) -> IssueList:
+        filepath = self._return_filepath(io)
+        input_rules: ReadRules = importers.OWLImporter.from_file(filepath).to_rules()
+        self._store_rules(io, input_rules, "Ontology")
+        return input_rules.issues
+
+    def imf(self, io: Any) -> IssueList:
+        filepath = self._return_filepath(io)
+        input_rules: ReadRules = importers.IMFImporter.from_file(filepath).to_rules()
+        self._store_rules(io, input_rules, "IMF Types")
+        return input_rules.issues
+
+    def inference(self, io: Any) -> IssueList:
+        if isinstance(io, NeatGraphStore):
+            importer = importers.InferenceImporter.from_graph_store(io)
+        else:
+            importer = importers.InferenceImporter.from_file(self._return_filepath(io))
+
+        input_rules: ReadRules = importer.to_rules()
+        self._store_rules(io, input_rules, "Inference")
+        return input_rules.issues
+
+    def _return_filepath(self, io: Any) -> Path:
         if isinstance(io, str):
-            filepath = Path(io)
+            return Path(io)
         elif isinstance(io, Path):
-            filepath = io
+            return io
         else:
             raise ValueError(f"Expected str or Path, got {type(io)}")
-        input_rules: ReadRules = ExcelImporter(filepath).to_rules()
+
+    def _store_rules(self, io: Any, input_rules: ReadRules, source: str) -> None:
         if input_rules.rules:
             self._state.input_rules.append(input_rules)
             if self._verbose:
                 if input_rules.issues.has_errors:
-                    print(f"Excel file {filepath} read failed")
+                    print(f"{source} {type(io)} {io} read failed")
                 else:
-                    print(f"Excel file {filepath} read successfully")
-        return input_rules.issues
+                    print(f"{source} {type(io)} {io} read successfully")
 
 
 class CDFReadAPI:
