33d2232e4abe7da526662a72377eb409248ebe14
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2024-10-23 09:11:48 +0200

    Update cognite/neat/session/_wizard.py
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- cognite/neat/session/_wizard.py
+++ cognite/neat/session/_wizard.py
@@ -1,18 +1,24 @@
-from typing import Literal
+from collections.abc import Sequence
+from typing import Literal, get_args, TypeVar
 
-from rich.prompt import Prompt
+from rich.prompt import IntPrompt
 
 RDFFileType = Literal["Ontology", "IMF Types", "Inference"]
 NeatObjectType = Literal["Data Model", "Instances"]
 
 
-def object_wizard() -> NeatObjectType:
-    selection = {str(i + 1): value for i, value in enumerate(NeatObjectType.__args__)}  # type: ignore
-    prompt = "Select object:\n" + "\n".join([f"[{key}] {value}" for key, value in selection.items()]) + "\n"
-    return selection[Prompt.ask(prompt, choices=list(selection.keys()))]
+def object_wizard(message: str = "Select object") -> NeatObjectType:
+    return _selection(message, get_args(NeatObjectType))
 
 
-def rdf_dm_wizard() -> RDFFileType:
-    selection = {str(i + 1): value for i, value in enumerate(RDFFileType.__args__)}  # type: ignore
-    prompt = "Select source:\n" + "\n".join([f"[{key}] {value}" for key, value in selection.items()]) + "\n"
-    return selection[Prompt.ask(prompt, choices=list(selection.keys()))]
+def rdf_dm_wizard(message: str = "Select source:") -> RDFFileType:
+    return _selection(message, get_args(RDFFileType))
+
+
+_T_Option = TypeVar("_T_Option")
+
+def _selection(message: str, options: Sequence[_T_Option]) -> _T_Option:
+    option_text = "\n  ".join([f"{i+1}) {option}" for i, option in enumerate(options)])
+    selected_index = IntPrompt().ask(f"{message}\n  {option_text}\n", choices=list(map(str,range(1, len(options) + 1)))) -1
+    return options[selected_index]
+
