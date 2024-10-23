8b0c2713b0e98be74b0796f2d4ed704484d933e1
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2024-10-23 09:36:54 +0200

    added wizard (#662)
    * added wizard
    
    * Update cognite/neat/session/_wizard.py
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    * Linting and static code checks
    
    * Update cognite/neat/session/_read.py
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    * Update cognite/neat/session/_read.py
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    Co-authored-by: nikokaoja <nikokaoja@users.noreply.github.com>

---
+++ cognite/neat/session/_wizard.py
@@ -0,0 +1,26 @@
+from collections.abc import Sequence
+from typing import Literal, TypeVar, get_args
+
+from rich.prompt import IntPrompt
+
+RDFFileType = Literal["Ontology", "IMF Types", "Inference"]
+NeatObjectType = Literal["Data Model", "Instances"]
+
+
+def object_wizard(message: str = "Select object") -> NeatObjectType:
+    return _selection(message, get_args(NeatObjectType))
+
+
+def rdf_dm_wizard(message: str = "Select source:") -> RDFFileType:
+    return _selection(message, get_args(RDFFileType))
+
+
+_T_Option = TypeVar("_T_Option")
+
+
+def _selection(message: str, options: Sequence[_T_Option]) -> _T_Option:
+    option_text = "\n  ".join([f"{i+1}) {option}" for i, option in enumerate(options)])
+    selected_index = (
+        IntPrompt().ask(f"{message}\n  {option_text}\n", choices=list(map(str, range(1, len(options) + 1)))) - 1
+    )
+    return options[selected_index]
