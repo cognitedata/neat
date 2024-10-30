8434e05af2f524c2b8ddb27fee7cbb499c259625
anders-albert <anders0albert@gmail.com>
2024-10-30 15:14:46 +0100

    docs: finished tutorial

--- cognite/neat/_session/_inspect.py
+++ cognite/neat/_session/_inspect.py
@@ -9,6 +9,7 @@ class InspectAPI:
     def __init__(self, state: SessionState) -> None:
         self._state = state
 
+    @property
     def properties(self) -> pd.DataFrame:
         """Returns the properties of the current data model."""
         return self._state.last_verified_rule.properties.to_pandas()
