04155e2e979f4b5da572d2c2f10913ccde076f4d
nikokaoja <nikola.vasiljevic@cognite.com>
2024-11-14 16:05:13 +0100

    improve inspect

--- cognite/neat/_session/_inspect.py
+++ cognite/neat/_session/_inspect.py
@@ -157,6 +157,7 @@ class InspectUploadOutcome:
 
             lines: list[str] = []
             for item in outcome:
+                print(item.name)
                 lines.append(f"### {item.name}")
                 if unique_errors := set(item.error_messages):
                     lines.append("#### Errors")
@@ -173,8 +174,18 @@ class InspectUploadOutcome:
                             continue
                         lines.append(f"#### {key}")
                         if isinstance(value, list):
-                            for v in value:
-                                lines.append(f"  * {v}")
+                            total = len(value)
+                            for i, v in enumerate(value):
+                                if key in ["created", "updated"]:
+                                    if i < 50:
+                                        lines.append(f"  * {v}")
+                                    elif i == 50 and total > 50:
+                                        lines.append(f"  * ... {total-50} more")
+                                    elif i == 50 and total == 50:
+                                        lines.append(f"  * {v}")
+                                else:
+                                    lines.append(f"  * {v}")
+
                         else:
                             lines.append(f"  * {value}")
 
