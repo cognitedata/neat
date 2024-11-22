898d94a2d6669a1a0d82612a8224c296eda78c45
anders-albert <anders0albert@gmail.com>
2024-11-22 16:06:39 +0100

    tests: added failing test

--- tests/tests_integration/test_session/test_read.py
+++ tests/tests_integration/test_session/test_read.py
@@ -1,8 +1,15 @@
 from cognite.client import CogniteClient
 
 from cognite.neat import NeatSession
+from tests import data
 
 
 class TestRead:
     def test_read_model_referencing_core(self, cognite_client: CogniteClient) -> None:
-        _ = NeatSession(cognite_client=cognite_client)
+        neat = NeatSession(client=cognite_client)
+
+        neat.read.yaml(data.REFERENCING_CORE, format="toolkit")
+
+        issues = neat.verify()
+
+        assert issues.has_errors is False
