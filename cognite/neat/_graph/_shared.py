4786e556c89fe316ec2defa08dfa64f17fd94260
nikokaoja <nikola.vasiljevic@cognite.com>
2024-11-22 18:12:52 +0100

    keeping only rdflib_to_oxi

--- cognite/neat/_graph/_shared.py
+++ cognite/neat/_graph/_shared.py
@@ -7,60 +7,6 @@ MIMETypes: TypeAlias = Literal[
 RDFTypes: TypeAlias = Literal["xml", "rdf", "owl", "n3", "ttl", "turtle", "nt", "nq", "nquads", "trig"]
 
 
-def rdflib_to_mime_types(rdflib_format: str) -> str | None:
-    """Convert an RDFlib format to a MIME type.
-
-    Args:
-        rdflib_format (str): The RDFlib format.
-
-    Returns:
-        MIMETypes: The MIME type.
-
-    !!! note
-        This will be replaced once new version of oxrdflib is released.
-
-    """
-
-    mapping = {
-        "xml": "application/rdf+xml",
-        "rdf": "application/rdf+xml",
-        "owl": "application/rdf+xml",
-        "n3": "application/n-triple",
-        "ttl": "text/turtle",
-        "turtle": "text/turtle",
-        "nt": "application/n-triple",
-        "nq": "application/n-quads",
-        "nquads": "application/n-quads",
-        "trig": "application/trig",
-    }
-    return mapping.get(rdflib_format, None)
-
-
-def mime_to_oxi_types(mime_type: str) -> str | None:
-    """Convert an MIME type to OXI type.
-
-    Args:
-        mime_type (str): The MIME type.
-
-    Returns:
-        MIMETypes: The MIME type.
-
-    !!! note
-        This will be replaced once new version of oxrdflib is released.
-
-    """
-
-    mapping = {
-        "application/rdf+xml": "ox-xml",
-        "application/n-triple": "ox-nt",
-        "text/turtle": "ox-turtle",
-        "application/n-quads": "ox-nquads",
-        "application/trig": "ox-trig",
-    }
-
-    return mapping.get(mime_type, None)
-
-
 def rdflib_to_oxi_types(rdflib_format: str) -> str | None:
     """Convert an RDFlib format to a MIME type.
 
