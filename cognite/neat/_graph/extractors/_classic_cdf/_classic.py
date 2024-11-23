58cf38bcff58ec7676899cc37ebb8ccb07aa8ad1
anders-albert <anders0albert@gmail.com>
2024-11-24 00:23:38 +0100

    feat: handle auth issues

--- cognite/neat/_graph/extractors/_classic_cdf/_classic.py
+++ cognite/neat/_graph/extractors/_classic_cdf/_classic.py
@@ -1,12 +1,15 @@
+import warnings
 from collections import defaultdict
 from collections.abc import Iterable, Sequence
 from typing import ClassVar, NamedTuple
 
 from cognite.client import CogniteClient
+from cognite.client.exceptions import CogniteAPIError
 from rdflib import Namespace
 
 from cognite.neat._constants import DEFAULT_NAMESPACE
 from cognite.neat._graph.extractors._base import BaseExtractor
+from cognite.neat._issues.warnings import AuthWarning
 from cognite.neat._shared import Triple
 from cognite.neat._utils.collection_ import chunker
 from cognite.neat._utils.rdf_ import remove_namespace_from_uri
@@ -111,8 +114,15 @@ class ClassicGraphExtractor(BaseExtractor):
 
         yield from self._extract_core_end_nodes()
 
-        yield from self._extract_labels()
-        yield from self._extract_data_sets()
+        try:
+            yield from self._extract_labels()
+        except CogniteAPIError as e:
+            warnings.warn(AuthWarning("extract labels", str(e)), stacklevel=2)
+
+        try:
+            yield from self._extract_data_sets()
+        except CogniteAPIError as e:
+            warnings.warn(AuthWarning("extract data sets", str(e)), stacklevel=2)
 
     def _extract_core_start_nodes(self):
         for core_node in self._classic_node_types:
