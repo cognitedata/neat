3e9b7e13f6606345c9e92ceed02b633201921a8c
nikokaoja <nikola.vasiljevic@cognite.com>
2024-12-03 15:52:59 +0100

    fixing failing test

--- tests/tests_integration/test_session/test_graph_flow.py
+++ tests/tests_integration/test_session/test_graph_flow.py
@@ -8,7 +8,7 @@ from pytest_regressions.data_regression import DataRegressionFixture
 from cognite.neat import NeatSession
 from cognite.neat._graph.loaders import DMSLoader
 from tests.data import classic_windfarm
-from tests.utils import normalize_neat_id_in_rules
+from tests.utils import remove_linking_in_rules
 
 RESERVED_PROPERTIES = frozenset(
     {
@@ -53,17 +53,13 @@ class TestExtractToLoadFlow:
 
         neat.verify()
 
-        normalize_neat_id_in_rules(neat._state.data_model.last_verified_information_rules[1])
-
         neat.convert("dms", mode="edge_properties")
-
-        normalize_neat_id_in_rules(neat._state.data_model.last_verified_dms_rules[1])
 
         neat.mapping.data_model.classic_to_core("Classic", use_parent_property_name=True)
 
         neat.set.data_model_id(("sp_windfarm", "WindFarm", "v1"))
 
-        normalize_neat_id_in_rules(neat._state.data_model.last_verified_dms_rules[1])
+        remove_linking_in_rules(neat._state.data_model.last_verified_dms_rules[1])
 
         rules_str = neat.to.yaml(format="neat")
 
