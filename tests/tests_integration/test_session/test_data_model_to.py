7ddf17ab3a9dab62ad70e697bba0615742fef667
anders-albert <anders0albert@gmail.com>
2024-12-05 16:59:04 +0100

    refactor; test for reuse

--- tests/tests_integration/test_session/test_data_model_to.py
+++ tests/tests_integration/test_session/test_data_model_to.py
@@ -1,4 +1,3 @@
-import pytest
 from cognite.client import data_modeling as dm
 
 from cognite.neat import NeatSession
@@ -6,44 +5,42 @@ from cognite.neat._client import NeatClient
 from tests.data import car
 
 
-@pytest.fixture
-def car_model(neat_client: NeatClient) -> dm.DataModelId:
+def create_new_car_model(neat_client: NeatClient, schema_space: str, instance_space: str) -> dm.DataModelId:
     # Creating a copy of the model to avoid modifying the original
     original_space = car.CAR_MODEL.space
-    new_space = "test_to_cdf_recreate"
-    raw_yaml = car.CONTAINERS.dump_yaml().replace(original_space, new_space)
+    raw_yaml = car.CONTAINERS.dump_yaml().replace(original_space, schema_space)
     container_copy = dm.ContainerApplyList.load(raw_yaml)
 
-    raw_yaml = car.CAR_MODEL.dump_yaml().replace(original_space, new_space)
+    raw_yaml = car.CAR_MODEL.dump_yaml().replace(original_space, schema_space)
     model_copy = dm.DataModel.load(raw_yaml)
 
-    neat_client.data_modeling.spaces.apply(dm.SpaceApply(space=new_space))
+    neat_client.data_modeling.spaces.apply(dm.SpaceApply(space=schema_space))
     neat_client.data_modeling.containers.apply(container_copy)
     neat_client.data_modeling.data_models.apply(model_copy)
 
     original_instance_space = car.INSTANCE_SPACE
-    new_instance_space = f"{new_space}_data"
     nodes = dm.NodeApplyList.load(
         dm.NodeApplyList([node for node in car.INSTANCES if isinstance(node, dm.NodeApply)])
         .dump_yaml()
-        .replace(original_instance_space, new_instance_space)
-        .replace(original_space, new_space)
+        .replace(original_instance_space, instance_space)
+        .replace(original_space, schema_space)
     )
     edges = dm.EdgeApplyList.load(
         dm.EdgeApplyList([edge for edge in car.INSTANCES if isinstance(edge, dm.EdgeApply)])
         .dump_yaml()
-        .replace(original_instance_space, new_instance_space)
-        .replace(original_space, new_space)
+        .replace(original_instance_space, instance_space)
+        .replace(original_space, schema_space)
     )
 
-    neat_client.data_modeling.spaces.apply(dm.SpaceApply(space=new_instance_space))
+    neat_client.data_modeling.spaces.apply(dm.SpaceApply(space=instance_space))
     neat_client.data_modeling.instances.apply(nodes, edges)
 
     return model_copy.as_id()
 
 
 class TestDataModelToCDF:
-    def test_to_cdf_recreate(self, neat_client: NeatClient, car_model: dm.DataModelId) -> None:
+    def test_to_cdf_recreate(self, neat_client: NeatClient) -> None:
+        car_model = create_new_car_model(neat_client, "test_to_cdf_recreate", "test_to_cdf_recreate_data")
         neat = NeatSession(neat_client)
 
         neat.read.cdf.data_model(car_model)
