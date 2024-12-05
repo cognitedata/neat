371826392adb3fb18ceec5471128cabd6ba2737f
anders-albert <anders0albert@gmail.com>
2024-12-05 18:06:05 +0100

    refactor: Share code

--- cognite/neat/_utils/reader/_base.py
+++ cognite/neat/_utils/reader/_base.py
@@ -96,12 +96,44 @@ class PathReader(NeatReader):
         return self.path.exists()
 
 
-class GitHubReader(NeatReader):
+class HttpFileReader(NeatReader):
+    def __init__(self, url: str):
+        self._url = url
+
+    def read_text(self) -> str:
+        response = requests.get(self._url)
+        response.raise_for_status()
+        return response.text
+
+    def size(self) -> int:
+        response = requests.head(self._url)
+        response.raise_for_status()
+        return int(response.headers["Content-Length"])
+
+    def iterate(self, chunk_size: int) -> Iterable[str]:
+        with requests.get(self._url, stream=True) as response:
+            response.raise_for_status()
+            for chunk in response.iter_content(chunk_size):
+                yield chunk.decode("utf-8")
+
+    def __enter__(self) -> IO:
+        return StringIO(self.read_text())
+
+    def __str__(self) -> str:
+        return self._url
+
+    def exists(self) -> bool:
+        response = requests.head(self._url)
+        return 200 <= response.status_code < 400
+
+
+class GitHubReader(HttpFileReader):
     raw_url = "https://raw.githubusercontent.com/"
 
     def __init__(self, raw: str):
         self.raw = raw
         self.repo, self.path = self._parse_url(raw)
+        super().__init__(self._full_url)
 
     @property
     def name(self) -> str:
@@ -136,60 +168,3 @@ class GitHubReader(NeatReader):
 
     def __str__(self) -> str:
         return self.raw
-
-    def read_text(self) -> str:
-        response = requests.get(self._full_url)
-        response.raise_for_status()
-        return response.text
-
-    def size(self) -> int:
-        response = requests.head(self._full_url)
-        response.raise_for_status()
-        return int(response.headers["Content-Length"])
-
-    def iterate(self, chunk_size: int) -> Iterable[str]:
-        with requests.get(self._full_url, stream=True) as response:
-            response.raise_for_status()
-            for chunk in response.iter_content(chunk_size):
-                yield chunk.decode("utf-8")
-
-    def __enter__(self) -> IO:
-        return StringIO(self.read_text())
-
-    def __exit__(self, exc_type, exc_value, traceback) -> None:
-        pass
-
-    def exists(self) -> bool:
-        response = requests.head(self._full_url)
-        return 200 <= response.status_code < 400
-
-
-class HttpFileReader(NeatReader):
-    def __init__(self, raw_url: str):
-        self._raw_url = raw_url
-
-    def read_text(self) -> str:
-        response = requests.get(self._raw_url)
-        response.raise_for_status()
-        return response.text
-
-    def size(self) -> int:
-        response = requests.head(self._raw_url)
-        response.raise_for_status()
-        return int(response.headers["Content-Length"])
-
-    def iterate(self, chunk_size: int) -> Iterable[str]:
-        with requests.get(self._raw_url, stream=True) as response:
-            response.raise_for_status()
-            for chunk in response.iter_content(chunk_size):
-                yield chunk.decode("utf-8")
-
-    def __enter__(self) -> IO:
-        return StringIO(self.read_text())
-
-    def __str__(self) -> str:
-        return self._raw_url
-
-    def exists(self) -> bool:
-        response = requests.head(self._raw_url)
-        return 200 <= response.status_code < 400
