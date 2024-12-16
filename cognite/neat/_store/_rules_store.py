f13ed7d30b2350f97b5f0421c4343c91bced2191
anders-albert <anders0albert@gmail.com>
2024-12-16 09:10:05 +0100

    refactor: stipulate neat rules store

--- cognite/neat/_store/_rules_store.py
+++ cognite/neat/_store/_rules_store.py
@@ -1,4 +1,6 @@
 from cognite.neat._issues import IssueList
+from cognite.neat._rules.exporters import BaseExporter
+from cognite.neat._rules.importers import BaseImporter
 from cognite.neat._rules.transformers import RulesTransformer
 
 from ._provenance import Provenance
@@ -8,5 +10,11 @@ class NeatRulesStore:
     def __init__(self):
         self._provenance = Provenance()
 
-    def write(self, transformation: RulesTransformer) -> IssueList:
+    def write(self, importer: BaseImporter) -> IssueList:
+        raise NotImplementedError()
+
+    def transform(self, transformer: RulesTransformer) -> IssueList:
+        raise NotImplementedError()
+
+    def read(self, exporter: BaseExporter) -> IssueList:
         raise NotImplementedError()
