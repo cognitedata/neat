031476196f5a7b037e469245cf272baa6240bf55
anders-albert <anders0albert@gmail.com>
2024-12-16 08:55:28 +0100

    refactor: stipulate new structure

--- cognite/neat/_store/__init__.py
+++ cognite/neat/_store/__init__.py
@@ -1,3 +1,4 @@
 from ._graph_store import NeatGraphStore
+from ._model_store import NeatModelStore
 
-__all__ = ["NeatGraphStore"]
+__all__ = ["NeatGraphStore", "NeatModelStore"]
---
+++ cognite/neat/_store/_model_store.py
@@ -0,0 +1,12 @@
+from cognite.neat._issues import IssueList
+from cognite.neat._rules.transformers import RulesTransformer
+
+from ._provenance import Provenance
+
+
+class NeatModelStore:
+    def __init__(self):
+        self._provenance = Provenance()
+
+    def write(self, transformation: RulesTransformer) -> IssueList:
+        raise NotImplementedError()
