95baf7ea30c01e6b902b3118476515f3e854ca23
anders-albert <anders0albert@gmail.com>
2025-01-03 15:41:08 +0100

    tests: refactor

--- tests/tests_integration/test_session/test_graph_flow.py
+++ tests/tests_integration/test_session/test_graph_flow.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import pytest
 import yaml
 from cognite.client import CogniteClient
 from cognite.client.data_classes.data_modeling import (
@@ -36,9 +37,8 @@ RESERVED_PROPERTIES = frozenset(
 
 
 class TestExtractToLoadFlow:
-    def test_classic_to_dms(
-        self, deterministic_uuid4: None, cognite_client: CogniteClient, data_regression: DataRegressionFixture
-    ) -> None:
+    @pytest.mark.usefixtures("deterministic_uuid4")
+    def test_classic_to_dms(self, cognite_client: CogniteClient, data_regression: DataRegressionFixture) -> None:
         neat = NeatSession(cognite_client, storage="oxigraph")
         # Hack to read in the test data.
         for extractor in classic_windfarm.create_extractors():
