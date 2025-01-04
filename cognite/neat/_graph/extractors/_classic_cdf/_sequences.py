8999e149ade843a6de78fb76892f1cd8528fe4c0
anders-albert <anders0albert@gmail.com>
2025-01-04 15:33:16 +0100

    refactor: move out special handling to sequence extractor

--- cognite/neat/_graph/extractors/_classic_cdf/_sequences.py
+++ cognite/neat/_graph/extractors/_classic_cdf/_sequences.py
@@ -1,11 +1,15 @@
 import itertools
+import json
 from collections.abc import Iterable
 from pathlib import Path
+from typing import Any
 
 from cognite.client import CogniteClient
 from cognite.client.data_classes import Sequence, SequenceFilter
+from rdflib import XSD, Literal, URIRef
 
 from cognite.neat._client.data_classes.neat_sequence import NeatSequence, NeatSequenceList
+from cognite.neat._shared import Triple
 
 from ._base import ClassicCDFBaseExtractor, InstanceIdPrefix
 
@@ -53,3 +57,26 @@ class SequencesExtractor(ClassicCDFBaseExtractor[NeatSequence]):
             rows_by_sequence_id = {row.id: row.rows for row in row_list}
             for seq in sequence_list:
                 yield NeatSequence.from_cognite_sequence(seq, rows_by_sequence_id.get(seq.id))
+
+    def _item2triples_special_cases(self, id_: URIRef, dumped: dict[str, Any]) -> list[Triple]:
+        """For sequences, columns and rows are special cases.'"""
+        triples: list[Triple] = []
+        if "columns" in dumped:
+            columns = dumped.pop("columns")
+            triples.extend(
+                [
+                    (
+                        id_,
+                        self.namespace.columns,
+                        # Rows have a rowNumber, so we introduce colNumber here to be consistent.
+                        Literal(json.dumps({"colNumber": no, **col}), datatype=XSD._NS["json"]),
+                    )
+                    for no, col in enumerate(columns, 1)
+                ]
+            )
+        if "rows" in dumped:
+            rows = dumped.pop("rows")
+            triples.extend(
+                [(id_, self.namespace.rows, Literal(json.dumps(row), datatype=XSD._NS["json"])) for row in rows]
+            )
+        return triples
