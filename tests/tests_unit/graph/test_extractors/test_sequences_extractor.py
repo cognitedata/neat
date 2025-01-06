daa6d962a296b703eb9e5816faea52982f31b470
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-01-06 16:24:08 +0100

    [NEAT-665] 🤔 Unpacking of sequences (#875)
    * docs: enums
    
    * build; freeze mistune
    
    * linting
    
    * feat: support reading url
    
    * refactor: catch_issues return issues
    
    * refactor; removed unused parameters
    
    * refactor; remove unused
    
    * refactor; moved logic out of NeatSession
    
    * tests: updated test
    
    * fix: DMS import bug
    
    * build: changelog
    
    * refactor; simplification
    
    * refactor; proper error
    
    * refactor; do not require you need to see resource
    
    * fix: introduced bug
    
    * tests: stipulate test
    
    * Linting and static code checks
    
    * refactor; renaming
    
    * tests: fix test
    
    * refactor; reset
    
    * build: changelog entry
    
    * style: fix typo
    
    * fix: writing model to zip
    
    * refactor: update cognite-core
    
    * fix: bug in DMSImporter
    
    * refactor: removed unused
    
    * refactor; introduced subclasses
    
    * refactor: split out enterprise
    
    * refactor; split out solution
    
    * refactor; split out data-product
    
    * fix: adjustment
    
    * refactor; cleanup ToExtension
    
    * refactor: combine init param
    
    * refactor; move out
    
    * fix; introduced bug
    
    * refactor: reduce lines
    
    * refactor; clean up ToSolutionModel
    
    * refactor cleanup data product
    
    * build: changelog
    
    * refactor: drop neat id from columns
    
    * fix: drop neatId
    
    * refactor; increase flexibility of data product
    
    * tests: extending to mock client
    
    * refactor: inherit filter
    
    * tests: extending test and fixing bug
    
    * tests: regen
    
    * tests: refactor
    
    * tests; extend test to include data product
    
    * build: changelog
    
    * fix: ensure valid model
    
    * build: changelog
    
    * tests: regen
    
    * tests: updated
    
    * feat; automatically include path and root when mapping to asset parent
    
    * refactor: regen
    
    * Ãrefactor; added readonly properties
    
    * fix: skip readonly properties
    
    * fix; proper implementation
    
    * build; changelog
    
    * test: regen
    
    * refactor: review feedback
    
    * build: upgrade toolkit to 0.3.23
    
    * build: upgrade modules
    
    * build: upgrade to toolkit 0.3
    
    * build: added missin acl\
    
    * docs; document the to solution model parameters
    
    * refactor; switch to container filter
    
    * fix: deterministic
    
    * tests: regen
    
    * tests: regen
    
    * tests: regen
    
    * build: changelog
    
    * feat; first draft connection to literal
    
    * build: changelog
    
    * tests: include in test
    
    * tests: regen
    
    * tests: added missing step
    
    * test: regen
    
    * fix: typo
    
    * tests: extending wind farm with sequence rows
    
    * refactor; update test data
    
    * feat: introduced neat sequence
    
    * feat: factory method
    
    * feat: first pass of updating extractor
    
    * tests: updated test data
    
    * refactor; update example
    
    * feat: implemented unpacking
    
    * refactor: column as list
    
    * tests: updated test
    
    * fix: keep order
    
    * Ãdocs; document new
    
    * build: changelog
    
    * refactor: move out special handling to sequence extractor
    
    * refactor: introduce extra parameter
    
    * tests: extend test
    
    * tests: as write instead
    
    * feat; implemented unpacking of sequences
    
    * refactor: finish implementation
    
    ---------
    
    Co-authored-by: doctrino <doctrino@users.noreply.github.com>

--- tests/tests_unit/graph/test_extractors/test_sequences_extractor.py
+++ tests/tests_unit/graph/test_extractors/test_sequences_extractor.py
@@ -1,15 +1,19 @@
 from unittest.mock import MagicMock
 
+import pytest
+from cognite.client import CogniteClient
 from cognite.client._api.sequences import SequencesAPI, SequencesDataAPI
 from cognite.client.data_classes import SequenceList, SequenceRowsList
 from cognite.client.testing import monkeypatch_cognite_client
 from rdflib import Graph
 
 from cognite.neat._graph.extractors import SequencesExtractor
+from cognite.neat._utils.rdf_ import remove_namespace_from_uri
 from tests.config import CLASSIC_CDF_EXTRACTOR_DATA
 
 
-def test_sequences_extractor():
+@pytest.fixture
+def client_mock() -> CogniteClient:
     row_list = SequenceRowsList.load((CLASSIC_CDF_EXTRACTOR_DATA / "sequence_rows.yaml").read_text())
     rows_by_id = {row.id: row for row in row_list}
 
@@ -26,10 +30,61 @@ def test_sequences_extractor():
         client_mock.sequences.return_value = sequences
         client_mock.sequences.aggregate_count.return_value = len(sequences)
         client_mock.sequences.rows.retrieve.side_effect = mock_row_retrieve
+        yield client_mock
 
+
+def test_sequences_extractor(client_mock: CogniteClient) -> None:
     g = Graph()
-
-    for triple in SequencesExtractor.from_dataset(client_mock, data_set_external_id="some data set").extract():
+    for triple in SequencesExtractor.from_dataset(
+        client_mock, data_set_external_id="some data set", as_write=True
+    ).extract():
         g.add(triple)
 
-    assert len(g) == 26
+    assert unique_properties(g) == {
+        "assetId",
+        "columns",
+        "rows",
+        "dataSetId",
+        "name",
+        "externalId",
+        "type",
+    }
+    assert len(g) == 22
+
+
+def test_sequence_extractor_unpack_columns(client_mock: CogniteClient) -> None:
+    g = Graph()
+    for triple in SequencesExtractor.from_dataset(
+        client_mock, data_set_external_id="some data set", as_write=True, unpack_columns=True
+    ).extract():
+        g.add(triple)
+
+    assert unique_properties(g) == {
+        "assetId",
+        "dataSetId",
+        "name",
+        "externalId",
+        "type",
+        "0Values",
+        "1Values",
+        "2Values",
+        "3Values",
+        "0",
+        "1",
+        "2",
+        "3",
+        "columnOrder",
+        "columnValueTypes",
+    }
+    assert len(g) == 34
+
+
+def unique_properties(g: Graph) -> set[str]:
+    result = g.query("""
+    select distinct ?predicate
+    where {
+        ?subject ?predicate ?object
+    }
+    """)
+
+    return {remove_namespace_from_uri(row[0]) for row in result}
