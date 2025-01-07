89f31af630df5acff304e41573dbda90d97e93a8
anders-albert <anders0albert@gmail.com>
2025-01-07 15:38:18 +0100

    tests: simplified and robustified test

--- tests/tests_unit/graph/test_transformers/test_make_connection_on_exact_match.py
+++ tests/tests_unit/graph/test_transformers/test_make_connection_on_exact_match.py
@@ -1,29 +1,33 @@
-from cognite.client.data_classes import Asset
+import contextlib
+import io
+
+from cognite.client.data_classes import Asset, FileMetadata
 
 from cognite.neat import NeatSession
 from cognite.neat._constants import CLASSIC_CDF_NAMESPACE
 from cognite.neat._graph.extractors._classic_cdf._assets import AssetsExtractor
-from tests.data import classic_windfarm
+from cognite.neat._graph.extractors._classic_cdf._files import FilesExtractor
 
 
 def test_exact_match() -> None:
     neat = NeatSession()
-    manufacturer = Asset(
-        external_id="manufacturer",
-        # Matching FileMetadata.source
-        name="manufacturer1",
-        description="Manufacturer of the wind turbine",
-    )
-    # Load test data
-    for extractor in classic_windfarm.create_extractors():
-        neat._state.instances.store.write(extractor)
+    manufacturer = Asset(id=1, external_id="manufacturer", name="manufacturer1")
+    file = FileMetadata(id=1, external_id="file", source="manufacturer1", name="datasheet")
     neat._state.instances.store.write(AssetsExtractor([manufacturer], as_write=True, namespace=CLASSIC_CDF_NAMESPACE))
+    neat._state.instances.store.write(FilesExtractor([file], as_write=True, namespace=CLASSIC_CDF_NAMESPACE))
 
-    neat.prepare.instances.make_connection_on_exact_match(
-        ("Asset", "name"), ("FileMetadata", "source"), connection="files", limit=None
-    )
+    # Neat Session does not raise an error, but prints it.
+    output = io.StringIO()
+    with contextlib.redirect_stdout(output):
+        neat.prepare.instances.make_connection_on_exact_match(
+            ("Asset", "name"), ("FileMetadata", "source"), connection="files", limit=None
+        )
+    printed_statements = output.getvalue()
+    assert not printed_statements.startswith(
+        "[ERROR]"
+    ), f"Failed to make connection on exact match: {printed_statements}"
 
     has_property = neat._state.instances.store.queries.type_with_property(
-        CLASSIC_CDF_NAMESPACE["Asset"], CLASSIC_CDF_NAMESPACE["name"]
+        CLASSIC_CDF_NAMESPACE["Asset"], CLASSIC_CDF_NAMESPACE["files"]
     )
     assert has_property is True
