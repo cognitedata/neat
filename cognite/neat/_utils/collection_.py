066065caf7610e63bbe07606d7aec383a34b6058
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-01-09 14:49:19 +0100

    [NEAT-673, NEAT-674, NEAT-677] ✅ Progress Bars (#896)
    * build: added tqdm to optional
    
    * refactor: cleanup unused
    
    * refactor: adding global config
    
    * refactor: make progress bar configurable
    
    * style: type tqdm
    
    * refactor: use notebook version
    
    * refactor; added notebook option
    
    * refactor: iterable for inferer
    
    * refactor: loader
    
    * build: changelog
    
    * refactor: cleanup
    
    * docs; updated tutorials

--- cognite/neat/_utils/collection_.py
+++ cognite/neat/_utils/collection_.py
@@ -2,6 +2,7 @@ from collections import Counter
 from collections.abc import Iterable, Sequence
 from typing import TypeVar
 
+from cognite.neat._config import GLOBAL_CONFIG
 from cognite.neat._constants import IN_PYODIDE
 
 T_Element = TypeVar("T_Element")
@@ -26,20 +27,40 @@ def remove_list_elements(input_list: list, elements_to_remove: list) -> list:
 
 
 def iterate_progress_bar(iterable: Iterable[T_Element], total: int, description: str) -> Iterable[T_Element]:
-    if IN_PYODIDE:
+    if IN_PYODIDE or GLOBAL_CONFIG.progress_bar in ("infer", "tqdm"):
         try:
-            from tqdm import tqdm  # type: ignore [import]
+            from tqdm import tqdm
         except ModuleNotFoundError:
             return iterable
         return tqdm(iterable, total=total, desc=description)
-    # Progress bar from rich requires multi-threading, which is not supported in Pyodide
-    try:
-        from rich.progress import track
-    except ModuleNotFoundError:
-        return iterable
 
-    return track(
-        iterable,
-        total=total,
-        description=description,
-    )
+    elif GLOBAL_CONFIG.progress_bar == "tqdm-notebook":
+        try:
+            from tqdm.notebook import tqdm as tqdm_notebook
+        except ModuleNotFoundError:
+            return iterable
+        return tqdm_notebook(iterable, total=total, desc=description)
+    elif GLOBAL_CONFIG.progress_bar in ("infer", "rich"):
+        # Progress bar from rich requires multi-threading, which is not supported in Pyodide
+        try:
+            from rich.progress import track
+        except ModuleNotFoundError:
+            return iterable
+
+        return track(
+            iterable,
+            total=total,
+            description=description,
+        )
+    elif GLOBAL_CONFIG.progress_bar is None:
+        return iterable
+    else:
+        raise ValueError(f"Unsupported progress bar type: {GLOBAL_CONFIG.progress_bar}")
+
+
+def iterate_progress_bar_if_above_config_threshold(
+    iterable: Iterable[T_Element], total: int, description: str
+) -> Iterable[T_Element]:
+    if GLOBAL_CONFIG.use_iterate_bar_threshold and total > GLOBAL_CONFIG.use_iterate_bar_threshold:
+        return iterate_progress_bar(iterable, total, description)
+    return iterable
--- scripts/clean_space.py
+++ scripts/clean_space.py
@@ -1,7 +1,6 @@
-from cognite.neat._config import Config
 from pathlib import Path
 
-from cognite.neat._client import NeatClient
+from cognite.neat import NeatSession, get_cognite_client
 
 
 REPO_ROOT = Path(__file__).resolve().parent.parent
@@ -9,12 +8,9 @@ SPACE = "sp_pump_station"
 
 
 def main() -> None:
-    config = Config.from_yaml(REPO_ROOT / "config.yaml")
-    client = config.cdf_auth_config.get_client()
-    client.files.list()
-
-    neat_client = NeatClient(client)
-    neat_client.loaders.spaces.clean(SPACE)
+    client = get_cognite_client(".env")
+    neat = NeatSession(client)
+    neat._state.client.loaders.spaces.clean(SPACE)
 
 
 if __name__ == '__main__':
