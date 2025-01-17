23669733b9a3b528d01722b972b4b3f06a8a67ac
anders-albert <anders0albert@gmail.com>
2025-01-17 14:11:18 +0100

    feat; add support for unquoting

--- cognite/neat/_graph/loaders/_rdf2dms.py
+++ cognite/neat/_graph/loaders/_rdf2dms.py
@@ -1,5 +1,6 @@
 import itertools
 import json
+import urllib.parse
 import warnings
 from collections import defaultdict
 from collections.abc import Iterable, Sequence
@@ -70,6 +71,7 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
         tracker: type[Tracker] | None = None,
         rules: DMSRules | None = None,
         client: NeatClient | None = None,
+        unquote_external_ids: bool = False,
     ):
         super().__init__(graph_store)
         self.data_model = data_model
@@ -79,6 +81,7 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
         self._tracker: type[Tracker] = tracker or LogTracker
         self.rules = rules
         self._client = client
+        self._unquote_external_ids = unquote_external_ids
 
     @classmethod
     def from_data_model_id(
@@ -483,6 +486,8 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
     ) -> dm.InstanceApply:
         type_ = properties.pop(RDF.type, [None])[0]
         created = pydantic_cls.model_validate(properties)
+        if self._unquote_external_ids:
+            identifier = urllib.parse.unquote(identifier)
 
         return dm.NodeApply(
             space=self.instance_space,
@@ -506,6 +511,9 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
         created = pydantic_cls.model_validate(properties)
         if type_ is None:
             raise ValueError(f"Missing type for edge {identifier}")
+
+        if self._unquote_external_ids:
+            identifier = urllib.parse.unquote(identifier)
 
         return dm.EdgeApply(
             space=self.instance_space,
@@ -543,6 +551,9 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
                 yield error
             for target in values:
                 external_id = f"{identifier}.{prop_id}.{target}"
+                if self._unquote_external_ids:
+                    external_id = urllib.parse.unquote(external_id)
+
                 yield dm.EdgeApply(
                     space=self.instance_space,
                     external_id=(external_id if len(external_id) < 256 else create_sha256_hash(external_id)),
