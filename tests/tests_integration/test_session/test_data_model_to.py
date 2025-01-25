b2065dc2c026293483a1ae8a15819e70ae65c90a
anders-albert <anders0albert@gmail.com>
2025-01-25 16:15:48 +0100

    tests: skip trouble some test

--- tests/tests_integration/test_session/test_data_model_to.py
+++ tests/tests_integration/test_session/test_data_model_to.py
@@ -110,6 +110,7 @@ class TestRulesStoreProvenanceSyncing:
             "this neat session have the same data model id"
         ) in e.value.raw_message
 
+    @pytest.mark.skip("Skipped need a discussion on how to add with the Info + DMS regime")
     def test_stop_model_import_which_source_is_not_in_session(self, neat_client: NeatClient) -> None:
         neat = NeatSession(neat_client)
         neat.read.excel.examples.pump_example()
