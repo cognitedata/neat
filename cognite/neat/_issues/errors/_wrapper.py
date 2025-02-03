17f443bba1497460ffd369403081df65d007a635
anders-albert <anders0albert@gmail.com>
2025-02-03 14:49:54 +0100

    refactor; expirement

--- cognite/neat/_issues/errors/_wrapper.py
+++ cognite/neat/_issues/errors/_wrapper.py
@@ -1,11 +1,24 @@
+from abc import ABC
 from dataclasses import dataclass
 
 from cognite.neat._issues import NeatError
 
 
 @dataclass(unsafe_hash=True)
-class MetadataValueError(NeatError, ValueError):
-    """Field {field_name} - {error}"""
+class SpreadsheetError(NeatError, ValueError, ABC):
+    location: str
+    error: NeatError
+    row: int
 
-    field_name: str
+
+@dataclass(unsafe_hash=True)
+class MetadataValueError(SpreadsheetError):
+    """In {row}, the {location} - {error}"""
+
+
+@dataclass(unsafe_hash=True)
+class ViewValueError(SpreadsheetError):
+    """View {view_name} - {error}"""
+
+    location: str
     error: NeatError
