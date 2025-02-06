aa64e40fc7369efd19820702c6a648abde97596b
anders-albert <anders0albert@gmail.com>
2025-02-06 17:41:29 +0100

    refactor; proteries by class

--- cognite/neat/_rules/analysis/_explore.py
+++ cognite/neat/_rules/analysis/_explore.py
@@ -1,5 +1,8 @@
+import warnings
 from collections import defaultdict
+from graphlib import TopologicalSorter
 
+from cognite.neat._issues.warnings import NeatValueWarning
 from cognite.neat._rules.models import DMSRules, InformationRules
 from cognite.neat._rules.models.entities import ClassEntity
 from cognite.neat._rules.models.information import InformationProperty
@@ -10,12 +13,47 @@ class Explore:
         self._information = information
         self._dms = dms
 
+    def parents_by_class(
+        self, include_ancestors: bool = False, include_different_space: bool = False
+    ) -> dict[ClassEntity, set[ClassEntity]]:
+        parents_by_class: dict[ClassEntity, set[ClassEntity]] = defaultdict(set)
+        for class_ in self._information.classes:
+            for parent in class_.implements or []:
+                if include_different_space or parent.prefix == class_.class_.prefix:
+                    parents_by_class[class_.class_].add(parent)
+                else:
+                    warnings.warn(
+                        NeatValueWarning(
+                            f"Parent class {parent} of class {class_} is not in the same namespace, skipping!"
+                        ),
+                        stacklevel=2,
+                    )
+        if include_ancestors:
+            # Topological sort to ensure that parents are defined before children
+            for class_entity in list(TopologicalSorter(parents_by_class).static_order()):
+                parents_by_class[class_entity] |= {
+                    parent for parent in parents_by_class[class_entity] for class_ in parents_by_class[parent]
+                }
+
+        return parents_by_class
+
     def properties_by_classes(
-        self,
-        include_inherited: bool = False,
+        self, include_ancestors: bool = False, include_different_space: bool = False
     ) -> dict[ClassEntity, list[InformationProperty]]:
         properties_by_classes = defaultdict(list)
         for prop in self._information.properties:
             properties_by_classes[prop.class_].append(prop)
+
+        if include_ancestors:
+            parents_by_classes = self.parents_by_class(
+                include_ancestors=include_ancestors, include_different_space=include_different_space
+            )
+            for class_, parents in parents_by_classes.items():
+                class_properties = {prop.property_ for prop in properties_by_classes[class_]}
+                for parent in parents:
+                    for parent_prop in properties_by_classes[parent]:
+                        if parent_prop.property_ not in class_properties:
+                            properties_by_classes[class_].append(parent_prop)
+                            class_properties.add(parent_prop.property_)
 
         return properties_by_classes
