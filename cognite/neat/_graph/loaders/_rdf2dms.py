7e5c5e3339bca6659876cfa2b299de09f5f55d02
anders-albert <anders0albert@gmail.com>
2025-02-17 10:01:07 +0100

    docs;

--- cognite/neat/_graph/loaders/_rdf2dms.py
+++ cognite/neat/_graph/loaders/_rdf2dms.py
@@ -312,6 +312,7 @@ class DMSLoader(CDFLoader[dm.InstanceApply]):
                     return value[0]
                 if len(value) > 1:
                     warnings.warn(
+                        # the identifier is unknown, it will be cest in the create_instances method
                         PropertyMultipleValueWarning("", "property", str(info.field_name), value=str(value[0])),
                         stacklevel=2,
                     )
