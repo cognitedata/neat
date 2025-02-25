72fff586ec8af15c61aab6ee92855a68f15613d7
anders-albert <anders0albert@gmail.com>
2025-02-25 10:40:07 +0100

    Ãfeat: added display function

--- cognite/neat/_utils/migration.py
+++ cognite/neat/_utils/migration.py
@@ -1,10 +1,15 @@
+import typing
+from pprint import pprint
 from typing import Any, TypeVar, cast
 
 import pandas as pd
+from cognite.client import CogniteClient
 from cognite.client.data_classes import Asset, Event, FileMetadata, Sequence, TimeSeries
-from cognite.client.data_classes.data_modeling import Node
+from cognite.client.data_classes.data_modeling import Node, ViewId
 
 from cognite.neat._rules._constants import get_reserved_words
+
+from .auxiliary import local_import
 
 T_Classic = TypeVar("T_Classic", bound=Asset | TimeSeries | Event | Sequence | FileMetadata)
 
@@ -13,6 +18,44 @@ SERVER_SIDE = {"parentId", "id", "createdTime", "lastUpdatedTime", "rootId", "so
 NODE_SERVER_SIDE = {"version", "lastUpdatedTime", "createdTime", "instanceType", "path", "root", "pathLastUpdatedTime"}
 RENAMING = {"parent": "parentExternalId"}
 RESERVED_PROPERTIES = {word.replace("_", "").lower() for word in get_reserved_words("property")}
+
+
+def display_diffs(
+    source_items: typing.Sequence[Asset | TimeSeries | Event | Sequence | FileMetadata],
+    target_view: ViewId | tuple[str, str, str],
+    source: CogniteClient,
+    target: CogniteClient | None = None,
+) -> None:
+    """Displays the differences between the source and target objects."""
+    local_import("deepdiff", "compare")
+    from deepdiff import DeepDiff
+
+    target_client = target or source
+    dataset_by_id = {
+        d.id: d.external_id
+        for d in source.data_sets.retrieve_multiple(
+            ids=list({item.data_set_id for item in source_items if item.data_set_id})
+        )
+    }
+    target_instances = target_client.data_modeling.instances.retrieve(
+        [
+            (dataset_by_id[item.data_set_id].lower(), item.external_id)  # type: ignore[union-attr]
+            for item in source_items
+            if item.data_set_id and dataset_by_id.get(item.data_set_id) and item.external_id
+        ],
+        sources=target_view,
+    )
+    node_by_id = {node.external_id: node for node in target_instances.nodes}
+    type_ = type(source_items[0])
+    for item in source_items:
+        if item.external_id not in node_by_id:
+            print(f"{type_} {item.external_id} is missing")
+            continue
+        node = node_by_id[item.external_id]
+        classic_node = as_classic(node, item)
+        if classic_node != item:
+            print(f"Failed: {type_} {item.external_id!r}")
+            pprint(DeepDiff(classic_node.dump(), item.dump()))
 
 
 def as_classic(node: Node, classic: T_Classic) -> T_Classic:
