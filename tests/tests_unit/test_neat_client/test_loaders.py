d3aed870584db75407789034ca53eefa60833a97
anders-albert <anders0albert@gmail.com>
2025-03-02 17:10:59 +0100

    tests: added failing tests

--- tests/tests_unit/test_neat_client/test_loaders.py
+++ tests/tests_unit/test_neat_client/test_loaders.py
@@ -1,2 +1,38 @@
+from cognite.client import data_modeling as dm
+
+from cognite.neat._client._api.data_modeling_loaders import DataModelLoader
+
+_DEFAULT_ARGS = dict(is_global=False, created_time=1, last_updated_time=1, name=None, description=None)
+
+
 class TestDataModelLoader:
-    def test_merge_data_models(self) -> None: ...
+    def test_merge_data_models(self) -> None:
+        local = dm.DataModelApply(
+            "my_space",
+            "my_model",
+            "v1",
+            views=[
+                dm.ViewId("my_space", "view1", "v1"),
+            ],
+        )
+        remote = dm.DataModel[dm.ViewId](
+            "my_space",
+            "my_model",
+            "v1",
+            views=[
+                dm.ViewId("my_space", "view2", "v1"),
+            ],
+            **_DEFAULT_ARGS,
+        )
+
+        merged = DataModelLoader.merge(local, remote)
+
+        assert merged == dm.DataModelApply(
+            "my_space",
+            "my_model",
+            "v1",
+            views=[
+                dm.ViewId("my_space", "view1", "v1"),
+                dm.ViewId("my_space", "view2", "v1"),
+            ],
+        )
