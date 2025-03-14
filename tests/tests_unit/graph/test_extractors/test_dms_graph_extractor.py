b6925d3add6dfb4eb6cdb4bb9b059d6496055585
anders-albert <anders0albert@gmail.com>
2025-03-14 12:41:50 +0900

    refactor: comment

--- tests/tests_unit/graph/test_extractors/test_dms_graph_extractor.py
+++ tests/tests_unit/graph/test_extractors/test_dms_graph_extractor.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable
+from collections.abc import Callable, Iterable
 from typing import Literal
 from unittest.mock import MagicMock
 
@@ -12,25 +12,37 @@ from cognite.neat._graph.extractors import DMSGraphExtractor
 from tests.data import car
 from tests.utils import as_read_containers, as_read_instance, as_read_space
 
-_IS_CALLED = False
 
+def create_car_instance(max_run: int = 1) -> Callable:
+    run_count = 0
 
-def car_instances(
-    instance_type: Literal["node", "edge"] = "node", source: Source | None = None, **other_args
-) -> Iterable[Instance]:
-    global _IS_CALLED
-    if _IS_CALLED:
-        return
-    for instance in car.INSTANCES:
-        yield as_read_instance(instance)
-    _IS_CALLED = True
+    # The extractor will call the instance endpoint multiple times. One for nodes,
+    # one for edges, and one for edge types.
+    # In this mocking function, we return all instances in the first call, and do nothing
+    # in the subsequent calls. This is to avoid having extra logic here to split the instances
+    # and potentially introduce bugs.
+    def car_instances(
+        instance_type: Literal["node", "edge"] = "node", source: Source | None = None, **other_args
+    ) -> Iterable[Instance]:
+        #
+        nonlocal run_count
+        if run_count >= max_run:
+            return
+        for instance in car.INSTANCES:
+            yield as_read_instance(instance)
+        run_count += 1
+
+    return car_instances
 
 
 class TestDMSGraphExtractor:
     def test_extract_car_example(self) -> None:
         with monkeypatch_neat_client() as client:
             response = MagicMock(spec=Response)
-            response.json.return_value = {"items": [item.dump() for item in car_instances()], "nextCursor": None}
+            response.json.return_value = {
+                "items": [item.dump() for item in create_car_instance(1)()],
+                "nextCursor": None,
+            }
             client.post.return_value = response
             client.data_modeling.spaces.retrieve.return_value = dm.SpaceList(
                 [as_read_space(dm.SpaceApply(car.MODEL_SPACE))]
