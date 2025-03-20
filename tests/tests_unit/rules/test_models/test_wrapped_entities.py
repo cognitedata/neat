e0f0c24488e8059e4063e1c7730cc95b2d305e07
anders-albert <anders0albert@gmail.com>
2025-03-20 09:38:57 -0500

    tests: added failing test

--- tests/tests_unit/rules/test_models/test_wrapped_entities.py
+++ tests/tests_unit/rules/test_models/test_wrapped_entities.py
@@ -27,6 +27,10 @@ RAW_FILTER_EXAMPLE = """{"and": [
   ]}"""
 
 RAW_FILTER_CELL_EXAMPLE = f"""rawFilter({RAW_FILTER_EXAMPLE})"""
+RAW_FILTER_WITH_AMPERSAND = (
+    'rawFilter({"equals": {"property": ["ne-013-i4-neat-g-m-spc", "Tag/1", '
+    '"TagCategoryDescription"], "value": "FIRE & GAS FIELD EQUIPMENT"}})'
+)
 
 
 class TestWrappedEntities:
@@ -70,6 +74,7 @@ class TestWrappedEntities:
                 RAW_FILTER_CELL_EXAMPLE,
                 RawFilter(filter=RAW_FILTER_EXAMPLE),
             ),
+            (RawFilter, RAW_FILTER_WITH_AMPERSAND, RawFilter(filter=RAW_FILTER_WITH_AMPERSAND)),
         ],
     )
     def test_load(self, cls_: type[WrappedEntity], raw: Any, expected: WrappedEntity) -> None:
