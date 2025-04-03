31aa26bb50e5e3836693ac61bf0a3668b4bcb6fe
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-03 10:22:42 +0200

    [NEAT-869] 👹 Setup new test framework for converting conceptual to physical (#1075)
    # Description
    
    Introducing a new test for checking the conversion of conceptual to
    physical. Found one small bug which I fixed.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ tests/tests_integration/test_api/test_conversion.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pytest
+import yaml
+
+from cognite.neat._client import NeatClient
+from cognite.neat._issues import catch_issues
+from cognite.neat._rules.importers import YAMLImporter
+from cognite.neat._rules.models import DMSRules
+from cognite.neat._rules.transformers import InformationToDMS, VerifyInformationRules
+from tests.data import SchemaData
+
+
+class TestValidate:
+    @pytest.mark.parametrize(
+        "conceptual_filepath, physical_filepath",
+        [
+            pytest.param(conceptual, physical, id=conceptual.stem)
+            for conceptual, physical in SchemaData.Conversion.iterate()
+        ],
+    )
+    def test_convert_conceptual_rules(
+        self, conceptual_filepath: Path, physical_filepath: Path, neat_client: NeatClient
+    ) -> None:
+        dms_rules: DMSRules | None = None
+        with catch_issues() as issues:
+            rules = YAMLImporter.from_file(conceptual_filepath, source_name=conceptual_filepath.name).to_rules()
+            info_rules = VerifyInformationRules(validate=True, client=neat_client).transform(rules)
+            dms_rules = InformationToDMS().transform(info_rules)
+
+        assert not issues
+        assert isinstance(dms_rules, DMSRules)
+        assert dms_rules.dump(mode="json", sort=True, exclude_none=True, exclude_unset=True) == yaml.safe_load(
+            physical_filepath.read_text()
+        )
