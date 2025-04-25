ade60e502a927369c07d0c3e48c19af76ecea677
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-25 13:52:09 +0200

    [NEAT-755] 🦿 Location filter loader (#1111)
    # Description
    
    Please describe the change you have made.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    fix: imports

--- cognite/neat/core/_instances/loaders/__init__.py
+++ cognite/neat/core/_instances/loaders/__init__.py
@@ -1,8 +1,9 @@
 from ._base import BaseLoader, CDFLoader
 from ._rdf2dms import DMSLoader
 from ._rdf_to_instance_space import InstanceSpaceLoader
+from ._to_location_filter import LocationFilterLoader
 
-__all__ = ["BaseLoader", "CDFLoader", "DMSLoader", "InstanceSpaceLoader"]
+__all__ = ["BaseLoader", "CDFLoader", "DMSLoader", "InstanceSpaceLoader", "LocationFilterLoader"]
 
 
 def _repr_html_() -> str:
