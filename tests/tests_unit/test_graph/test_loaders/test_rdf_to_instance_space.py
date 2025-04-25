3d83f7e68112fb3ba589c855d3dd2945a4ffd7ec
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-25 11:12:08 +0200

    [prep NEAT-806] 👷Move out space instance logic from DMS Loader (#1108)
    # Description
    
    Move out the logic for creating an instance space. 
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ tests/tests_unit/test_graph/test_loaders/test_rdf_to_instance_space.py
@@ -0,0 +1,61 @@
+from collections.abc import Iterable
+
+import pytest
+from cognite.client.data_classes import AssetWriteList
+from rdflib import RDF, Namespace
+
+from cognite.neat._client.testing import monkeypatch_neat_client
+from cognite.neat._constants import DEFAULT_SPACE_URI
+from cognite.neat._graph.extractors import AssetsExtractor, BaseExtractor
+from cognite.neat._graph.loaders import InstanceSpaceLoader
+from cognite.neat._shared import Triple
+from cognite.neat._store import NeatGraphStore
+from cognite.neat._utils.upload import UploadResult
+from tests.data import InstanceData
+
+
+def load_instance_spaces_test_cases():
+    """Test cases for instance space exporter."""
+    yield pytest.param(InstanceSpaceLoader(instance_space="my_space"), {"my_space"}, id="Simple single space")
+
+    store = NeatGraphStore.from_memory_store()
+
+    store.write(
+        extractor=AssetsExtractor(
+            AssetWriteList.load(InstanceData.AssetCentricCDF.assets_yaml.read_text()), identifier="externalId"
+        )
+    )
+
+    yield pytest.param(
+        InstanceSpaceLoader(graph_store=store, space_property="dataSetId", instance_space="fallback_space"),
+        {"data_set_6931754270713290"},
+        id="Space from property",
+    )
+
+    store_from_cdf = NeatGraphStore.from_memory_store()
+
+    class CDFMockExtractor(BaseExtractor):
+        def extract(self) -> Iterable[Triple]:
+            namespace = Namespace(DEFAULT_SPACE_URI.format(space="source_space"))
+            yield namespace["my_instance"], RDF.type, namespace["CogniteAsset"]
+
+    store_from_cdf.write(CDFMockExtractor())
+    yield pytest.param(
+        InstanceSpaceLoader(graph_store=store_from_cdf, use_source_space=True), {"source_space"}, id="Space from source"
+    )
+
+
+class TestInstanceSpaceLoader:
+    @pytest.mark.parametrize("exporter, expected_spaces", list(load_instance_spaces_test_cases()))
+    def test_export_instance_space(self, exporter: InstanceSpaceLoader, expected_spaces: set[str]) -> None:
+        loaded = list(exporter.load())
+
+        assert {space.space for space in loaded} == expected_spaces
+
+        with monkeypatch_neat_client() as client:
+            results = exporter.load_into_cdf(client, check_client=False)
+
+        assert len(results) == 1
+        result = results[0]
+        assert isinstance(result, UploadResult)
+        assert result.created == expected_spaces
