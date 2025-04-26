e71e3a2a418651d8dd4589590fdde1ea4b9e5f69
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-26 12:12:29 +0200

    [prep NEAT-914] 🌍 Utils for conceptual to mapping (#1122)
    # Description
    
    Utility function for conversion from conceptual to mappings.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    fix: imports

---
+++ cognite/neat/core/_utils/mapping.py
@@ -0,0 +1,25 @@
+from collections.abc import Iterable
+
+from rdflib import Literal, Namespace, URIRef
+
+from cognite.neat.core._rules.models.information import InformationClass, InformationProperty
+
+
+def create_type_mapping(
+    classes: Iterable[InformationClass], namespace: Namespace
+) -> dict[URIRef | Literal, URIRef | Literal]:
+    """Creates a mapping of types to new types.
+
+    Args:
+        classes: The classes to map.
+        namespace: The namespace to use for the mapping.
+
+    Returns:
+        A mapping of types to new types.
+    """
+    return {namespace[cls.name]: namespace[cls.class_.suffix] for cls in classes if cls.name}
+
+
+def create_predicate_mapping(properties: Iterable[InformationProperty], namespace: Namespace) -> dict[URIRef, URIRef]:
+    """Creates a mapping of predicates to new predicates."""
+    return {namespace[prop.name]: namespace[prop.property_] for prop in properties if prop.name}
