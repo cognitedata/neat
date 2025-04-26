9684c78cb193974eac120f8d0771a0be0ee311df
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-26 07:12:06 +0200

    [prep NEAT-909]🪟 Refactor out InstanceExtractor from DMSExtractor (#1116)
    # Description
    
    Needed to refactor for reuse in view extactor
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    fix: imports
    
    fix: imports

--- cognite/neat/core/_instances/extractors/_dms.py
+++ cognite/neat/core/_instances/extractors/_dms.py
@@ -1,23 +1,20 @@
 import typing
-import urllib.parse
-from collections.abc import Callable, Iterable, Iterator, Set
+from collections.abc import Iterable, Iterator, Set
 from functools import cached_property
 
 from cognite.client import data_modeling as dm
 from cognite.client.data_classes.data_modeling import DataModelIdentifier
-from cognite.client.data_classes.data_modeling.instances import Edge, Instance, Node
+from cognite.client.data_classes.data_modeling.instances import Instance
 from cognite.client.utils.useful_types import SequenceNotStr
-from rdflib import RDF, Literal, Namespace, URIRef
+from rdflib import Namespace
 
 from cognite.neat.core._client import NeatClient
-from cognite.neat.core._config import GLOBAL_CONFIG
-from cognite.neat.core._constants import DEFAULT_SPACE_URI, is_readonly_property
 from cognite.neat.core._issues.errors import ResourceRetrievalError
 from cognite.neat.core._shared import Triple
-from cognite.neat.core._utils.collection_ import iterate_progress_bar
 
 from ._base import BaseExtractor
-from ._dict import DEFAULT_EMPTY_VALUES, DMSPropertyExtractor
+from ._dict import DEFAULT_EMPTY_VALUES
+from ._instances import InstancesExtractor
 
 
 class DMSExtractor(BaseExtractor):
@@ -130,110 +127,23 @@ class DMSExtractor(BaseExtractor):
         )
 
     def extract(self) -> Iterable[Triple]:
-        total_instances = sum(total for total, _ in self.total_instances_pair_by_view.values() if total is not None)
-        use_progress_bar = (
-            GLOBAL_CONFIG.use_iterate_bar_threshold and total_instances > GLOBAL_CONFIG.use_iterate_bar_threshold
-        )
-
         for view_id, (total, instances) in self.total_instances_pair_by_view.items():
             if total == 0:
                 continue
-            if use_progress_bar and total is not None:
-                instances = iterate_progress_bar(
-                    instances,
-                    total,
-                    f"Extracting instances from {view_id.space}:{view_id.external_id}(version={view_id.version})",
-                )
-
-            for count, item in enumerate(instances, 1):
-                if self.limit and count > self.limit:
-                    break
-                yield from self._extract_instance(item)
-
-    def _extract_instance(self, instance: Instance) -> Iterable[Triple]:
-        if isinstance(instance, dm.Edge):
-            if not instance.properties:
-                yield (
-                    self._as_uri_ref(instance.start_node),
-                    self._as_uri_ref(instance.type),
-                    self._as_uri_ref(instance.end_node),
-                )
-                return
-            else:
-                # If the edge has properties, we create a node for the edge and connect it to the start and end nodes.
-                id_ = self._as_uri_ref(instance)
-                type_ = self._create_type(
-                    instance, fallback=self._get_namespace(instance.space).Edge, type_priority=self.edge_type
-                )
-                yield id_, RDF.type, type_
-                yield (
-                    id_,
-                    self._as_uri_ref(dm.DirectRelationReference(instance.space, "startNode")),
-                    self._as_uri_ref(instance.start_node),
-                )
-                yield (
-                    id_,
-                    self._as_uri_ref(dm.DirectRelationReference(instance.space, "endNode")),
-                    self._as_uri_ref(instance.end_node),
-                )
-
-        elif isinstance(instance, dm.Node):
-            id_ = self._as_uri_ref(instance)
-            type_ = self._create_type(
-                instance, fallback=self._get_namespace(instance.space).Node, type_priority=self.node_type
+            instance_extractor = InstancesExtractor(
+                instances,
+                name=f"{view_id.space}:{view_id.external_id}(version={view_id.version})",
+                total=total,
+                limit=self.limit,
+                overwrite_namespace=self.overwrite_namespace,
+                unpack_json=self.unpack_json,
+                empty_values=self.empty_values,
+                str_to_ideal_type=self.str_to_ideal_type,
+                node_type=self.node_type,
+                edge_type=self.edge_type,
             )
-            yield id_, RDF.type, type_
-        else:
-            raise NotImplementedError(f"Unknown instance type {type(instance)}")
 
-        if self.overwrite_namespace:
-            # If the namespace is overwritten, keep the original space as a property to avoid losing information.
-            yield id_, self._get_namespace(instance.space)["space"], Literal(instance.space)
-
-        for view_id, properties in instance.properties.items():
-            namespace = self._get_namespace(view_id.space)
-            yield from DMSPropertyExtractor(
-                id_,
-                properties,
-                namespace,
-                self._as_uri_ref,
-                self.empty_values,
-                self.str_to_ideal_type,
-                self.unpack_json,
-            ).extract()
-
-    def _create_type(
-        self, instance: Node | Edge, fallback: URIRef, type_priority: tuple[typing.Literal["view", "type"], ...]
-    ) -> URIRef:
-        method_by_name: dict[str, Callable[[Node | Edge], URIRef | None]] = {
-            "view": self._view_to_rdf_type,
-            "type": self._instance_type_to_rdf,
-        }
-        for method_name in type_priority:
-            type_ = method_by_name[method_name](instance)
-            if type_:
-                return type_
-        else:
-            return fallback
-
-    def _instance_type_to_rdf(self, instance: Node | Edge) -> URIRef | None:
-        if instance.type:
-            return self._as_uri_ref(instance.type)
-        return None
-
-    def _view_to_rdf_type(self, instance: Node | Edge) -> URIRef | None:
-        view_id = next(iter((instance.properties or {}).keys()), None)
-        if view_id:
-            return self._get_namespace(view_id.space)[urllib.parse.quote(view_id.external_id)]
-        return None
-
-    def _as_uri_ref(self, instance: Instance | dm.DirectRelationReference) -> URIRef:
-        return self._get_namespace(instance.space)[urllib.parse.quote(instance.external_id)]
-
-    def _get_namespace(self, space: str) -> Namespace:
-        if self.overwrite_namespace:
-            return self.overwrite_namespace
-        return Namespace(DEFAULT_SPACE_URI.format(space=urllib.parse.quote(space)))
+            yield from instance_extractor.extract()
 
 
 class _ViewInstanceIterator(Iterable[Instance]):
