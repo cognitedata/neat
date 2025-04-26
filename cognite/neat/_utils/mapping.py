12aed41c8b98bbcf4574f545d1469380ad590ddd
anders-albert <anders0albert@gmail.com>
2025-04-26 11:49:45 +0200

    feat: first implementation

--- cognite/neat/_utils/mapping.py
+++ cognite/neat/_utils/mapping.py
@@ -8,7 +8,16 @@ from cognite.neat._rules.models.information import InformationClass, Information
 def create_type_mapping(
     classes: Iterable[InformationClass], namespace: Namespace
 ) -> dict[URIRef | Literal, URIRef | Literal]:
-    raise NotImplementedError()
+    """Creates a mapping of types to new types.
+
+    Args:
+        classes: The classes to map.
+        namespace: The namespace to use for the mapping.
+
+    Returns:
+        A mapping of types to new types.
+    """
+    return {namespace[cls.name]: namespace[cls.class_.suffix] for cls in classes if cls.name}
 
 
 def create_predicate_mapping(classes: Iterable[InformationProperty], namespace: Namespace) -> dict[URIRef, URIRef]:
