57929596b91f73ac757caae2d82503ea4f56d129
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-26 12:12:29 +0200

    [prep NEAT-914] 🌍 Utils for conceptual to mapping (#1122)
    # Description
    
    Utility function for conversion from conceptual to mappings.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ tests/tests_unit/test_utils/test_mapping.py
@@ -0,0 +1,25 @@
+from rdflib import Namespace
+
+from cognite.neat._rules.models.information import InformationInputClass, InformationInputProperty
+from cognite.neat._utils.mapping import create_predicate_mapping, create_type_mapping
+
+
+class TestCreateTypeMapping:
+    def test_create_predicate_mapping(self) -> None:
+        namespace = Namespace("http://example.org/")
+        classes = [InformationInputClass("MyType", name="my$$$Type").as_verified(default_prefix="my_space")]
+        mapping = create_type_mapping(classes, namespace)
+        assert mapping == {namespace["my$$$Type"]: namespace["MyType"]}
+
+
+class TestCreatePredicateMapping:
+    def test_create_predicate_mapping(self) -> None:
+        namespace = Namespace("http://example.org/")
+        properties = [
+            InformationInputProperty("myClass", "myProperty", "text", name="my###Property").as_verified(
+                default_prefix="my_space"
+            )
+        ]
+
+        mapping = create_predicate_mapping(properties, namespace)
+        assert mapping == {namespace["my###Property"]: namespace["myProperty"]}
