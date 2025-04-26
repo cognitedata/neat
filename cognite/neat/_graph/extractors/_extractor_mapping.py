62f67d395c966c5dbb8fa40eeaa24fd7780310d6
anders-albert <anders0albert@gmail.com>
2025-04-26 11:13:38 +0200

    feat: implemented extractor mapper

---
+++ cognite/neat/_graph/extractors/_extractor_mapping.py
@@ -0,0 +1,39 @@
+from collections.abc import Iterable
+
+from rdflib import RDF, Literal, URIRef
+
+from cognite.neat._shared import Triple
+
+from ._base import BaseExtractor
+
+
+class ExtractorMapper(BaseExtractor):
+    """This extractor maps the output of another extractor to a new set of triples.
+
+    This is used to transform the output of an extractor before the triples are written
+    to the graph. The motivation for this is that changing triples before they are written
+    is much cheaper than changing them after they are written to the graph.
+
+    Args:
+        extractor: The extractor to map.
+        predicate_mapping: A mapping of predicates to new predicates.
+        type_mapping: A mapping of types to new types.
+    """
+
+    def __init__(
+        self,
+        extractor: BaseExtractor,
+        predicate_mapping: dict[URIRef, URIRef],
+        type_mapping: dict[URIRef | Literal, URIRef | Literal],
+    ) -> None:
+        self.extractor = extractor
+        self.predicate_mapping = predicate_mapping
+        self.type_mapping = type_mapping
+
+    def extract(self) -> Iterable[Triple]:
+        """Extracts triples from the extractor and maps them to new predicates and types."""
+        for subject, predicate, obj in self.extractor.extract():
+            predicate = self.predicate_mapping.get(predicate, predicate)
+            if predicate == RDF.type:
+                obj = self.type_mapping.get(obj, obj)
+            yield subject, predicate, obj
