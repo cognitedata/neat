72874205b83e0a7c4e0e9afb14cb85bf3f0be629
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-27 11:41:52 +0200

    [prep NEAT-915]🏟BestClassMatch Transformer (#1126)
    # Description
    
    Adds a graph transformer that sets the `'RDF.type` based on minimizing
    the number of lost properties, if the
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    fix: imports

---
+++ tests/tests_unit/test_graph/test_transformers/test_best_class_match.py
@@ -0,0 +1,43 @@
+from rdflib import RDF, Literal, Namespace
+
+from cognite.neat._graph.transformers import BestClassMatch
+from cognite.neat._store import NeatGraphStore
+
+
+class TestBestClassMatch:
+    def test_find_best_class_match(self) -> None:
+        store = NeatGraphStore.from_memory_store()
+        namespace = Namespace("http://example.com/")
+        id_ = namespace["MyInstance"]
+        # Write a car instance to the store.
+        store._add_triples(
+            [
+                (id_, RDF.type, namespace["Unknown"]),
+                (id_, namespace["wheels"], Literal(4)),
+                (id_, namespace["color"], Literal("red")),
+                (id_, namespace["engine"], Literal("V8")),
+            ],
+            named_graph=store.default_named_graph,
+        )
+        transformer = BestClassMatch(
+            classes={
+                namespace["Car"]: frozenset({"wheels", "engine"}),
+                namespace["Bike"]: frozenset({"wheels"}),
+            }
+        )
+        issues = store.transform(transformer)
+        assert len(issues) == 1
+        assert issues[0].as_message() == (
+            "NeatValueWarning: Instance 'MyInstance' has no class match with all properties. "
+            "Best class match is 'Car' with 1 missing properties: color"
+        )
+
+        results = store.queries.select.types_with_instance_and_property_count(remove_namespace=True)
+
+        assert len(results) == 1
+        result = results[0]
+        assert result == {
+            "type": "Car",
+            "instanceCount": 1,
+            "propertyCount": 3,
+        }
