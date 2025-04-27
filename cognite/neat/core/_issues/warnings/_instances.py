359492426476c79e280554ecc6f2852802db7117
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-27 14:00:40 +0200

    [NEAT-915] 🗻 Rewriting BestClassMatchTransformer for oxigraph (#1128)
    # Description
    
    The `GROUP_CONCAT` does not seem to be implemented on `oxigraph`.
    Reimplementing using describe instead.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    fix: imports

---
+++ cognite/neat/core/_issues/warnings/_instances.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from cognite.neat._issues import NeatWarning
+
+
+@dataclass(unsafe_hash=True)
+class NoClassFoundWarning(NeatWarning):
+    """No class match found for instance {instance}"""
+
+    instance: str
+
+
+@dataclass(unsafe_hash=True)
+class PartialClassFoundWarning(NeatWarning):
+    """Instance '{instance}' has no class match with all properties. Best class match is '{best_class}'
+    with {missing_count} missing properties: {missing_properties}"""
+
+    instance: str
+    best_class: str
+    missing_count: int
+    missing_properties: frozenset[str]
