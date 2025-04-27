ef66122d630e647fd4d812981a91640336b80754
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-27 14:00:40 +0200

    [NEAT-915] 🗻 Rewriting BestClassMatchTransformer for oxigraph (#1128)
    # Description
    
    The `GROUP_CONCAT` does not seem to be implemented on `oxigraph`.
    Reimplementing using describe instead.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    fix: imports

--- tests/tests_unit/test_graph/test_transformers/test_best_class_match.py
+++ tests/tests_unit/test_graph/test_transformers/test_best_class_match.py
@@ -1,7 +1,9 @@
 from rdflib import RDF, Literal, Namespace
 
-from cognite.neat._graph.transformers import BestClassMatch
-from cognite.neat._store import NeatGraphStore
+from cognite.neat.core._constants import DEFAULT_SPACE_URI
+from cognite.neat.core._graph.transformers import BestClassMatch
+from cognite.neat.core._issues.warnings import PartialClassFoundWarning
+from cognite.neat.core._store import NeatGraphStore
 
 
 class TestBestClassMatch:
@@ -27,10 +29,7 @@ class TestBestClassMatch:
         )
         issues = store.transform(transformer)
         assert len(issues) == 1
-        assert issues[0].as_message() == (
-            "NeatValueWarning: Instance 'MyInstance' has no class match with all properties. "
-            "Best class match is 'Car' with 1 missing properties: color"
-        )
+        assert issues[0] == PartialClassFoundWarning("MyInstance", "Car", 1, frozenset({"color"}))
 
         results = store.queries.select.types_with_instance_and_property_count(remove_namespace=True)
 
@@ -41,3 +40,36 @@ class TestBestClassMatch:
             "instanceCount": 1,
             "propertyCount": 3,
         }
+
+    def test_multi_classes_match(self) -> None:
+        store = NeatGraphStore.from_oxi_local_store()
+        namespace = Namespace(DEFAULT_SPACE_URI.format(space="sp_instance_space"))
+        schema_ns = Namespace("http://example.com/schema/")
+        id_ = namespace[urllib.parse.quote("MyInstanceッ差")]
+
+        # Write a car instance to the store.
+        store._add_triples(
+            [
+                (id_, RDF.type, namespace["Unknown"]),
+                (id_, schema_ns["wheels"], Literal(4)),
+                (id_, schema_ns["engine"], Literal("V8")),
+            ],
+            named_graph=store.default_named_graph,
+        )
+
+        transformer = BestClassMatch(
+            classes={
+                schema_ns["SportsCarッ"]: frozenset({"wheels", "engine"}),
+                schema_ns["Truckッ"]: frozenset({"wheels", "engine"}),
+            }
+        )
+
+        issues = store.transform(transformer)
+
+        assert len(issues) == 1
+
+        assert issues[0] == MultiClassFoundWarning(
+            instance="sp_instance_space:MyInstanceッ差",
+            selected_class="SportsCarッ",
+            alternatives=frozenset({"Truckッ"}),
+        )
