6602f5a51906ef264d8da2c92a0e0aadaff5c124
anders-albert <anders0albert@gmail.com>
2025-04-27 07:18:58 +0200

    tests: added tests cases without namespace

--- tests/tests_unit/test_utils/test_mapping.py
+++ tests/tests_unit/test_utils/test_mapping.py
@@ -13,6 +13,11 @@ class TestCreateTypeMapping:
         mapping = create_type_mapping(classes, namespace)
         assert mapping == {namespace[urllib.parse.quote("my$$$Type")]: namespace["MyType"]}
 
+    def test_create_predicate_mapping_no_namespace(self) -> None:
+        classes = [InformationInputClass("MyType", name="my$$$Type").as_verified(default_prefix="my_space")]
+        mapping = create_type_mapping(classes)
+        assert mapping == {urllib.parse.quote("my$$$Type"): "MyType"}
+
 
 class TestCreatePredicateMapping:
     def test_create_predicate_mapping(self) -> None:
@@ -25,3 +30,13 @@ class TestCreatePredicateMapping:
 
         mapping = create_predicate_mapping(properties, namespace)
         assert mapping == {namespace[urllib.parse.quote("my###Property")]: namespace["myProperty"]}
+
+    def test_create_predicate_mapping_no_namespace(self) -> None:
+        properties = [
+            InformationInputProperty("myClass", "myProperty", "text", name="my###Property").as_verified(
+                default_prefix="my_space"
+            )
+        ]
+
+        mapping = create_predicate_mapping(properties)
+        assert mapping == {urllib.parse.quote("my###Property"): "myProperty"}
