94bc62f8737be95e51d63c62e1bfa4c78868b2e9
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-27 06:45:34 +0200

    [prep NEAT-914] 🌏 Namespace free mapper (#1123)
    # Description
    
    When extracting instances we typically do not know the spaces ahead of
    time.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ tests/tests_unit/test_graph/test_extractors/test_unknown_namespace_extractor_mapper.py
@@ -0,0 +1,30 @@
+from collections.abc import Iterable
+
+from rdflib import RDF, Literal, Namespace
+
+from cognite.neat._graph.extractors import BaseExtractor, UnknownNamespaceExtractorMapper
+from cognite.neat._shared import Triple
+
+
+class TestUnknownNamespaceExtractorMapping:
+    def test_extract(self) -> None:
+        namespace = Namespace("http://example.org/")
+
+        class MockExtractor(BaseExtractor):
+            def extract(self) -> Iterable[Triple]:
+                my_instance = namespace["my_instance"]
+                yield my_instance, RDF.type, namespace["my$$$type"]
+                yield my_instance, namespace["イ覧誠んしは"], Literal(1)
+
+        extractor = MockExtractor()
+
+        mapper = UnknownNamespaceExtractorMapper(
+            extractor,
+            {"イ覧誠んしは": "myProperty"},
+            {"my$$$type": "MyType"},
+        )
+        triples = list(mapper.extract())
+
+        assert len(triples) == 2
+        assert triples[0] == (namespace["my_instance"], RDF.type, namespace["MyType"])
+        assert triples[1] == (namespace["my_instance"], namespace["myProperty"], Literal(1))
