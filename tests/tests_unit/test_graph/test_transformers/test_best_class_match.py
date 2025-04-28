e0f224b54577707409b53f6f2b96c673c06755df
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-28 10:28:10 +0200

    [prep NEAT-916] 🥳 Improving Best Class Match transformer. (#1134)
    # Description
    
    Instance in warning is now correctly unquoted and space is part of it if
    available.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

--- tests/tests_unit/test_graph/test_transformers/test_best_class_match.py
+++ tests/tests_unit/test_graph/test_transformers/test_best_class_match.py
@@ -1,7 +1,10 @@
+import urllib.parse
+
 from rdflib import RDF, Literal, Namespace
 
+from cognite.neat._constants import DEFAULT_SPACE_URI
 from cognite.neat._graph.transformers import BestClassMatch
-from cognite.neat._issues.warnings import PartialClassFoundWarning
+from cognite.neat._issues.warnings import MultiClassFoundWarning, PartialClassFoundWarning
 from cognite.neat._store import NeatGraphStore
 
 
@@ -39,3 +42,36 @@ class TestBestClassMatch:
             "instanceCount": 1,
             "propertyCount": 3,
         }
+
+    def test_multi_classes_match(self) -> None:
+        store = NeatGraphStore.from_oxi_local_store()
+        namespace = Namespace(DEFAULT_SPACE_URI.format(space="sp_instance_space"))
+        schema_ns = Namespace("http://example.com/schema/")
+        id_ = namespace[urllib.parse.quote("MyInstanceッ差")]
+
+        # Write a car instance to the store.
+        store._add_triples(
+            [
+                (id_, RDF.type, namespace["Unknown"]),
+                (id_, schema_ns["wheels"], Literal(4)),
+                (id_, schema_ns["engine"], Literal("V8")),
+            ],
+            named_graph=store.default_named_graph,
+        )
+
+        transformer = BestClassMatch(
+            classes={
+                schema_ns["SportsCarッ"]: frozenset({"wheels", "engine"}),
+                schema_ns["Truckッ"]: frozenset({"wheels", "engine"}),
+            }
+        )
+
+        issues = store.transform(transformer)
+
+        assert len(issues) == 1
+
+        assert issues[0] == MultiClassFoundWarning(
+            instance="sp_instance_space:MyInstanceッ差",
+            selected_class="SportsCarッ",
+            alternatives=frozenset({"Truckッ"}),
+        )
