63dd54d99dd1d00460cedb73342c576957dabdb8
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-04-28 13:19:29 +0200

    [NEAT-916] 🤔 Implement custom script best match (#1142)
    # Description
    
    Please describe the change you have made.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ scripts/one_off/report_best_class_match.py
@@ -0,0 +1,45 @@
+"""This script goes through the results of the best class match and generates a report, spreadsheet with the
+summary of the results.
+"""
+import ast
+from pathlib import Path
+from collections import defaultdict
+import pandas as pd
+import json
+from cognite.neat._issues.warnings import MultiClassFoundWarning, PartialClassFoundWarning
+
+def create_report(issue_csv_file: Path, output_excel_file: Path) -> None:
+    """Create report from the issue CSV file."""
+    df = pd.read_csv(issue_csv_file)
+    is_multi_class = df["NeatIssue"] == 'MultiClassFoundWarning'
+    multi_df = df[is_multi_class].copy()
+    partial_df = df[~is_multi_class].copy()
+    multi_df["alternatives"] = multi_df["alternatives"].apply(ast.literal_eval)
+    partial_df["missingProperties"] = partial_df["missingProperties"].apply(ast.literal_eval)
+
+    multi_issues = [
+        MultiClassFoundWarning.load(record) for record in multi_df.to_dict(orient="records")
+    ]
+    partial_issues = [
+        PartialClassFoundWarning.load(record) for record in partial_df.to_dict(orient="records")
+    ]
+
+    multi_issue_by_alternatives = defaultdict(list)
+    for multi_issue in multi_issues:
+        multi_issue_by_alternatives[frozenset(multi_issue.alternatives | {multi_issue.selected_class})].append(multi_issue.instance)
+
+    instancees_by_class_missing_pair = defaultdict(list)
+    for partial_issue in partial_issues:
+        instancees_by_class_missing_pair[(partial_issue.best_class, partial_issue.missing_properties)].append(partial_issue.instance)
+
+    df = pd.DataFrame()
+    for no, (alternatives, instances) in enumerate(multi_issue_by_alternatives.items(), 1):
+        df[f"Alternatives_{no}"] = pd.Series(sorted(alternatives))
+        df[f"Instances_{no}"] = pd.Series(instances)
+
+    for no, ((class_name, missing_properties), instances) in enumerate(instancees_by_class_missing_pair.items(), 1):
+        df[f"Case_{no}_class_{class_name}_missing"] = pd.Series(sorted(missing_properties))
+        df[f"Instances_{no}"] = pd.Series(instances)
+
+    df.to_excel(output_excel_file, index=False)
+    print(f"Report created: {output_excel_file.as_posix()!r}")
