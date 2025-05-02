1a8278a270c34cb40c7529251972c1ead0224464
anders-albert <anders0albert@gmail.com>
2025-05-02 15:57:33 +0200

    tests: added failing

--- tests/tests_unit/test_graph/test_loaders/test_dict_loader.py
+++ tests/tests_unit/test_graph/test_loaders/test_dict_loader.py
@@ -50,6 +50,52 @@ class TestLocationFilterLoader:
         ]
         assert table.to_pylist() == expected_content
 
+    def test_write_parquet_invalid_type(self, tmp_path: Path) -> None:
+        store = NeatGraphStore.from_oxi_local_store()
+
+        class DummyExtractor:
+            def extract(self) -> Iterable[Triple]:
+                namespace = Namespace("http://example.org/")
+                my_asset = namespace["my_asset"]
+                yield my_asset, RDF.type, namespace["Asset"]
+                yield my_asset, namespace["name"], Literal("Doctrino Asset")
+                yield my_asset, namespace["createdYear"], Literal(2025)
+                yield my_asset, namespace["price"], Literal(1234.56)
+                yield my_asset, namespace["isActive"], Literal(True)
+                invalid_asset = namespace["invalid_asset"]
+                yield invalid_asset, RDF.type, namespace["Asset"]
+                yield invalid_asset, namespace["name"], Literal("Invalid Asset")
+                yield invalid_asset, namespace["createdYear"], Literal("TwentyTwenty Five")
+                yield invalid_asset, namespace["price"], Literal(28)
+                yield invalid_asset, namespace["isActive"], Literal("Sant")
+
+        store.write(DummyExtractor())
+
+        loader = DictLoader(store, "parquet")
+        parquet_folder = tmp_path / "parquet_folder"
+        parquet_folder.mkdir(parents=True, exist_ok=True)
+
+        loader.write_to_file(parquet_folder)
+
+        expected_asset_file = parquet_folder / "Asset.parquet"
+        assert expected_asset_file.exists()
+        table = pq.read_table(expected_asset_file)
+        expected_content = [
+            {
+                "name": "Doctrino Asset",
+                "createdYear": 2025,
+                "price": 1234.56,
+                "isActive": True,
+            },
+            {
+                "name": "Invalid Asset",
+                "createdYear": None,
+                "price": 28,
+                "isActive": None,
+            },
+        ]
+        assert table.to_pylist() == expected_content
+
     def test_write_parquet_tables_nordic44(self, tmp_path: Path) -> None:
         store = NeatGraphStore.from_oxi_local_store()
         store.write(extractors.RdfFileExtractor(nordic44_knowledge_graph))
