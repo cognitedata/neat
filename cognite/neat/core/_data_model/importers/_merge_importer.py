ed99315d473c9382a5c8709a5b464fad4a494cf3
anders-albert <anders0albert@gmail.com>
2025-05-09 12:42:12 +0200

    refactor: stipulate

---
+++ cognite/neat/core/_data_model/importers/_merge_importer.py
@@ -0,0 +1,27 @@
+from cognite.neat.core._data_model._shared import InputRules, ReadRules
+from cognite.neat.core._data_model.models import DMSInputRules
+
+from ._base import BaseImporter
+
+
+class DMSMergeImporter(BaseImporter):
+    """Merges two importers into data model one.
+    Args:
+        existing: The existing data model.
+        additional: The additional data model to merge.
+    """
+
+    def __init__(self, existing: ReadRules[InputRules], additional: ReadRules[InputRules]):
+        self.existing = existing
+        self.additional = additional
+
+    @property
+    def description(self) -> str:
+        return "Merges two data models into one."
+
+    @classmethod
+    def from_importers(cls, existing: BaseImporter, additional: BaseImporter) -> "DMSMergeImporter":
+        return cls(existing.to_rules(), additional.to_rules())
+
+    def to_rules(self) -> ReadRules[DMSInputRules]:
+        raise NotImplementedError()
