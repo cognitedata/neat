d2180939e9163c3e76bb25a1ce53125f6bb9b607
anders-albert <anders0albert@gmail.com>
2025-05-10 14:25:41 +0200

    tests: extending test cases

--- tests/tests_unit/test_rules/test_importers/test_graph_importer.py
+++ tests/tests_unit/test_rules/test_importers/test_graph_importer.py
@@ -1,9 +1,17 @@
 from collections.abc import Iterable
 
 import pytest
+from rdflib import RDF, RDFS, Literal, Namespace
 
+from cognite.neat.core._constants import DEFAULT_NAMESPACE
 from cognite.neat.core._data_model.importers import GraphImporter
-from cognite.neat.core._data_model.models.information import InformationInputRules
+from cognite.neat.core._data_model.models.information import (
+    InformationInputClass,
+    InformationInputMetadata,
+    InformationInputProperty,
+    InformationInputRules,
+)
+from cognite.neat.core._issues.warnings import NeatValueWarning
 from cognite.neat.core._shared import Triple
 from cognite.neat.core._store import NeatGraphStore
 from tests.data import GraphData
@@ -16,6 +24,67 @@ def graph_importer_test_cases() -> Iterable:
         GraphData.car.TRIPLES,
         car_conceptual,
         id="Car example",
+    )
+
+    instance_space = Namespace("http://example.com/instance_space/")
+    schema_space = DEFAULT_NAMESPACE
+    expected_model = InformationInputRules(
+        metadata=InformationInputMetadata("my_space", "my_model", "v1", "doctrino"),
+        properties=[
+            InformationInputProperty(
+                "Car", "manufacturer", "#N/A", max_count=1, instance_source=schema_space["manufacturer"]
+            ),
+            InformationInputProperty(
+                "Car", "weight", "integer, text", max_count=1, instance_source=schema_space["weight"]
+            ),
+        ],
+        classes=[
+            InformationInputClass("Vehicle"),
+            InformationInputClass("Car", implements="Vehicle", instance_source=schema_space["Car"]),
+        ],
+        prefixes={},
+    )
+
+    my_car = instance_space["my_car"]
+    car_type = schema_space["Car"]
+    my_car2 = instance_space["my_car2"]
+    yield pytest.param(
+        [
+            (car_type, RDFS.subClassOf, schema_space["Vehicle"]),
+            (my_car, RDF.type, car_type),
+            (my_car, schema_space["weight"], Literal(100)),
+            (my_car2, RDF.type, car_type),
+            (my_car2, schema_space["weight"], Literal("105kg")),
+            (my_car2, schema_space["manufacturer"], instance_space["Toyota"]),
+        ],
+        expected_model,
+        id="Parent, multi-value, and unknown value type.",
+    )
+
+
+def graph_importer_warning_test_cases() -> Iterable:
+    empty_model = InformationInputRules(
+        metadata=InformationInputMetadata("my_space", "my_model", "v1", "doctrino"),
+        properties=[],
+        classes=[],
+        prefixes={},
+    )
+    yield pytest.param(
+        [],
+        empty_model,
+        NeatValueWarning("Cannot infer data model. No data found in the graph."),
+        id="Empty graph",
+    )
+    instance_space = Namespace("http://example.com/instance_space/")
+    schema_space = DEFAULT_NAMESPACE
+    yield pytest.param(
+        [
+            (instance_space["car1"], schema_space["wheelCount"], Literal(4)),
+            (instance_space["car2"], schema_space["weight"], Literal(100)),
+        ],
+        empty_model,
+        NeatValueWarning("Cannot infer data model. No RDF.type triples found in the graph."),
+        id="Graph with no RDF.type triples",
     )
 
 
@@ -38,3 +107,20 @@ class TestGraphImporter:
         assert actual.as_verified_rules().dump(exclude=exclude) == expected.as_verified_rules().dump(exclude=exclude), (
             "The rules generated from the graph do not match the expected rules."
         )
+
+    @pytest.mark.parametrize("triples, expected_model, expected_warning", list(graph_importer_warning_test_cases()))
+    def test_graph_importer_warnings(
+        self, triples: list[Triple], expected_model: InformationInputRules, expected_warning: NeatValueWarning
+    ) -> None:
+        store = NeatGraphStore.from_oxi_local_store()
+        store._add_triples(triples, store.default_named_graph)
+        metadata = expected_model.metadata
+        data_model_id = (metadata.space, metadata.external_id, metadata.version)
+        importer = GraphImporter(store, data_model_id)
+
+        with pytest.warns(NeatValueWarning) as record:
+            rules = importer.to_rules()
+            actual = rules.rules
+            assert actual is not None, "Failed to convert graph to rules"
+            assert len(record) == 1
+            assert str(record[0]) == str(expected_warning)
