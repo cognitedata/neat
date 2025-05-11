c412c5f21197f0fbbd74c99c660aed0c60354e6e
anders-albert <anders0albert@gmail.com>
2025-05-11 12:08:45 +0200

    feat: implemented main structure of DMS merging

--- cognite/neat/core/_data_model/transformers/_merge_physical.py
+++ cognite/neat/core/_data_model/transformers/_merge_physical.py
@@ -1,8 +1,19 @@
+from collections.abc import Iterable
 from typing import Literal
 
-from cognite.neat.core._data_model.models import DMSRules
-from cognite.neat.core._data_model.models.dms import DMSContainer, DMSProperty, DMSView
+from cognite.neat.core._data_model.models import DMSRules, SheetList
+from cognite.neat.core._data_model.models.data_types import Enum
+from cognite.neat.core._data_model.models.dms import DMSContainer, DMSEnum, DMSNode, DMSProperty, DMSView
+from cognite.neat.core._data_model.models.entities import (
+    ClassEntity,
+    ContainerEntity,
+    DMSNodeEntity,
+    EdgeEntity,
+    NodeTypeFilter,
+    ViewEntity,
+)
 from cognite.neat.core._data_model.transformers import VerifiedRulesTransformer
+from cognite.neat.core._issues.errors import NeatValueError
 
 
 class MergeDMSRules(VerifiedRulesTransformer[DMSRules, DMSRules]):
@@ -38,7 +49,183 @@ class MergeDMSRules(VerifiedRulesTransformer[DMSRules, DMSRules]):
         return f"Merged with {self.secondary.metadata.as_data_model_id()}"
 
     def transform(self, rules: DMSRules) -> DMSRules:
-        raise NotImplementedError()
+        if self.join in ["primary", "combined"]:
+            output = rules.model_copy(deep=True)
+            secondary_views = {view.view: view for view in self.secondary.views}
+            secondary_properties = {(prop.view, prop.view_property): prop for prop in self.secondary.properties}
+            secondary_containers = {container.container: container for container in self.secondary.containers or []}
+            secondary_nodes = {node.node: node for node in self.secondary.nodes or []}
+            secondary_enum = {(enum.collection, enum.value): enum for enum in self.secondary.enum or []}
+        elif self.join == "secondary":
+            output = self.secondary.model_copy(deep=True)
+            secondary_views = {view.view: view for view in rules.views}
+            secondary_properties = {(prop.view, prop.view_property): prop for prop in rules.properties}
+            secondary_containers = {container.container: container for container in rules.containers or []}
+            secondary_nodes = {node.node: node for node in rules.nodes or []}
+            secondary_enum = {(enum.collection, enum.value): enum for enum in rules.enum or []}
+        else:
+            raise NeatValueError(
+                f"Invalid join strategy: {self.join}. Must be one of ['primary', 'secondary', 'combined']"
+            )
+        merged_views_by_id = self._merge_views(output.views, secondary_views)
+        output.views = SheetList[DMSView](merged_views_by_id.values())
+
+        merged_properties = self._merge_properties(
+            output.properties, secondary_properties, set(merged_views_by_id.keys())
+        )
+        output.properties = SheetList[DMSProperty](merged_properties.values())
+
+        used_containers = {prop.container for prop in output.properties if prop.container}
+        merged_containers = self._merge_containers(output.containers or [], secondary_containers, used_containers)
+        output.containers = SheetList[DMSContainer](merged_containers.values())
+
+        used_nodes = self._get_used_nodes(output.views, output.properties)
+        merged_nodes = self._merge_nodes(output.nodes or [], secondary_nodes, used_nodes)
+        output.nodes = SheetList[DMSNode](merged_nodes.values())
+
+        used_enum_collections = self._get_used_enum_collections(output.properties)
+        merged_enum = self._merge_enum(output.enum or [], secondary_enum, used_enum_collections)
+        output.enum = SheetList[DMSEnum](merged_enum.values())
+
+        return output
+
+    @property
+    def _swap_priority(self) -> bool:
+        """We swap the priority if 'join' and 'priority' are mismatched. For example, if
+        we use a 'primary' join strategy, i.e., selecting classes from the primary model, but prioritize the
+        secondary classes that matches the primary classes.
+        """
+
+        return (self.priority == "secondary" and (self.join in ["primary", "combined"])) or (
+            self.priority == "primary" and (self.join == "secondary")
+        )
+
+    def _merge_views(
+        self,
+        primary_views: Iterable[DMSView],
+        secondary_views: dict[ViewEntity, DMSView],
+    ) -> dict[ViewEntity, DMSView]:
+        merged_views = {view.view: view for view in primary_views}
+        for view, primary in merged_views.items():
+            if view not in secondary_views:
+                continue
+            secondary = secondary_views[view]
+            if self._swap_priority:
+                primary, secondary = primary, secondary
+            merged_views[view] = self.merge_views(primary, secondary)
+
+        if self.join == "combined":
+            for view, secondary in secondary_views.items():
+                if view in merged_views:
+                    continue
+                merged_views[view] = secondary
+        return merged_views
+
+    def _merge_properties(
+        self,
+        primary_properties: Iterable[DMSProperty],
+        secondary_properties: dict[tuple[ViewEntity, str], DMSProperty],
+        used_views: set[ViewEntity],
+    ) -> dict[tuple[ViewEntity, str], DMSProperty]:
+        merged_properties = {(prop.view, prop.view_property): prop for prop in primary_properties}
+        for (view, prop), primary in merged_properties.items():
+            if ((view, prop) not in secondary_properties) or (view not in used_views):
+                continue
+            secondary = secondary_properties[(view, prop)]
+            if self._swap_priority:
+                primary, secondary = secondary, primary
+            merged_properties[(view, prop)] = self.merge_properties(primary, secondary)
+
+        if self.join == "combined":
+            for (view, prop), secondary in secondary_properties.items():
+                if ((view, prop) not in merged_properties) and view in used_views:
+                    merged_properties[(view, prop)] = secondary
+        return merged_properties
+
+    def _merge_containers(
+        self,
+        primary_containers: Iterable[DMSContainer],
+        secondary_containers: dict[ContainerEntity, DMSContainer],
+        used_containers: set[ContainerEntity],
+    ) -> dict[ContainerEntity, DMSContainer]:
+        merged_containers = {container.container: container for container in primary_containers}
+        for container, primary in merged_containers.items():
+            if (container not in secondary_containers) or (container not in used_containers):
+                continue
+            secondary = secondary_containers[container]
+            if self._swap_priority:
+                primary, secondary = secondary, primary
+            merged_containers[container] = self.merge_containers(primary, secondary)
+
+        if self.join == "combined":
+            for container, secondary in secondary_containers.items():
+                if (container not in merged_containers) and (container in used_containers):
+                    merged_containers[container] = secondary
+        return merged_containers
+
+    @staticmethod
+    def _get_used_nodes(views: SheetList[DMSView], properties: SheetList[DMSProperty]) -> set[DMSNodeEntity]:
+        """Get the set of used nodes from views and properties."""
+        used_nodes: set[DMSNodeEntity] = set()
+        for view in views:
+            if isinstance(view.filter_, NodeTypeFilter):
+                used_nodes.update(view.filter_.inner or [])
+        for prop in properties:
+            if isinstance(prop.connection, EdgeEntity):
+                if prop.connection.edge_type:
+                    used_nodes.add(prop.connection.edge_type)
+        return used_nodes
+
+    def _merge_nodes(
+        self,
+        primary_nodes: Iterable[DMSNode],
+        secondary_nodes: dict[DMSNodeEntity, DMSNode],
+        used_nodes: set[DMSNodeEntity],
+    ) -> dict[DMSNodeEntity, DMSNode]:
+        merged_nodes = {node.node: node for node in primary_nodes}
+        for node, primary in merged_nodes.items():
+            if (node not in secondary_nodes) or (node not in used_nodes):
+                continue
+            secondary = secondary_nodes[node]
+            if self._swap_priority:
+                primary, secondary = secondary, primary
+            merged_nodes[node] = self.merge_nodes(primary, secondary)
+
+        if self.join == "combined":
+            for node, secondary in secondary_nodes.items():
+                if (node not in merged_nodes) and (node in used_nodes):
+                    merged_nodes[node] = secondary
+        return merged_nodes
+
+    @staticmethod
+    def _get_used_enum_collections(properties: SheetList[DMSProperty]) -> set[ClassEntity]:
+        """Get the set of used enum collections from properties."""
+        used_enum_collections: set[ClassEntity] = set()
+        for prop in properties:
+            if isinstance(prop.value_type, Enum):
+                used_enum_collections.add(prop.value_type.collection)
+        return used_enum_collections
+
+    def _merge_enum(
+        self,
+        primary_enum: Iterable[DMSEnum],
+        secondary_enum: dict[tuple[ClassEntity, str], DMSEnum],
+        used_enum_collections: set[ClassEntity],
+    ) -> dict[tuple[ClassEntity, str], DMSEnum]:
+        merged_enum = {(enum.collection, enum.value): enum for enum in primary_enum}
+        for (collection, value), primary in merged_enum.items():
+            if ((collection, value) not in secondary_enum) or (collection not in used_enum_collections):
+                continue
+            secondary = secondary_enum[(collection, value)]
+            if self._swap_priority:
+                primary, secondary = secondary, primary
+            merged_enum[(collection, value)] = self.merge_enum(primary, secondary)
+
+        if self.join == "combined":
+            for (collection, value), secondary in secondary_enum.items():
+                if ((collection, value) not in merged_enum) and (collection in used_enum_collections):
+                    merged_enum[(collection, value)] = secondary
+        return merged_enum
 
     @classmethod
     def merge_properties(
@@ -63,4 +250,20 @@ class MergeDMSRules(VerifiedRulesTransformer[DMSRules, DMSRules]):
         primary: DMSContainer,
         secondary: DMSContainer,
     ) -> DMSContainer:
+        raise NotImplementedError()
+
+    @classmethod
+    def merge_nodes(
+        cls,
+        primary: DMSNode,
+        secondary: DMSNode,
+    ) -> DMSNode:
+        raise NotImplementedError()
+
+    @classmethod
+    def merge_enum(
+        cls,
+        primary: DMSEnum,
+        secondary: DMSEnum,
+    ) -> DMSEnum:
         raise NotImplementedError()
