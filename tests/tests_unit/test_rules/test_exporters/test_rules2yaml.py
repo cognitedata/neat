077c1b86ab4a79e5c8906f186b4c89bac2edcf0b
Rogerio Saboia Júnior <rogerio.junior@cognite.com>
2025-05-13 09:06:50 +0200

    Refactor YAMLImporter to Dict Importer (#1168)
    # Description
    
    Refactoring YAMLImporter into DictImporter.This refactor enables to have
    other "dict like" importers such as plain JSON files, and also Python
    dict (not reading from a static file).
    
    Also, It introduces a base clase for file read in the importers, which
    can also be reused later on for spreadsheets or other sources.
    
    I am skipping a version bump for now as there is no change to the end
    users, but the upcoming PRs will introduce new functionalites which will
    be possible because of this refactor
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip
    
    ## Changelog
    ### Improved
    - Refactoring YAMLImporter into a generic DictImporter
    - Having a BaseFileReader class so we can reuse this later

--- tests/tests_unit/test_rules/test_exporters/test_rules2yaml.py
+++ tests/tests_unit/test_rules/test_exporters/test_rules2yaml.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from cognite.neat.core._data_model.exporters import YAMLExporter
-from cognite.neat.core._data_model.importers import YAMLImporter
+from cognite.neat.core._data_model.importers import DictImporter
 from cognite.neat.core._data_model.models import ConceptualDataModel, DMSRules
 
 
@@ -9,7 +9,7 @@ class TestYAMLExporter:
     def test_export_import_rules(self, alice_rules: DMSRules, tmp_path: Path) -> None:
         exporter = YAMLExporter(files="single", output="yaml")
         exporter.export_to_file(alice_rules, tmp_path / "tmp.yaml")
-        importer = YAMLImporter.from_file(tmp_path / "tmp.yaml")
+        importer = DictImporter.from_yaml_file(tmp_path / "tmp.yaml")
 
         recreated_rules = importer.to_rules().rules.as_verified_rules()
 
@@ -18,7 +18,7 @@ class TestYAMLExporter:
     def test_export_import_information_rules(self, david_rules: ConceptualDataModel, tmp_path: Path) -> None:
         exporter = YAMLExporter(files="single", output="yaml")
         exporter.export_to_file(david_rules, tmp_path / "tmp.yaml")
-        importer = YAMLImporter.from_file(tmp_path / "tmp.yaml")
+        importer = DictImporter.from_yaml_file(tmp_path / "tmp.yaml")
 
         recreated_rules = importer.to_rules().rules.as_verified_rules()
 
