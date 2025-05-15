42ffdc07110280e4fccfd5475b5278a64a044643
anders-albert <anders0albert@gmail.com>
2025-05-15 18:05:23 +0200

    tests: added some naughty URLs

--- tests/tests_unit/test_utils/test_rdf.py
+++ tests/tests_unit/test_utils/test_rdf.py
@@ -12,7 +12,19 @@ class TestURIInstanceToDisplayName:
             (URIRef("http://example.com/namespace#instance"), "instance"),
             (URIRef("http://example.com/namespace#"), ""),
             (Namespace(DEFAULT_SPACE_URI.format(space="my_space"))["myInstance"], "my_space:myInstance"),
+            (URIRef("http://example.com/namespace#instance"), "instance"),
+            (URIRef(" http://example.com/namespace#"), ""),
+            (URIRef("http://example.com/namespace"), "namespace"),
+            (URIRef("http://example.com/namespace#instance/subinstance"), "instance/subinstance"),
+            (URIRef("http://example.com/namespace#instance?query=param"), "instance?query=param"),
+            (URIRef("http://example.com/namespace#instance#fragment"), "instance#fragment"),
+            (URIRef("http://example.com/namespace#instance%20with%20spaces"), "instance with spaces"),
+            (
+                URIRef("http://example.com/namespace#instance_with_special_chars!@#$%^&*()"),
+                "instance_with_special_chars!@#$%^&*()",
+            ),
+            (URIRef("http://example.com/namespace#instance_with_unicode_✓"), "instance_with_unicode_✓"),
         ],
     )
-    def test_to_display_name(self, uri: URIRef, expected: str) -> None:
+    def test_uri_to_cdf_id(self, uri: URIRef, expected: str) -> None:
         assert uri_to_cdf_id(uri) == expected
