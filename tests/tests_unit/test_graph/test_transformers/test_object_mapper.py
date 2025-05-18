1af7acf0fc0c75474832ac608d72d3cfb570519e
anders-albert <anders0albert@gmail.com>
2025-05-18 17:20:57 +0200

    tests: extend test

--- tests/tests_unit/test_graph/test_transformers/test_object_mapper.py
+++ tests/tests_unit/test_graph/test_transformers/test_object_mapper.py
@@ -1,6 +1,7 @@
 from rdflib import RDF, XSD, Literal, Namespace
 
 from cognite.neat.core._instances.transformers import ObjectMapper
+from cognite.neat.core._issues.warnings import NeatValueWarning
 from cognite.neat.core._store import NeatInstanceStore
 
 
@@ -10,18 +11,27 @@ class TestObjectMapper:
 
         namespace = Namespace("http://example.com/")
         id_ = namespace["MyEvent"]
+        other_event = namespace["OtherEvent"]
         store._add_triples(
             [
                 (id_, RDF.type, namespace["Event"]),
                 (id_, namespace["type"], Literal("モ語5速ち")),
+                (other_event, RDF.type, namespace["Event"]),
+                (id_, namespace["type"], Literal("missing-in-mapping")),
             ],
             named_graph=store.default_named_graph,
         )
         mapping = {"モ語5速ち": "aliquam sed"}
         transformer = ObjectMapper(mapping, namespace["type"])
 
-        store.transform(transformer)
-        assert set(store.queries.select.list_triples(limit=3)) == {
+        issues = store.transform(transformer)
+        assert set(store.queries.select.list_triples(limit=5)) == {
             (id_, RDF.type, namespace["Event"]),
             (id_, namespace["type"], Literal("aliquam sed", datatype=XSD.string)),
+            (other_event, RDF.type, namespace["Event"]),
+            (id_, namespace["type"], Literal("missing-in-mapping", datatype=XSD.string)),
         }
+        assert len(issues) == 1
+        assert issues[0] == NeatValueWarning(
+            "MyEvent could not map type: 'missing-in-mapping'. It does not exist in the given mapping."
+        )
