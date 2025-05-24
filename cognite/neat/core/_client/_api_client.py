f032321303af13ec89dc5b1206f1fdfc9ad27ee4
anders-albert <anders0albert@gmail.com>
2025-05-24 19:33:12 +0200

    refactor: stipulate deploy method

--- cognite/neat/core/_client/_api_client.py
+++ cognite/neat/core/_client/_api_client.py
@@ -1,4 +1,8 @@
+from collections.abc import Sequence
+from typing import Literal, TypeAlias
+
 from cognite.client import ClientConfig, CogniteClient
+from cognite.client.data_classes._base import CogniteResource
 
 from cognite.neat.core._utils.auth import _CLIENT_NAME
 
@@ -7,6 +11,9 @@ from ._api.location_filters import LocationFiltersAPI
 from ._api.neat_instances import NeatInstancesAPI
 from ._api.schema import SchemaAPI
 from ._api.statistics import StatisticsAPI
+from .data_classes.deploy_result import DeployResult
+
+ExistingResource: TypeAlias = Literal["skip", "fail", "update", "force", "recreate"]
 
 
 class NeatClient(CogniteClient):
@@ -21,3 +28,31 @@ class NeatClient(CogniteClient):
         self.instances = NeatInstancesAPI(self)
         self.instance_statistics = StatisticsAPI(self._config, self._config.api_subversion, self)
         self.location_filters = LocationFiltersAPI(self._config, self._API_VERSION, self)
+
+    def deploy(
+        self,
+        resource: CogniteResource | Sequence[CogniteResource],
+        existing: ExistingResource,
+        dry_run: bool = False,
+        restore: bool = False,
+    ) -> DeployResult:
+        """Deploy a resource or a sequence of resources to CDF.
+
+        Args:
+            resource: The resource or resources to deploy.
+            existing: How to handle existing resources. Options are "skip", "fail", "update", "force", and "recreate".
+            dry_run: If True, only simulate the deployment without making any changes.
+            restore: If True, restore the resource if it fails to deploy. This is only applicable if
+                `existing` is set to "update.
+
+        ... note::
+
+        - "fail": Raise an error if the resource already exists.
+        - "skip": Skip the resource if it already exists.
+        - "update": Update the resource if it already exists. This has different behavior depending
+            on the resource type.
+        - "force": Tries to update the resource, but if it fails, it will recreate the resource.
+        - "recreate": Recreate the resource if it already exists, regardless of its current state.
+
+        """
+        raise NotImplementedError()
