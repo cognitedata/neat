5fc8fb4aa1501f44ec46fb8a013988941ae49711
anders-albert <anders0albert@gmail.com>
2025-05-25 18:00:29 +0200

    fix: styling

--- cognite/neat/session/notebook_visualization/deploy_report.py
+++ cognite/neat/session/notebook_visualization/deploy_report.py
@@ -135,11 +135,7 @@ def display_deploy_result(deploy_result: DeployResult) -> HTML:
                 left: 0;
                 right: 0;
                 bottom: 0;
-                background: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 100 100"><defs><pattern id="grid" width="10" height="10"
-                patternUnits="userSpaceOnUse"><path d="M 10 0 L 0 0 0 10" fill="none"
-                 stroke="rgba(255,255,255,0.05)" stroke-width="0.5"/></pattern></defs><rect
-                 width="100" height="100" fill="url(%23grid)"/></svg>');
+                background: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><pattern id="grid" width="10" height="10"  patternUnits="userSpaceOnUse"><path d="M 10 0 L 0 0 0 10" fill="none" stroke="rgba(255,255,255,0.05)" stroke-width="0.5"/></pattern></defs><rect width="100" height="100" fill="url(%23grid)"/></svg>');
                 opacity: 0.3;
             }}
 
