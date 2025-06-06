ae1052c3494a2214f1c4a159e6c96eb660983b8d
nikokaoja <nikola.vasiljevic@cognite.com>
2025-06-06 12:46:06 +0200

    implement feedbacks

--- cognite/neat/plugins/data_model/importers/_base.py
+++ cognite/neat/plugins/data_model/importers/_base.py
@@ -4,8 +4,10 @@ from cognite.neat.core._data_model.importers._base import BaseImporter
 
 
 class DataModelImporterPlugin:
-    def __init__(self) -> None:
-        pass
+    """This class is used an interface for data model import plugins.
+    Any plugin that is used for importing data models should inherit from this class.
+    It is expected to implement the `configure` method which returns a configured
+    """
 
     def configure(self, io: Any, **kwargs: Any) -> BaseImporter:
         """Return a configure plugin for data model import.
