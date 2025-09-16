1c50ed2d36bf4dacb4e4a9f8fb034c84875f915c
nikokaoja <nikola.vasiljevic@cognite.com>
2025-09-16 10:14:22 +0200

    add new object oriented endpoint

--- tests/tests_integration/test_session/test_create_enterprise.py
+++ tests/tests_integration/test_session/test_create_enterprise.py
@@ -27,3 +27,26 @@ class TestCreateAPI:
 
         missing = expected_sheets - actual_sheets
         assert not missing, f"Missing sheets: {missing}"
+
+    def test_create_enterprise_maintain_reference_new_endpoint(
+        self, cognite_client: CogniteClient, tmp_path: Path
+    ) -> None:
+        neat = NeatSession(cognite_client)
+        neat.read.examples.pump_example()
+
+        neat.template.enterprise_model(("my_space", "MySpace", "v1"))
+
+        result_path = tmp_path / "result.xlsx"
+        neat.data_model.write.excel(result_path, include_reference=True, include_properties="same-space")
+
+        workbook: Workbook | None = None
+        try:
+            workbook = load_workbook(result_path)
+            actual_sheets = set(workbook.sheetnames)
+        finally:
+            workbook.close()
+        expected_sheets = {"Metadata", "Views", "Properties", "Containers", "Enum", "Nodes"}
+        expected_sheets |= {f"Ref{name}" for name in expected_sheets}
+
+        missing = expected_sheets - actual_sheets
+        assert not missing, f"Missing sheets: {missing}"
