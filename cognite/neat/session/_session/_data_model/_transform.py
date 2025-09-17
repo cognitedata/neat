dfc9e6ff01547942503d0e1e167b3a3d09a6159d
nikokaoja <nikola.vasiljevic@cognite.com>
2025-09-17 13:19:17 +0200

    call is only for external plugins now

--- cognite/neat/session/_session/_data_model/_transform.py
+++ cognite/neat/session/_session/_data_model/_transform.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal
+from typing import Any
 
 from cognite.neat.core._data_model.transformers._converters import (
     PrefixEntities,
@@ -7,15 +7,10 @@ from cognite.neat.core._data_model.transformers._converters import (
     ToCompliantEntities,
 )
 from cognite.neat.core._issues._base import IssueList
-from cognite.neat.core._utils.auxiliary import filter_kwargs_by_method
 from cognite.neat.plugins._manager import get_plugin_manager
 from cognite.neat.plugins.data_model.transformers._base import DataModelTransformerPlugin
 from cognite.neat.session._state import SessionState
 from cognite.neat.session.exceptions import session_class_wrapper
-
-InternalTransformerName = Literal[
-    "cdf_compliant_external_ids", "prefix", "standardize_naming", "standardize_space_and_version"
-]
 
 
 @session_class_wrapper
@@ -24,12 +19,11 @@ class TransformAPI:
         self._state = state
 
     def __call__(self, name: str, **kwargs: Any) -> IssueList:
-        """Provides access to internal data model transformers and external data model
-        transformer plugins.
+        """Provides access to external data model transformers provided as python
+        plugins.
 
         Args:
-            name (str): The name of format (e.g. Excel) transformer is handling.
-            io (str | Path | | DataModelIdentifier | None): The input/output interface for the transformer.
+            name (str): The name of transformer.
             **kwargs (Any): Additional keyword arguments for the transformer.
 
         !!! note "kwargs"
@@ -37,32 +31,13 @@ class TransformAPI:
             what keyword arguments are supported.
         """
 
-        # Clean the input name once before matching.
-        clean_name: InternalTransformerName | str = name.strip().lower()
-
-        # The match statement cleanly handles each case.
-        match clean_name:
-            case "cdf_compliant_external_ids":
-                return self.cdf_compliant_external_ids()
-
-            case "prefix":
-                return self.prefix(**filter_kwargs_by_method(kwargs, self.prefix))
-
-            case "standardize_naming":
-                return self.standardize_naming()
-
-            case "standardize_space_and_version":
-                return self.standardize_space_and_version()
-
-            case _:  # The wildcard '_' acts as the default 'else' case.
-                return self._plugin(name, **kwargs)
+        return self._plugin(name, **kwargs)
 
     def _plugin(self, name: str, **kwargs: Any) -> IssueList:
-        """Provides access to the external plugins for data model importing.
+        """Provides access to the external plugins for data model transformations.
 
         Args:
-            name (str): The name of format (e.g. Excel) plugin is handling.
-            io (str | Path | None): The input/output interface for the plugin.
+            name (str): The name of transformer.
             **kwargs (Any): Additional keyword arguments for the plugin.
 
         !!! note "kwargs"
