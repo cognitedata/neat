fdb0d641765b4c8d63690540d7674a4a63810375
anders-albert <anders0albert@gmail.com>
2025-09-21 14:22:37 +0200

    tests: MyPy happy

--- tests/tests_unit_v1/test_data_model/test_models/test_dms/test_resources.py
+++ tests/tests_unit_v1/test_data_model/test_models/test_dms/test_resources.py
@@ -1,22 +1,20 @@
-from collections.abc import Iterable
-
 import pytest
+from polyfactory.factories.pydantic_factory import ModelFactory
 
 from cognite.neat.core._utils.auxiliary import get_concrete_subclasses
-from cognite.neat.data_model.models.dms import WriteableResource, Resource
-from polyfactory.factories.pydantic_factory import ModelFactory
+from cognite.neat.data_model.models.dms import Resource, WriteableResource
 
 
 def all_concrete_resources() -> list[type[WriteableResource]]:
-    return get_concrete_subclasses(WriteableResource, exclude_ABC_base=True)
+    return get_concrete_subclasses(WriteableResource, exclude_ABC_base=True)  # type: ignore[type-abstract]
+
 
 class TestAsRequest:
-    @pytest.mark.parametrize(
-        "resource_cls", all_concrete_resources()
-    )
+    @pytest.mark.parametrize("resource_cls", all_concrete_resources())
     def test_writeable_resource_as_request(self, resource_cls: type[WriteableResource]) -> None:
-        class ResourceFactory(ModelFactory[resource_cls]): ...
-        instance = ResourceFactory.build()
-        request_instance = instance.as_request()
-        assert isinstance(request_instance, Resource)
+        class ResourceFactory(ModelFactory[resource_cls]): ...  # type: ignore[valid-type]
 
+        instance = ResourceFactory.build()
+        # MyPy fails to understand that instance is a WriteableResource and not a type[WriteableResource]
+        request_instance = instance.as_request()  # type: ignore[attr-defined]
+        assert isinstance(request_instance, Resource)
