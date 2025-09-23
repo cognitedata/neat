00ac09d734bb576f226d10a86d8a7458cf511ee9
anders-albert <anders0albert@gmail.com>
2025-09-23 16:45:21 +0200

    refctor: revert

--- cognite/neat/_data_model/models/dms/_constants.py
+++ cognite/neat/_data_model/models/dms/_constants.py
@@ -1,6 +1,2 @@
+SPACE_FORMAT_PATTERN = r"[a-zA-Z][a-zA-Z0-9_-]{0,41}[a-zA-Z0-9]?$"
 FORBIDDEN_SPACES = frozenset(["space", "cdf", "dms", "pg3", "shared", "system", "node", "edge"])
-
-SPACE_FORMAT_PATTERN = (
-    rf"^(?!({'|'.join(FORBIDDEN_SPACES)})$)"  # ban exact matches
-    r"[a-zA-Z][a-zA-Z0-9_-]{0,41}[a-zA-Z0-9]?$"
-)
