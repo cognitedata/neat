cb6f09cfe3e295ff989f03f2f945944116fc6df7
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-09-23 19:51:13 +0200

    [THIS-729] 🐈‍⬛ Add space pydantic (#1283)
    # Description
    
    **Context**: We are changing the internal structure of the neat data
    models. We are dropping the physical data model format in Neat, and
    instead will have a one-to-one DMS format identical to the API. In
    principle, we could use the data classes that are in the `cognite-sdk`,
    but we decided to reimplement them in `pydantic`. This is to 1. Get
    better validation. 2. Better performance on serialization (pydantic does
    serialization in rust), 3. Make them easy to extend and connect to the
    Neat Conceptual model.
    
    This PR implements Pydantic classes for Space. 
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ cognite/neat/_data_model/models/dms/_space.py
@@ -0,0 +1,44 @@
+from abc import ABC
+
+from pydantic import Field, field_validator
+
+from cognite.neat.core._utils.text import humanize_collection
+
+from ._base import WriteableResource
+from ._constants import FORBIDDEN_SPACES, SPACE_FORMAT_PATTERN
+
+
+class Space(WriteableResource["SpaceRequest"], ABC):
+    space: str = Field(
+        description="The Space identifier (id).",
+        min_length=1,
+        max_length=43,
+        pattern=SPACE_FORMAT_PATTERN,
+    )
+    name: str | None = Field(None, description="Name of the space.", max_length=1024)
+    description: str | None = Field(None, description="The description of the space.", max_length=255)
+
+    @field_validator("space")
+    def check_forbidden_space_value(cls, val: str) -> str:
+        """Check the space name not present in forbidden set"""
+        if val in FORBIDDEN_SPACES:
+            raise ValueError(f"{val!r} is a reserved space. Reserved Spaces: {humanize_collection(FORBIDDEN_SPACES)}")
+        return val
+
+    def as_request(self) -> "SpaceRequest":
+        return SpaceRequest.model_validate(self.model_dump(by_alias=True))
+
+
+class SpaceResponse(Space):
+    created_time: int = Field(
+        description="When the space was created. The number of milliseconds since 00:00:00 Thursday, 1 January 1970, "
+        "Coordinated Universal Time (UTC), minus leap seconds."
+    )
+    last_updated_time: int = Field(
+        description="When the space was last updated. The number of milliseconds since 00:00:00 Thursday, "
+        "1 January 1970, Coordinated Universal Time (UTC), minus leap seconds."
+    )
+    is_global: bool = Field(description="Whether the space is a global space.")
+
+
+class SpaceRequest(Space): ...
