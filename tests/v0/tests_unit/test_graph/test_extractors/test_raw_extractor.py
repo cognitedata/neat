bd8c24486d0403752ad820a6fcfcaa1d36a37aa7
nikokaoja <nikola.vasiljevic@cognite.com>
2025-09-26 19:30:18 +0200

    extend test for URIrefs

--- tests/v0/tests_unit/test_graph/test_extractors/test_raw_extractor.py
+++ tests/v0/tests_unit/test_graph/test_extractors/test_raw_extractor.py
@@ -9,10 +9,16 @@ class TestRAWExtractor:
     def test_extract_triples(self) -> None:
         with monkeypatch_neat_client() as client:
             client.raw.rows.return_value = [
-                Row(key="key1", columns={"column1": "value1", "column2": "100", "column3": "1983-01-22"}),
-                Row(key="key2", columns={"column1": "value3", "column2": "200", "column3": "1983-01-22"}),
+                Row(
+                    key="key1",
+                    columns={"column1": "value1", "column2": "100", "column3": "1983-01-22", "column4": "key2"},
+                ),
+                Row(
+                    key="key2",
+                    columns={"column1": "value3", "column2": "200", "column3": "1983-01-22", "column4": "key1"},
+                ),
             ]
-        extractor = RAWExtractor(client, "my_db", "my_table", str_to_ideal_type=True)
+        extractor = RAWExtractor(client, "my_db", "my_table", str_to_ideal_type=True, foreign_keys={"column4"})
         ns = extractor.namespace
 
         triples = set(extractor.extract())
@@ -22,8 +28,10 @@ class TestRAWExtractor:
             (ns["key1"], ns["column1"], Literal("value1")),
             (ns["key1"], ns["column2"], Literal("100", datatype=XSD.integer)),
             (ns["key1"], ns["column3"], Literal("1983-01-22T00:00:00", datatype=XSD.dateTime)),
+            (ns["key1"], ns["column4"], ns["key2"]),
             (ns["key2"], RDF.type, ns["my_table"]),
             (ns["key2"], ns["column1"], Literal("value3")),
             (ns["key2"], ns["column2"], Literal("200", datatype=XSD.integer)),
             (ns["key2"], ns["column3"], Literal("1983-01-22T00:00:00", datatype=XSD.dateTime)),
+            (ns["key2"], ns["column4"], ns["key1"]),
         }
