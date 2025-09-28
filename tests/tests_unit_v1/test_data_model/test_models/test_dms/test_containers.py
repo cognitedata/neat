91c42579cb0ba3d23c2e3bb1b2e7f0d6b190e7e7
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-09-28 11:24:32 +0200

    [THIS-730] 🐋 Add container pydantic (#1284)
    # Description
    
    <del>Stacked on #1283, #1285</del>
    
    **Context**: We are changing the internal structure of the neat data
    models. We are dropping the physical data model format in Neat, and
    instead will have a one-to-one DMS format identical to the API. In
    principle, we could use the data classes that are in the `cognite-sdk`,
    but we decided to reimplement them in `pydantic`. This is to 1. Get
    better validation. 2. Better performance on serialization (pydantic does
    serialization in rust), 3. Make them easy to extend and connect to the
    Neat Conceptual model.
    
    This PR implements the Pydantic class for Container. 
    
    Note this PR has very few tests. This is because this an implementation
    of the API in pydantic. Having a lot of tests of the
    serialization/deserialization is essentially testing that `pydantic`
    functionality. What makes sense to test is the error messages, and
    ensure that they are adapted to human readable messages as the Neat user
    base is data modelers and not necessarily developers. I have moved that
    logic and testing out to #1286.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ tests/tests_unit_v1/test_data_model/test_models/test_dms/test_containers.py
@@ -0,0 +1,42 @@
+from typing import get_args
+
+from cognite.neat._data_model.models.dms import (
+    Constraint,
+    ConstraintDefinition,
+    DataType,
+    Index,
+    IndexDefinition,
+    PropertyTypeDefinition,
+)
+from cognite.neat._utils.auxiliary import get_concrete_subclasses
+from cognite.neat._utils.text import humanize_collection
+
+
+def test_all_indices_are_in_union() -> None:
+    all_indices = get_concrete_subclasses(IndexDefinition, exclude_direct_abc_inheritance=True)
+    all_union_indices = get_args(Index.__args__[0])
+    missing = set(all_indices) - set(all_union_indices)
+    assert not missing, (
+        f"The following IndexDefinition subclasses are "
+        f"missing from the Index union: {humanize_collection([cls.__name__ for cls in missing])}"
+    )
+
+
+def test_all_constraints_are_in_union() -> None:
+    all_constraints = get_concrete_subclasses(ConstraintDefinition, exclude_direct_abc_inheritance=True)
+    all_union_constraints = get_args(Constraint.__args__[0])
+    missing = set(all_constraints) - set(all_union_constraints)
+    assert not missing, (
+        f"The following ConstraintDefinition subclasses are "
+        f"missing from the Constraint union: {humanize_collection([cls.__name__ for cls in missing])}"
+    )
+
+
+def test_all_property_types_are_in_union() -> None:
+    all_property_types = get_concrete_subclasses(PropertyTypeDefinition, exclude_direct_abc_inheritance=True)
+    all_union_property_types = get_args(DataType.__args__[0])
+    missing = set(all_property_types) - set(all_union_property_types)
+    assert not missing, (
+        f"The following PropertyTypeDefinition subclasses are "
+        f"missing from the DataType union: {humanize_collection([cls.__name__ for cls in missing])}"
+    )
