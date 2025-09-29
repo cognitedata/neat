c5d058586cf454738c17b9d93efa7ef642c54928
nikokaoja <nikola.vasiljevic@cognite.com>
2025-09-29 11:51:15 +0200

    improve test

--- tests/v0/tests_unit/test_rules/test_exporters/test_rules2ontology.py
+++ tests/v0/tests_unit/test_rules/test_exporters/test_rules2ontology.py
@@ -4,6 +4,7 @@ from rdflib import DCTERMS, OWL, RDF, RDFS, Graph, Literal, Namespace
 
 from cognite.neat.v0.core._data_model.exporters._data_model2semantic_model import OWLExporter, SHACLExporter
 from cognite.neat.v0.core._data_model.models import ConceptualDataModel
+from cognite.neat.v0.core._data_model.models.entities._single_value import ConceptEntity
 
 SHACL = Namespace("http://www.w3.org/ns/shacl#")
 
@@ -38,8 +39,21 @@ class TestOntologyExporter:
         assert len(david_rules.concepts) == len(list(shacl_shapes.subjects(RDF.type, SHACL.NodeShape)))
 
         actual_target_classes = set(shacl_shapes.objects(None, SHACL.targetClass))
-        expected_target_classes = {
+        expected_node = {
             concept.instance_source or david_rules.metadata.namespace[concept.concept.suffix]
             for concept in david_rules.concepts
         }
-        assert actual_target_classes == expected_target_classes
+        assert actual_target_classes == expected_node
+
+        expected_node = set()
+
+        for property_ in david_rules.properties:
+            if isinstance(property_.value_type, ConceptEntity):
+                if property_.instance_source and len(property_.instance_source) == 1:
+                    expected_node.add(property_.instance_source)
+                else:
+                    expected_node.add(david_rules.metadata.namespace[property_.value_type.suffix])
+
+        actual_node = set(shacl_shapes.objects(None, SHACL.node))
+
+        assert actual_node == expected_node
