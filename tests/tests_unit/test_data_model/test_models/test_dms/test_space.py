236a464a16a398b8a93fef79549d262f911bf653
anders-albert <anders0albert@gmail.com>
2025-10-01 15:15:11 +0200

    tests: improve test

--- tests/tests_unit/test_data_model/test_models/test_dms/test_space.py
+++ tests/tests_unit/test_data_model/test_models/test_dms/test_space.py
@@ -1,13 +1,16 @@
+from collections.abc import Callable
+from typing import Any
+
 import hypothesis.strategies as st
-from hypothesis import given
+from hypothesis import given, settings
 
 from cognite.neat._data_model.models.dms import SpaceResponse
 from cognite.neat._data_model.models.dms._constants import SPACE_FORMAT_PATTERN
 
 
 @st.composite
-def space_strategy(draw) -> SpaceResponse:
-    return SpaceResponse(
+def space_strategy(draw: Callable) -> dict[str, Any]:
+    return dict(
         space=draw(st.from_regex(SPACE_FORMAT_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 43)),
         name=draw(st.text(min_size=1, max_size=255)),
         description=draw(st.text(min_size=0, max_size=1024)),
@@ -18,8 +21,10 @@ def space_strategy(draw) -> SpaceResponse:
 
 
 class TestSpaceRequest:
+    @settings(max_examples=1)
     @given(space_strategy())
-    def test_space_as_request(self, space_response: SpaceResponse) -> None:
+    def test_space_as_request(self, space_data: dict[str, Any]) -> None:
+        space_response = SpaceResponse.model_validate(space_data)
         request = space_response.as_request()
         assert request.space == space_response.space
         assert request.name == space_response.name
