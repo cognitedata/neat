2da78ee289516c285aae9baec2437141c4f1f0a8
anders-albert <anders0albert@gmail.com>
2025-10-02 11:32:45 +0200

    tests: added more tests for views

--- tests/tests_unit/test_data_model/test_models/test_dms/test_views.py
+++ tests/tests_unit/test_data_model/test_models/test_dms/test_views.py
@@ -1,18 +1,37 @@
-from collections.abc import Iterator
+from collections.abc import Callable, Iterator
 from typing import Any, get_args
 
+import hypothesis.strategies as st
 import pytest
+from hypothesis import given, settings
 from pydantic import ValidationError
 
 from cognite.neat._data_model.models.dms import (
+    EnumProperty,
+    PropertyTypeDefinition,
     ViewPropertyDefinition,
     ViewRequest,
     ViewRequestProperty,
+    ViewResponse,
     ViewResponseProperty,
+)
+from cognite.neat._data_model.models.dms._constants import (
+    CONTAINER_AND_VIEW_PROPERTIES_IDENTIFIER_PATTERN,
+    DM_EXTERNAL_ID_PATTERN,
+    DM_VERSION_PATTERN,
+    INSTANCE_ID_PATTERN,
+    SPACE_FORMAT_PATTERN,
 )
 from cognite.neat._utils.auxiliary import get_concrete_subclasses
 from cognite.neat._utils.text import humanize_collection
 from cognite.neat._utils.validation import humanize_validation_error
+
+AVAILABLE_PROPERTY_TYPES = [
+    # Skipping enum as it requires special handling
+    subclass.model_fields["type"].default
+    for subclass in get_concrete_subclasses(PropertyTypeDefinition)
+    if subclass is not EnumProperty
+]
 
 
 def test_all_view_properties_are_in_union() -> None:
@@ -252,3 +271,144 @@ class TestViewRequests:
             ViewRequest.model_validate(data)
         errors = set(humanize_validation_error(excinfo.value))
         assert errors == expected_errors
+
+
+@st.composite
+def view_reference(draw: Callable) -> dict[str, Any]:
+    return {
+        "space": draw(st.from_regex(SPACE_FORMAT_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 43)),
+        "externalId": draw(st.from_regex(DM_EXTERNAL_ID_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 255)),
+        "version": draw(st.from_regex(DM_EXTERNAL_ID_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 43)),
+    }
+
+
+@st.composite
+def container_reference(draw: Callable) -> dict[str, Any]:
+    return {
+        "space": draw(st.from_regex(SPACE_FORMAT_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 43)),
+        "externalId": draw(st.from_regex(DM_EXTERNAL_ID_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 255)),
+    }
+
+
+@st.composite
+def node_reference(draw: Callable) -> dict[str, Any]:
+    return {
+        "space": draw(st.from_regex(SPACE_FORMAT_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 43)),
+        "externalId": draw(st.from_regex(INSTANCE_ID_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 255)),
+    }
+
+
+@st.composite
+def container_direct_reference(draw: Callable) -> dict[str, Any]:
+    return {
+        "source": draw(container_reference()),
+        "identifier": draw(st.from_regex(CONTAINER_AND_VIEW_PROPERTIES_IDENTIFIER_PATTERN, fullmatch=True)),
+    }
+
+
+@st.composite
+def edge_property(draw: Callable) -> dict[str, Any]:
+    return dict(
+        connectionType=draw(st.sampled_from(["single_edge_connection", "multi_edge_connection"])),
+        name=draw(st.text(max_size=255)),
+        description=draw(st.text(max_size=1024)),
+        source=draw(view_reference()),
+        type=draw(node_reference()),
+        edgeSource=draw(st.one_of(st.none(), view_reference())),
+        direction=draw(st.sampled_from(["outwards", "inwards"])),
+    )
+
+
+@st.composite
+def reverse_direct_property(draw: Callable) -> dict[str, Any]:
+    return dict(
+        connectionType=draw(st.sampled_from(["single_reverse_direct_relation", "multi_reverse_direct_relation"])),
+        name=draw(st.text(max_size=255)),
+        description=draw(st.text(max_size=1024)),
+        source=draw(view_reference()),
+        through=draw(container_direct_reference()),
+    )
+
+
+@st.composite
+def primary_property(draw: Callable) -> dict[str, Any]:
+    return dict(
+        connectionType="primary_property",
+        name=draw(st.one_of(st.none(), st.text(max_size=255))),
+        description=draw(st.one_of(st.none(), st.text(max_size=1024))),
+        container=draw(container_reference()),
+        containerPropertyIdentifier=draw(
+            st.from_regex(CONTAINER_AND_VIEW_PROPERTIES_IDENTIFIER_PATTERN, fullmatch=True)
+        ),
+        source=draw(st.one_of(st.none(), view_reference())),
+        immutable=draw(st.one_of(st.none(), st.booleans())),
+        nullable=draw(st.one_of(st.none(), st.booleans())),
+        autoIncrement=draw(st.one_of(st.none(), st.booleans())),
+        defaultValue=draw(st.one_of(st.none(), st.text(), st.integers())),
+        type={"type": draw(st.sampled_from(AVAILABLE_PROPERTY_TYPES))},
+        constraintState={"nullability": draw(st.sampled_from(["current", "pending", "failed"]))},
+    )
+
+
+@st.composite
+def view_strategy(draw: Callable) -> dict[str, Any]:
+    # Generate property keys matching the pattern
+    prop_keys = draw(
+        st.lists(
+            st.from_regex(CONTAINER_AND_VIEW_PROPERTIES_IDENTIFIER_PATTERN, fullmatch=True),
+            min_size=1,
+            max_size=5,
+            unique=True,
+        )
+    )
+    properties = {
+        k: draw(st.one_of([edge_property(), reverse_direct_property(), primary_property()])) for k in prop_keys
+    }
+    mapped_containers = [prop["container"] for prop in properties.values() if "container" in prop]
+    return dict(
+        space=draw(st.from_regex(SPACE_FORMAT_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 43)),
+        externalId=draw(st.from_regex(DM_EXTERNAL_ID_PATTERN, fullmatch=True).filter(lambda s: 1 <= len(s) <= 255)),
+        version=draw(st.from_regex(DM_VERSION_PATTERN, fullmatch=True).filter(lambda s: len(s) <= 43)),
+        name=draw(st.one_of(st.none(), st.text(max_size=255))),
+        description=draw(st.one_of(st.none(), st.text(max_size=1024))),
+        usedFor=draw(st.sampled_from(["node", "edge", "all"])),
+        properties=properties,
+        isGlobal=draw(st.booleans()),
+        createdTime=draw(st.integers(min_value=0)),
+        lastUpdatedTime=draw(st.integers(min_value=0)),
+        queryable=draw(st.booleans()),
+        writable=draw(st.booleans()),
+        mappedContainers=mapped_containers,
+    )
+
+
+class TestViewResponse:
+    @settings(max_examples=1)
+    @given(view_strategy())
+    def test_as_request(self, view: dict[str, Any]) -> None:
+        response = ViewResponse.model_validate(view)
+
+        assert isinstance(response, ViewResponse)
+
+        request = response.as_request()
+        assert isinstance(request, ViewRequest)
+
+        # Properties have differences in request and response, so we need to compare them
+        # separately
+        dumped = request.model_dump(exclude={"properties"})
+        response_dumped = response.model_dump(exclude={"properties"})
+        response_only_keys = set(ViewResponse.model_fields.keys()) - set(ViewRequest.model_fields.keys())
+        for keys in response_only_keys:
+            response_dumped.pop(keys, None)
+        assert dumped == response_dumped
+
+        assert list(request.properties.keys()) == list(response.properties.keys())
+        for request_prop, response_prop in zip(request.properties.values(), response.properties.values(), strict=False):
+            dumped_prop = request_prop.model_dump()
+            response_dumped_prop = response_prop.model_dump()
+            response_only_prop_keys = set(type(response_prop).model_fields.keys()) - set(
+                type(request_prop).model_fields.keys()
+            )
+            for keys in response_only_prop_keys:
+                response_dumped_prop.pop(keys, None)
+            assert dumped_prop == response_dumped_prop
