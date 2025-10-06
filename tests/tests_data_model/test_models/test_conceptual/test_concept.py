e69de48670e8e17714417f4d5fa75e31d7c69551
nikokaoja <nikola.vasiljevic@cognite.com>
2025-10-06 14:08:54 +0200

    extend tests

--- tests/tests_data_model/test_models/test_conceptual/test_concept.py
+++ tests/tests_data_model/test_models/test_conceptual/test_concept.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 import pytest
 from pydantic import ValidationError
 
@@ -6,7 +8,42 @@ from cognite.neat._data_model.models.entities import ConceptEntity
 
 
 class TestConcept:
-    def test_concept_cannot_implement_itself(self):
+    def test_happy_path_simple_concept(self) -> None:
+        concept = Concept(space="my_space", external_id="my_concept", version="1.0.0")
+        assert concept.space == "my_space"
+        assert concept.external_id == "my_concept"
+        assert concept.version == "1.0.0"
+        assert concept.implements is None
+
+    def test_happy_path_simple_concept_empty_list_of_implements(self) -> None:
+        concept = Concept(space="my_space", external_id="my_concept", version="1.0.0", implements=[])
+        assert concept.space == "my_space"
+        assert concept.external_id == "my_concept"
+        assert concept.version == "1.0.0"
+        assert concept.implements == []
+
+    def test_happy_path_concept_with_implements(self) -> None:
+        concept = Concept(
+            space="my_space",
+            external_id="my_concept",
+            version="1.0.0",
+            implements=[
+                ConceptEntity(prefix="other_space", suffix="other_concept"),
+                ConceptEntity(prefix="another_space", suffix="another_concept", version="2.0.0"),
+            ],
+        )
+        assert concept.space == "my_space"
+        assert concept.external_id == "my_concept"
+        assert concept.version == "1.0.0"
+        assert len(cast(list[ConceptEntity], concept.implements)) == 2
+        assert cast(list[ConceptEntity], concept.implements)[0].prefix == "other_space"
+        assert cast(list[ConceptEntity], concept.implements)[0].suffix == "other_concept"
+        assert cast(list[ConceptEntity], concept.implements)[0].version is None
+        assert cast(list[ConceptEntity], concept.implements)[1].prefix == "another_space"
+        assert cast(list[ConceptEntity], concept.implements)[1].suffix == "another_concept"
+        assert cast(list[ConceptEntity], concept.implements)[1].version == "2.0.0"
+
+    def test_concept_cannot_implement_itself(self) -> None:
         with pytest.raises(ValidationError) as exc_info:
             Concept(
                 space="my_space",
@@ -19,7 +56,7 @@ class TestConcept:
             )
         assert "A concept cannot implement itself" in str(exc_info.value)
 
-    def test_concept_cannot_have_duplicate_implements(self):
+    def test_concept_cannot_have_duplicate_implements(self) -> None:
         with pytest.raises(ValidationError) as exc_info:
             Concept(
                 space="my_space",
