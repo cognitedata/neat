880a81483070a09bdb44a4dd35e4299316f60537
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-10-13 10:54:19 +0200

    [THIS-776] 🧑‍⚖️ Add table data classes (#1328)
    # Description
    
    Stacked on #1327 <del>and #1326</del>
    
    **Context** We are in the process of reimplementing the core part of
    Neat.
    
    This PR is a a split from #1323. It adds:
    * The pydantic classes used to validate Tables and columns as well as
    parsing the entities.
    * Introduce NeatException and NeatModelImport error. 
    * Creates the skeleton for the DMSTableImporter that will eventually
    support reading from Excel.
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

--- cognite/neat/_utils/useful_types.py
+++ cognite/neat/_utils/useful_types.py
@@ -1,7 +1,11 @@
 from collections.abc import Hashable
+from datetime import date, datetime, time, timedelta
 from typing import TypeAlias, TypeVar
 
 JsonVal: TypeAlias = None | str | int | float | bool | dict[str, "JsonVal"] | list["JsonVal"]
 
 
 T_ID = TypeVar("T_ID", bound=Hashable)
+
+# These are the types that openpyxl supports in cells
+CellValue: TypeAlias = str | int | float | bool | datetime | date | time | timedelta | None
