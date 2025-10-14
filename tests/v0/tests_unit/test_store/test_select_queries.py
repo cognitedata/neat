b6262c1548dbc04b3c7fafa132a141456d31a259
Andrian Gasper <andrian.gasper@cognite.com>
2025-10-14 08:27:20 +0200

    refactor: unify graph diff logic and merge add/delete queries into one generic query

--- tests/v0/tests_unit/test_store/test_select_queries.py
+++ tests/v0/tests_unit/test_store/test_select_queries.py
@@ -26,22 +26,22 @@ class TestListInstanceObjectIds:
         assert result[0] == my_other_entity
 
 
-def test_get_triples_to_delete() -> None:
-    """Test finding triples that exist in old graph but not in new graph"""
+def test_get_graph_diff() -> None:
+    """Test finding triples that exist in current graph but not in new graph"""
     store = NeatInstanceStore.from_oxi_local_store()
     example = Namespace("http://example.org/")
 
-    old_graph = URIRef("urn:test:old")
+    current_graph = URIRef("urn:test:current")
     new_graph = URIRef("urn:test:new")
 
-    # add 3 triples to old graph
+    # add 3 triples to current graph
     store._add_triples(
         [
             (example.subject1, example.pred1, Literal("value1")),
             (example.subject2, example.pred2, Literal("value2")),
             (example.subject3, example.pred3, Literal("value3")),
         ],
-        named_graph=old_graph,
+        named_graph=current_graph,
     )
 
     # add 2 same triples + 1 new to new graph
@@ -54,51 +54,22 @@ def test_get_triples_to_delete() -> None:
         named_graph=new_graph,
     )
 
-    # get triples to delete
-    result = list(store.queries.select.get_triples_to_delete(old_graph, new_graph))
+    # test triples to delete
+    to_delete = list(store.queries.select.get_graph_diff(current_graph, new_graph))
 
-    assert len(result) == 1
-    assert tuple(result[0]) == (
+    assert len(to_delete) == 1
+    assert tuple(to_delete[0]) == (
         example.subject3,
         example.pred3,
         Literal("value3", datatype=XSD.string),
     )
 
+    # test triples to add
+    to_add = list(store.queries.select.get_graph_diff(new_graph, current_graph))
 
-def test_get_triples_to_add() -> None:
-    """Test finding triples that exist in new graph but not in old graph"""
-
-    store = NeatInstanceStore.from_oxi_local_store()
-    example = Namespace("http://example.org/")
-
-    old_graph = URIRef("urn:test:old")
-    new_graph = URIRef("urn:test:new")
-
-    # add 2 triples to old graph
-    store._add_triples(
-        [
-            (example.subject1, example.pred1, Literal("value1")),
-            (example.subject2, example.pred2, Literal("value2")),
-        ],
-        named_graph=old_graph,
-    )
-
-    # add 2 same triples + 1 new to new graph
-    store._add_triples(
-        [
-            (example.subject1, example.pred1, Literal("value1")),
-            (example.subject2, example.pred2, Literal("value2")),
-            (example.subject3, example.pred3, Literal("value3")),
-        ],
-        named_graph=new_graph,
-    )
-
-    # get triples to add
-    result = list(store.queries.select.get_triples_to_add(old_graph, new_graph))
-
-    assert len(result) == 1
-    assert tuple(result[0]) == (
-        example.subject3,
-        example.pred3,
-        Literal("value3", datatype=XSD.string),
+    assert len(to_add) == 1
+    assert tuple(to_add[0]) == (
+        example.subject4,
+        example.pred4,
+        Literal("value4", datatype=XSD.string),
     )
