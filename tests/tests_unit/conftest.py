3cdc6ff659683becd1f3396c097a28d3d448892d
anders-albert <anders0albert@gmail.com>
2025-10-22 15:56:41 +0200

    tests:

--- tests/tests_unit/conftest.py
+++ tests/tests_unit/conftest.py
@@ -1,6 +1,31 @@
 from typing import Any
 
 import pytest
+from cognite.client import ClientConfig
+from cognite.client.credentials import Token
+
+from cognite.neat._client import NeatClient, NeatClientConfig
+
+BASE_URL = "http://neat.cognitedata.com"
+
+
+@pytest.fixture(scope="session")
+def neat_config() -> NeatClientConfig:
+    return NeatClientConfig(
+        ClientConfig(
+            client_name="test-client",
+            project="test-project",
+            base_url=BASE_URL,
+            max_workers=1,
+            timeout=10,
+            credentials=Token("abc"),
+        )
+    )
+
+
+@pytest.fixture(scope="session")
+def neat_client(neat_config: NeatClientConfig) -> NeatClient:
+    return NeatClient(neat_config)
 
 
 @pytest.fixture
