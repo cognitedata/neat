1facc8474b549bfe50c6f185e3a0cbdfb0c0aa63
nikokaoja <nikola.vasiljevic@cognite.com>
2025-10-24 10:36:50 +0200

    improve code, get around python 3.10 issue

--- cognite/neat/_session/_wrappers.py
+++ cognite/neat/_session/_wrappers.py
@@ -1,9 +1,14 @@
 from collections.abc import Callable
-from typing import Any, TypeVar
+from functools import wraps
+from typing import Any, Protocol, TypeVar
 
-from pyparsing import wraps
-
+from cognite.neat._store._store import NeatStore
 from cognite.neat._utils.text import split_on_capitals
+
+
+class HasStore(Protocol):
+    _store: NeatStore
+
 
 T_Class = TypeVar("T_Class", bound=object)
 
@@ -14,17 +19,22 @@ def session_wrapper(cls: type[T_Class]) -> type[T_Class]:
         """Decorator to handle exceptions and print provenance length"""
 
         @wraps(func)
-        def wrapper(self: Any, *args: Any, **kwargs: Any) -> Any:
+        def wrapper(self: HasStore, *args: Any, **kwargs: Any) -> Any:
             try:
                 res = func(self, *args, **kwargs)
-
                 change = self._store.provenance[-1]
+
+                issues_count = len(change.issues) if change.issues else 0
+                errors_count = len(change.errors) if change.errors else 0
+                total_issues = issues_count + errors_count
+
+                newline = "\n"  # python 3.10 compatibility
                 print(
                     f"{' '.join(split_on_capitals(cls.__name__))} - {func.__name__} "
                     f"{'✅' if change.successful else '❌'}"
-                    f" | Issues: {len(change.issues) + len(change.errors) if change.issues or change.errors else 0}"
-                    f" (of which {len(change.errors) if change.errors else 0} critical)"
-                    f"{'\nFor more details run neat.issues()' if change.issues or change.errors else ''}"
+                    f" | Issues: {total_issues}"
+                    f" (of which {errors_count} critical)"
+                    f"{newline + 'For more details run neat.issues()' if change.issues or change.errors else ''}"
                 )
 
                 return res
