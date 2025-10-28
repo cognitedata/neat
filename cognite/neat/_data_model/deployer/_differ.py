780c758c2ac58700abca0961d0a5c44a664265ef
anders-albert <anders0albert@gmail.com>
2025-10-28 13:56:00 +0100

    refactor: in-progress on container differ

--- cognite/neat/_data_model/deployer/_differ.py
+++ cognite/neat/_data_model/deployer/_differ.py
@@ -3,7 +3,7 @@ from typing import Generic
 
 from cognite.neat._data_model.models.dms import T_Item
 
-from .data_classes import PropertyChange
+from .data_classes import AddedProperty, ContainerPropertyChange, PropertyChange, RemovedProperty, SeverityType
 
 
 class ItemDiffer(Generic[T_Item], ABC):
@@ -21,3 +21,54 @@ class ItemDiffer(Generic[T_Item], ABC):
             A list of changes between the two resources.
         """
         raise NotImplementedError()
+
+
+def diff_container(
+    parent_path: str,
+    cdf_items: dict[str, T_Item],
+    desired_items: dict[str, T_Item],
+    add_severity: SeverityType,
+    remove_severity: SeverityType,
+    differ: ItemDiffer[T_Item],
+) -> list[PropertyChange]:
+    """Diff two containers of items.
+
+    A container is for example the properties, constraints, or indexes of a container,
+    properties of a space, views of a data model, etc.
+
+    Args:
+        parent_path: The JSON path to the container being compared.
+        cdf_items: The items as they are in CDF.
+        desired_items: The items as they are desired to be.
+        add_severity: The severity to assign to added items.
+        remove_severity: The severity to assign to removed items.
+        differ: The differ to use for comparing individual items.
+
+    """
+    changes: list[PropertyChange] = []
+    for key, desired_item in desired_items.items():
+        item_path = f"{parent_path}{key}"
+        if key not in cdf_items:
+            changes.append(
+                AddedProperty(
+                    item_severity=add_severity,
+                    field_path=item_path,
+                    new_value=desired_item,
+                )
+            )
+            continue
+        cdf_item = cdf_items[key]
+        diffs = differ.diff(cdf_item, desired_item)
+        if diffs:
+            changes.append(ContainerPropertyChange(field_path=item_path, changed_items=diffs))
+
+    for key, cdf_item in cdf_items.items():
+        if key not in desired_items:
+            changes.append(
+                RemovedProperty(
+                    item_severity=remove_severity,
+                    field_path=f"{parent_path}{key}",
+                    old_value=cdf_item,
+                )
+            )
+    return changes
---
+++ cognite/neat/_data_model/deployer/_differ_container.py
@@ -0,0 +1,84 @@
+from cognite.neat._data_model.models.dms import ContainerPropertyDefinition, ContainerRequest, ConstraintDefinition, IndexDefinition
+
+from .data_classes import (
+    AddedProperty,
+    ContainerPropertyChange,
+    PrimitivePropertyChange,
+    PropertyChange,
+    RemovedProperty,
+    SeverityType,
+)
+from ._differ import ItemDiffer, diff_container
+
+
+class ContainerDiffer(ItemDiffer[ContainerRequest]):
+    def diff(self, cdf_container: ContainerRequest, container: ContainerRequest) -> list[PropertyChange]:
+        changes: list[PropertyChange] = []
+        if cdf_container.name != container.name:
+            changes.append(
+                PrimitivePropertyChange(
+                    item_severity=SeverityType.SAFE,
+                    field_path="name",
+                    old_value=cdf_container.name,
+                    new_value=container.name,
+                )
+            )
+
+        if cdf_container.description != container.description:
+            changes.append(
+                PrimitivePropertyChange(
+                    item_severity=SeverityType.SAFE,
+                    field_path="description",
+                    old_value=cdf_container.description,
+                    new_value=container.description,
+                )
+            )
+        if cdf_container.used_for != container.used_for:
+            changes.append(
+                PrimitivePropertyChange(
+                    item_severity=SeverityType.BREAKING,
+                    field_path="usedFor",
+                    old_value=cdf_container.used_for,
+                    new_value=container.used_for,
+                )
+            )
+        changes.extend(
+            diff_container(
+                "properties.",
+                cdf_container.properties,
+                container.properties,
+                SeverityType.SAFE,
+                SeverityType.BREAKING,
+                ContainerPropertyDiffer(),
+            )
+        )
+        changes.extend(
+            diff_container(
+                "constraints.", cdf_container.constraints, container.constraints,
+                           SeverityType.SAFE, SeverityType.WARNING
+            , ConstraintDiffer()
+            )
+        )
+        changes.extend(diff_container(
+            "indexes.", cdf_container.indexes, container.indexes, SeverityType.SAFE, SeverityType.WARNING, IndexDiffer()))
+
+        return changes
+
+
+class ContainerPropertyDiffer(ItemDiffer[ContainerPropertyDefinition]):
+    def diff(
+        self, cdf_property: ContainerPropertyDefinition, desired_property: ContainerPropertyDefinition
+    ) -> list[PropertyChange]:
+        raise NotImplementedError()
+
+class ConstraintDiffer(ItemDiffer[ConstraintDefinition]):
+    def diff(
+        self, cdf_constraint: ConstraintDefinition, desired_constraint: ConstraintDefinition
+    ) -> list[PropertyChange]:
+        raise NotImplementedError()
+
+class IndexDiffer(ItemDiffer[IndexDefinition]):
+    def diff(
+        self, cdf_index: IndexDefinition, desired_index: IndexDefinition
+    ) -> list[PropertyChange]:
+        raise NotImplementedError()
