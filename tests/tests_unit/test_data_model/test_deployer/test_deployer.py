62cefdb3f6980bc7cbffb160c1adb23224a714de
anders-albert <anders0albert@gmail.com>
2025-10-30 14:45:04 +0100

    tests: added test caes

---
+++ tests/tests_unit/test_data_model/test_deployer/test_deployer.py
@@ -0,0 +1,51 @@
+from typing import Any
+
+import pytest
+import respx
+
+from cognite.neat._client import NeatClient
+from cognite.neat._data_model.deployer import SchemaDeployer
+from cognite.neat._data_model.deployer.data_classes import SchemaSnapshot
+from cognite.neat._data_model.models.dms import RequestSchema
+
+
+@pytest.fixture()
+def model(example_dms_schema: dict[str, Any]) -> RequestSchema:
+    return RequestSchema.model_validate(example_dms_schema)
+
+
+@pytest.fixture()
+def schema_snapshot(
+    neat_client: NeatClient, model: RequestSchema, respx_mock_data_model: respx.MockRouter
+) -> SchemaSnapshot:
+    deployer = SchemaDeployer(client=neat_client)
+    return deployer.fetch_cdf_state(model)
+
+
+class TestSchemaDeployer:
+    def test_fetch_existing_schemas(
+        self, neat_client: NeatClient, model: RequestSchema, schema_snapshot: SchemaSnapshot
+    ) -> None:
+        cdf_schema = schema_snapshot
+        assert set(cdf_schema.data_model) == {model.data_model.as_reference()}
+        assert set(cdf_schema.views) == {view.as_reference() for view in model.views}
+        assert set(cdf_schema.spaces) == {space.as_reference() for space in model.spaces}
+        assert set(cdf_schema.containers) == {container.as_reference() for container in model.containers}
+
+    def test_create_deployment_plan(
+        self, neat_client: NeatClient, model: RequestSchema, schema_snapshot: SchemaSnapshot
+    ) -> None:
+        deployer = SchemaDeployer(neat_client)
+        plan = deployer.create_deployment_plan(schema_snapshot, model)
+        # Basic assertions to ensure plan is created
+        assert len(plan) == 4  # spaces, containers, views, datamodels
+        for resource_plan in plan:
+            assert resource_plan.endpoint in {"spaces", "containers", "views", "datamodels"}
+            assert len(resource_plan.unchanged) == len(resource_plan.resources), (
+                "All resources should be unchanged as we use the same new as current model"
+            )
+
+    def test_deploy(self, neat_client: NeatClient, model: RequestSchema) -> None:
+        deployer = SchemaDeployer(neat_client)
+        with pytest.raises(NotImplementedError):
+            deployer.deploy(model)
