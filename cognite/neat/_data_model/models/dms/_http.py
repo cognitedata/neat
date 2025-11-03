82c330544105d85991014cd0e72c57cca33cb90e
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-11-03 07:04:17 +0100

    [prep THIS-734, THIS-740,THIS-834] 🚢Schema deployer Part 1 (#1374)
    # Description
    
    **Context** This is part of a reimplementation of Neat.
    
    This PR starts on the `SchemaDeployer` which will be used to write data
    models to DMS from neat. It set up the structure + implements the fetch
    and planning par of it.
    
    In Part 2 I will add the `applied_changes` implementation along with the
    recovery logic.
    
    
    ## Bump
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

--- cognite/neat/_data_model/models/dms/_http.py
+++ cognite/neat/_data_model/models/dms/_http.py
@@ -5,12 +5,22 @@ from cognite.neat._utils.useful_types import ReferenceObject
 
 from ._container import ContainerRequest
 from ._data_model import DataModelRequest
+from ._references import (
+    ContainerReference,
+    DataModelReference,
+    SpaceReference,
+    ViewReference,
+)
 from ._space import SpaceRequest
 from ._views import ViewRequest
 
 DataModelResource: TypeAlias = SpaceRequest | DataModelRequest | ViewRequest | ContainerRequest
 
 T_DataModelResource = TypeVar("T_DataModelResource", bound=DataModelResource)
+
+ResourceId: TypeAlias = SpaceReference | DataModelReference | ViewReference | ContainerReference
+
+T_ResourceId = TypeVar("T_ResourceId", bound=ResourceId)
 
 
 class DataModelBody(ItemBody[ReferenceObject, DataModelResource]):
