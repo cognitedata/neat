beabb0ff5437dc2d20afc4585424885ddec587ac
anders-albert <anders0albert@gmail.com>
2025-11-05 06:00:19 +0100

    refactor: expose

--- cognite/neat/_data_model/exporters/__init__.py
+++ cognite/neat/_data_model/exporters/__init__.py
@@ -1,4 +1,12 @@
+from ._api_exporter import DMSAPIExporter, DMSAPIYAMLExporter
 from ._base import DMSExporter
 from ._table_exporter.exporter import DMSExcelExporter, DMSTableExporter, DMSYamlExporter
 
-__all__ = ["DMSExcelExporter", "DMSExporter", "DMSTableExporter", "DMSYamlExporter"]
+__all__ = [
+    "DMSAPIExporter",
+    "DMSAPIYAMLExporter",
+    "DMSExcelExporter",
+    "DMSExporter",
+    "DMSTableExporter",
+    "DMSYamlExporter",
+]
