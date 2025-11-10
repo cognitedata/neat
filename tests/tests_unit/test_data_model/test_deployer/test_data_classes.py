ef54ccef7098c3a401d35c19c0fc1c0e50b18b2e
anders-albert <anders0albert@gmail.com>
2025-11-10 07:09:43 +0100

    tests: moved out fixture for reuse

--- tests/tests_unit/test_data_model/test_deployer/test_data_classes.py
+++ tests/tests_unit/test_data_model/test_deployer/test_data_classes.py
@@ -56,80 +56,84 @@ class TestSeverityType:
         assert result == expected_max
 
 
-class TestResourceDeploymentPlanList:
-    def test_consolidate_resources(self) -> None:
-        current_container = ContainerRequest(
-            space="space1",
-            externalId="container1",
-            name="container1",
-            properties={"prop1": ContainerPropertyDefinition(type=TextProperty())},
-            constraints={
-                "constraint1": RequiresConstraintDefinition(
-                    require=ContainerReference(space="space1", external_id="container2")
-                )
-            },
-            indexes={"index1": BtreeIndex(properties=["prop1"])},
-        )
-        new_container = current_container.model_copy(update={"constraints": {}, "indexes": {}, "properties": {}})
-        current_view = ViewRequest(
-            space="space1",
-            externalId="view1",
-            version="v1",
-            properties={
-                "prop1": ViewCorePropertyRequest(
-                    container=current_container.as_reference(), containerPropertyIdentifier="prop1"
-                )
-            },
-        )
-        new_view = current_view.model_copy(update={"properties": {}})
-        model = DataModelRequest(space="space1", externalId="model1", version="v1", views=[current_view.as_reference()])
-        new_model = model.model_copy(update={"views": []}, deep=True)
+@pytest.fixture
+def plan_with_removals() -> ResourceDeploymentPlanList:
+    current_container = ContainerRequest(
+        space="space1",
+        externalId="container1",
+        name="container1",
+        properties={"prop1": ContainerPropertyDefinition(type=TextProperty())},
+        constraints={
+            "constraint1": RequiresConstraintDefinition(
+                require=ContainerReference(space="space1", external_id="container2")
+            )
+        },
+        indexes={"index1": BtreeIndex(properties=["prop1"])},
+    )
+    new_container = current_container.model_copy(update={"constraints": {}, "indexes": {}, "properties": {}})
+    current_view = ViewRequest(
+        space="space1",
+        externalId="view1",
+        version="v1",
+        properties={
+            "prop1": ViewCorePropertyRequest(
+                container=current_container.as_reference(), containerPropertyIdentifier="prop1"
+            )
+        },
+    )
+    new_view = current_view.model_copy(update={"properties": {}})
+    model = DataModelRequest(space="space1", externalId="model1", version="v1", views=[current_view.as_reference()])
+    new_model = model.model_copy(update={"views": []}, deep=True)
 
-        plan = ResourceDeploymentPlanList(
-            [
-                ResourceDeploymentPlan(
-                    endpoint="containers",
-                    resources=[
-                        ResourceChange(
-                            resource_id=current_container.as_reference(),
-                            new_value=new_container,
-                            current_value=current_container,
-                            changes=ContainerDiffer().diff(current_container, new_container),
-                        )
-                    ],
-                ),
-                ResourceDeploymentPlan(
-                    endpoint="views",
-                    resources=[
-                        ResourceChange(
-                            resource_id=current_view.as_reference(),
-                            new_value=new_view,
-                            current_value=current_view,
-                            changes=ViewDiffer(
-                                current_container_map={current_container.as_reference(): current_container},
-                                new_container_map={new_container.as_reference(): new_container},
-                            ).diff(current_view, new_view),
-                        )
-                    ],
-                ),
-                ResourceDeploymentPlan(
-                    endpoint="datamodels",
-                    resources=[
-                        ResourceChange(
-                            resource_id=model.as_reference(),
-                            new_value=new_model,
-                            current_value=model,
-                            changes=DataModelDiffer().diff(model, new_model),
-                        )
-                    ],
-                ),
-            ]
-        )
+    return ResourceDeploymentPlanList(
+        [
+            ResourceDeploymentPlan(
+                endpoint="containers",
+                resources=[
+                    ResourceChange(
+                        resource_id=current_container.as_reference(),
+                        new_value=new_container,
+                        current_value=current_container,
+                        changes=ContainerDiffer().diff(current_container, new_container),
+                    )
+                ],
+            ),
+            ResourceDeploymentPlan(
+                endpoint="views",
+                resources=[
+                    ResourceChange(
+                        resource_id=current_view.as_reference(),
+                        new_value=new_view,
+                        current_value=current_view,
+                        changes=ViewDiffer(
+                            current_container_map={current_container.as_reference(): current_container},
+                            new_container_map={new_container.as_reference(): new_container},
+                        ).diff(current_view, new_view),
+                    )
+                ],
+            ),
+            ResourceDeploymentPlan(
+                endpoint="datamodels",
+                resources=[
+                    ResourceChange(
+                        resource_id=model.as_reference(),
+                        new_value=new_model,
+                        current_value=model,
+                        changes=DataModelDiffer().diff(model, new_model),
+                    )
+                ],
+            ),
+        ]
+    )
+
+
+class TestResourceDeploymentPlanList:
+    def test_consolidate_resources(self, plan_with_removals: ResourceDeploymentPlanList) -> None:
+        plan = plan_with_removals
 
         consolidated_plan = plan.consolidate_changes()
 
         assert len(consolidated_plan) == 3
-        resource_plan: ResourceDeploymentPlan
         for resource_plan in consolidated_plan.data:
             # All removed properties, constraints, indexes, and views should lead to unchanged resources
             assert len(resource_plan.unchanged) == 1
