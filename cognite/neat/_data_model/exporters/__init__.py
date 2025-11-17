6c574c498b8b160d0f46c59ddd04113e4370f69a
anders-albert <anders0albert@gmail.com>
2025-11-17 14:46:03 +0100

    refactro: added exporter

--- cognite/neat/_data_model/exporters/__init__.py
+++ cognite/neat/_data_model/exporters/__init__.py
@@ -1,9 +1,10 @@
-from ._api_exporter import DMSAPIExporter, DMSAPIYAMLExporter
+from ._api_exporter import DMSAPIExporter, DMSAPIJSONExporter, DMSAPIYAMLExporter
 from ._base import DMSExporter, DMSFileExporter
 from ._table_exporter.exporter import DMSExcelExporter, DMSTableExporter, DMSYamlExporter
 
 __all__ = [
     "DMSAPIExporter",
+    "DMSAPIJSONExporter",
     "DMSAPIYAMLExporter",
     "DMSExcelExporter",
     "DMSExporter",
