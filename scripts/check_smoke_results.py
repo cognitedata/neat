ca824f8d10d507795eb8689f69190fdc6234314a
anders-albert <anders0albert@gmail.com>
2025-11-30 13:06:06 +0100

    feat: implemented more of script

--- scripts/check_smoke_results.py
+++ scripts/check_smoke_results.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime, timezone
 from dataclasses import dataclass, field
 from typing import Literal, TypeAlias
-
+import httpx
 from pydantic import BaseModel, JsonValue
 # Environment variable names
 SLACK_WEBHOOK_URL_NAME = "SLACK_WEBHOOK_URL"
@@ -17,6 +17,11 @@ class Context:
     now: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
 
 
+class SlackMessage(BaseModel):
+    topic: Literal["Smoke Test Execution", "DMS Service", "Cognite Core Model"]
+    message: str
+
+
 def check_smoke_tests_results(pytest_report: Path, context: Context) -> None:
     """Check the smoke tests results from a pytest report file.
 
@@ -24,7 +29,19 @@ def check_smoke_tests_results(pytest_report: Path, context: Context) -> None:
         pytest_report (Path): Path to the pytest report file.
         context (Context): Context object that contains information about the test run.
     """
-    raise NotImplementedError()
+    report = _load_pytest_report(pytest_report, context)
+    if report is None:
+        return None
+
+    messages = _check_results(report, context)
+
+    if not messages:
+        if alive_message := _alive_message(context.now):
+            _notify_slack([alive_message], context)
+        return None
+
+    _notify_slack(messages, context)
+    return None
 
 
 Outcome: TypeAlias = Literal["passed", "failed", "skipped", "error", "xfailed", "xpassed"]
@@ -75,6 +92,39 @@ class PytestReportModel(BaseModel):
     tests: list[TestResult] | None = None
     warnings: list[JsonValue] | None = None
 
+def _load_pytest_report(pytest_report: Path, context: Context) -> PytestReportModel | None:
+    if not pytest_report.is_file():
+        _notify_slack([SlackMessage(
+            topic="Smoke Test Execution",
+            message=f"Smoke tests failed to execute. Report file {pytest_report.name!r} does not exist. Go to {context.github_repo_url} to investigate."
+        )], context)
+        return None
+    try:
+        return PytestReportModel.model_validate_json(pytest_report.read_text())
+    except ValueError as _:
+        _notify_slack([SlackMessage(
+            topic="Smoke Test Execution",
+            message=f"Smoke tests failed to execute. Report file {pytest_report.name!r} is not a valid pytest report. Go to {context.github_repo_url} to investigate."
+        )], context)
+        return None
+
+
+def _check_results(report: PytestReportModel, context: Context) -> list[SlackMessage]:
+    raise NotImplementedError()
+
+def _alive_message(now: datetime) -> SlackMessage | None:
+    """Create an "alive" message if the current time is Monday in the morning UTC."""
+    if now.weekday() == 0 and 2 <= now.hour < 10:
+        return SlackMessage(
+            topic="Smoke Test Execution",
+            message="Smoke tests are running fine. No issues detected. Have a great week ahead!"
+        )
+    return None
+
+
+def _notify_slack(messages: list[SlackMessage], context: Context) -> None:
+    for message in messages:
+        httpx.post(context.slack_webhook_url, content=message.model_dump_json())
 
 
 if __name__ == '__main__':
