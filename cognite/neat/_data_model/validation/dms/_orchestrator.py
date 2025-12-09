f4f94392d818e825fa8228e0d8c170a79dcb5f70
nikokaoja <nikola.vasiljevic@cognite.com>
2025-12-09 15:57:16 +0100

    simplify validator init

--- cognite/neat/_data_model/validation/dms/_orchestrator.py
+++ cognite/neat/_data_model/validation/dms/_orchestrator.py
@@ -1,50 +1,12 @@
 from collections.abc import Callable
 
-from cognite.neat._client import NeatClient
 from cognite.neat._data_model._shared import OnSuccessIssuesChecker
 from cognite.neat._data_model.models.dms._limits import SchemaLimits
 from cognite.neat._data_model.models.dms._schema import RequestSchema
-from cognite.neat._data_model.validation.dms._connections import (
-    ConnectionValueTypeUndefined,
-    ConnectionValueTypeUnexisting,
-    ReverseConnectionContainerMissing,
-    ReverseConnectionContainerPropertyMissing,
-    ReverseConnectionContainerPropertyWrongType,
-    ReverseConnectionPointsToAncestor,
-    ReverseConnectionSourcePropertyMissing,
-    ReverseConnectionSourcePropertyWrongType,
-    ReverseConnectionSourceViewMissing,
-    ReverseConnectionTargetMismatch,
-    ReverseConnectionTargetMissing,
-)
-from cognite.neat._data_model.validation.dms._limits import (
-    ContainerPropertyCountIsOutOfLimits,
-    ContainerPropertyListSizeIsOutOfLimits,
-    DataModelViewCountIsOutOfLimits,
-    ViewContainerCountIsOutOfLimits,
-    ViewImplementsCountIsOutOfLimits,
-    ViewPropertyCountIsOutOfLimits,
-)
+from cognite.neat._utils.auxiliary import get_concrete_subclasses
 from cognite.neat._utils.useful_types import ModusOperandi
 
-from ._ai_readiness import (
-    DataModelMissingDescription,
-    DataModelMissingName,
-    EnumerationMissingDescription,
-    EnumerationMissingName,
-    ViewMissingDescription,
-    ViewMissingName,
-    ViewPropertyMissingDescription,
-    ViewPropertyMissingName,
-)
-from ._base import DataModelValidator, CDFSnapshot, LocalSnapshot, ValidationResources
-from ._consistency import ViewSpaceVersionInconsistentWithDataModel
-from ._containers import (
-    ExternalContainerDoesNotExist,
-    ExternalContainerPropertyDoesNotExist,
-    RequiredContainerDoesNotExist,
-)
-from ._views import ImplementedViewNotExisting, ViewToContainerMappingNotPossible
+from ._base import CDFSnapshot, DataModelValidator, LocalSnapshot, ValidationResources
 
 
 class DmsDataModelValidation(OnSuccessIssuesChecker):
@@ -67,48 +29,11 @@ class DmsDataModelValidation(OnSuccessIssuesChecker):
     def run(self, request_schema: RequestSchema) -> None:
         """Run quality assessment on the DMS data model."""
 
-
         validation_resources = self._gather_validation_resources(request_schema)
 
         # Initialize all validators
         validators: list[DataModelValidator] = [
-            # Limits
-            DataModelViewCountIsOutOfLimits(validation_resources),
-            ViewPropertyCountIsOutOfLimits(validation_resources),
-            ViewImplementsCountIsOutOfLimits(validation_resources),
-            ViewContainerCountIsOutOfLimits(validation_resources),
-            ContainerPropertyCountIsOutOfLimits(validation_resources),
-            ContainerPropertyListSizeIsOutOfLimits(validation_resources),
-            # Views
-            ViewToContainerMappingNotPossible(validation_resources),
-            ImplementedViewNotExisting(validation_resources),
-            # Containers
-            ExternalContainerDoesNotExist(validation_resources),
-            ExternalContainerPropertyDoesNotExist(validation_resources),
-            RequiredContainerDoesNotExist(validation_resources),
-            # Consistency
-            ViewSpaceVersionInconsistentWithDataModel(validation_resources),
-            # Connections
-            ConnectionValueTypeUnexisting(validation_resources),
-            ConnectionValueTypeUndefined(validation_resources),
-            ReverseConnectionContainerMissing(validation_resources),
-            ReverseConnectionContainerPropertyMissing(validation_resources),
-            ReverseConnectionContainerPropertyWrongType(validation_resources),
-            ReverseConnectionSourceViewMissing(validation_resources),
-            ReverseConnectionSourcePropertyMissing(validation_resources),
-            ReverseConnectionSourcePropertyWrongType(validation_resources),
-            ReverseConnectionPointsToAncestor(validation_resources),
-            ReverseConnectionTargetMismatch(validation_resources),
-            ReverseConnectionTargetMissing(validation_resources),
-            # AI Readiness
-            DataModelMissingName(validation_resources),
-            DataModelMissingDescription(validation_resources),
-            ViewMissingName(validation_resources),
-            ViewMissingDescription(validation_resources),
-            ViewPropertyMissingName(validation_resources),
-            ViewPropertyMissingDescription(validation_resources),
-            EnumerationMissingName(validation_resources),
-            EnumerationMissingDescription(validation_resources),
+            cls(validation_resources) for cls in get_concrete_subclasses(DataModelValidator)
         ]
 
         # Run validators
@@ -121,7 +46,7 @@ class DmsDataModelValidation(OnSuccessIssuesChecker):
     def _gather_validation_resources(self, request_schema: RequestSchema) -> ValidationResources:
         return ValidationResources(
             cdf=self._cdf_snapshot,
-            local=LocalSnapshot.from_request_schema(request_schema),
+            local=LocalSnapshot.from_request_schema(request_schema.model_copy(deep=True)),
             limits=self._limits,
             modus_operandi=self._modus_operandi,
         )
