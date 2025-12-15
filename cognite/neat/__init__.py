b301d2ec67ea4beb9cb3100439bf6033ccb708c5
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2025-12-15 16:14:10 +0100

    This 978 neat config docs (#1512)
    # Description
    
    Add docs for NeatConfig
    
    ## Bump
    
    - [ ] Patch
    - [x] Skip

--- cognite/neat/__init__.py
+++ cognite/neat/__init__.py
@@ -1,7 +1,7 @@
 from cognite.neat._v0.core._utils.auth import get_cognite_client
 
-from ._config import NeatConfig
+from ._config import NeatConfig, get_neat_config_from_file
 from ._session import NeatSession
 from ._version import __version__
 
-__all__ = ["NeatConfig", "NeatSession", "__version__", "get_cognite_client"]
+__all__ = ["NeatConfig", "NeatSession", "__version__", "get_cognite_client", "get_neat_config_from_file"]
