e7a4da1d7c33d6d8ace01b43dccba4a33cafbaf2
anders-albert <anders0albert@gmail.com>
2026-01-15 07:59:21 +0100

    tests: added invalid cases

--- tests/tests_unit/test_data_model/test_importers/test_dms_table_importer.py
+++ tests/tests_unit/test_data_model/test_importers/test_dms_table_importer.py
@@ -5,7 +5,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from pydantic import TypeAdapter
+from pydantic import TypeAdapter, ValidationError
 
 from cognite.neat._data_model.exporters._table_exporter.exporter import (
     DMSExcelExporter,
@@ -1561,6 +1561,23 @@ class TestTableViewFilter:
         assert result.model_dump() == expected.model_dump()
 
         assert str(result) == expected_dump_str
+
+    @pytest.mark.parametrize(
+        "cell_text",
+        [
+            pytest.param("hasData()", id="Empty entity list"),
+            pytest.param("nodeType( )", id="Empty entity list with space"),
+            pytest.param("hasData(my_space:)", id="Missing externalId"),
+        ],
+    )
+    def test_parse_invalid(self, cell_text: str) -> None:
+        """Test parsing of invalid TableViewFilter inputs
+
+        Note that most invalid cases will just return as a RAWFilter and not raise an error.
+        They will instead be caught during the data model import validation together with other errors.
+        """
+        with pytest.raises(ValidationError):
+            _ = TypeAdapter(TableViewFilter).validate_python(cell_text)
 
 
 class TestDMSTableImporter:
