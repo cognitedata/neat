89ab62a250224947b7b4e6752d929689c35a4688
nikokaoja <nikola.vasiljevic@cognite.com>
2026-01-20 17:28:26 +0100

    make legacy optional

--- cognite/neat/legacy.py
+++ cognite/neat/legacy.py
@@ -1,3 +1,14 @@
+import importlib
+
+from cognite.neat._exceptions import NeatImportError
+
+try:
+    importlib.import_module("rdflib")
+    # Legacy is installed, allow v0 tests
+except ImportError as e:
+    raise NeatImportError("legacy module", "legacy") from e
+
+
 from cognite.neat._v0.core._utils.auth import get_cognite_client
 from cognite.neat._v0.session._base import NeatSession
 
