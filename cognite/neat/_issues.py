0adeb29de827e3206e3a57ab2a7211f568582233
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2026-02-03 10:15:21 +0100

    Reverting a unecessary change

--- cognite/neat/_issues.py
+++ cognite/neat/_issues.py
@@ -4,13 +4,7 @@ from pydantic import BaseModel
 
 
 class Issue(BaseModel):
-    """Base class for all issues.
-
-    Attributes:
-        message: Human-readable description of the issue.
-        code: Unique code identifying the type of issue (e.g., "NEAT-DMS-PERFORMANCE-001").
-        fix: Human-readable description of how to fix the issue.
-    """
+    """Base class for all issues"""
 
     message: str
     code: str | None = None
