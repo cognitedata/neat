04ff7c4afae997967d94d6812801a2cb9cd07bd4
nikokaoja <nikola.vasiljevic@cognite.com>
2026-02-26 12:57:49 +0100

    clean imports

--- cognite/neat/_toolkit_adapter.py
+++ cognite/neat/_toolkit_adapter.py
@@ -7,9 +7,15 @@ from cognite.neat._data_model._snapshot import SchemaSnapshot
 from cognite.neat._data_model.importers import DMSAPIImporter
 from cognite.neat._data_model.models.dms._limits import SchemaLimits
 from cognite.neat._data_model.rules.dms import DmsDataModelRulesOrchestrator
-from cognite.neat._issues import ConsistencyError as NeatConsistencyError
-from cognite.neat._issues import ModelSyntaxError as NeatModelSyntaxError
-from cognite.neat._issues import Recommendation as NeatRecommendation
+from cognite.neat._issues import (
+    ConsistencyError as NeatConsistencyError,
+)
+from cognite.neat._issues import (
+    ModelSyntaxError as NeatModelSyntaxError,
+)
+from cognite.neat._issues import (
+    Recommendation as NeatRecommendation,
+)
 
 __all__ = [
     "DMSAPIImporter",
