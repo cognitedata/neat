f0b015f6403ba9ec09a69d151f57b1975f586e27
nikokaoja <nikola.vasiljevic@cognite.com>
2026-02-26 12:57:17 +0100

    simplify test

--- tests/tests_unit/test_toolkit_adapter.py
+++ tests/tests_unit/test_toolkit_adapter.py
@@ -1,52 +1,47 @@
-from cognite.neat._toolkit_adapter import (
-    DMSAPIImporter,
-    DmsDataModelRulesOrchestrator,
-    NeatClient,
-    NeatConsistencyError,
-    NeatModelSyntaxError,
-    NeatRecommendation,
-    SchemaLimits,
-    SchemaSnapshot,
-    __all__,
+import pytest
+
+from cognite.neat import _toolkit_adapter
+from cognite.neat._client import NeatClient
+from cognite.neat._data_model._snapshot import SchemaSnapshot
+from cognite.neat._data_model.importers import DMSAPIImporter
+from cognite.neat._data_model.models.dms._limits import SchemaLimits
+from cognite.neat._data_model.rules.dms import DmsDataModelRulesOrchestrator
+from cognite.neat._issues import (
+    ConsistencyError,
+    ModelSyntaxError,
+    Recommendation,
 )
 
 
-class TestToolkitAdapterImports:
-    """Test that all expected exports are available from the toolkit adapter module."""
+@pytest.mark.parametrize(
+    "name, original_obj",
+    [
+        ("DMSAPIImporter", DMSAPIImporter),
+        ("DmsDataModelRulesOrchestrator", DmsDataModelRulesOrchestrator),
+        ("NeatClient", NeatClient),
+        ("NeatConsistencyError", ConsistencyError),
+        ("NeatModelSyntaxError", ModelSyntaxError),
+        ("NeatRecommendation", Recommendation),
+        ("SchemaLimits", SchemaLimits),
+        ("SchemaSnapshot", SchemaSnapshot),
+    ],
+)
+def test_adapter_exports_correct_object(name: str, original_obj: object) -> None:
+    """Tests that the toolkit adapter module exports the correct objects."""
+    adapter_obj = getattr(_toolkit_adapter, name)
+    assert adapter_obj is original_obj
 
-    def test_dms_api_importer_is_importable(self) -> None:
-        assert DMSAPIImporter is not None
 
-    def test_dms_data_model_rules_orchestrator_is_importable(self) -> None:
-        assert DmsDataModelRulesOrchestrator is not None
-
-    def test_neat_client_is_importable(self) -> None:
-        assert NeatClient is not None
-
-    def test_neat_consistency_error_is_importable(self) -> None:
-        assert NeatConsistencyError is not None
-
-    def test_neat_model_syntax_error_is_importable(self) -> None:
-        assert NeatModelSyntaxError is not None
-
-    def test_neat_recommendation_is_importable(self) -> None:
-        assert NeatRecommendation is not None
-
-    def test_schema_snapshot_is_importable(self) -> None:
-        assert SchemaSnapshot is not None
-
-    def test_schema_limits_is_importable(self) -> None:
-        assert SchemaLimits is not None
-
-    def test_all_exports_are_defined(self) -> None:
-        expected_exports = [
-            "DMSAPIImporter",
-            "DmsDataModelRulesOrchestrator",
-            "NeatConsistencyError",
-            "NeatModelSyntaxError",
-            "NeatRecommendation",
-            "SchemaSnapshot",
-            "SchemaLimits",
-            "NeatClient",
-        ]
-        assert set(__all__) == set(expected_exports)
+def test_all_exports_are_defined() -> None:
+    """Tests that __all__ in the adapter module is correct."""
+    expected_exports = {
+        "DMSAPIImporter",
+        "DmsDataModelRulesOrchestrator",
+        "NeatClient",
+        "NeatConsistencyError",
+        "NeatModelSyntaxError",
+        "NeatRecommendation",
+        "SchemaLimits",
+        "SchemaSnapshot",
+    }
+    assert set(_toolkit_adapter.__all__) == expected_exports
