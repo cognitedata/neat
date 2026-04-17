6b8a27c5949260ceea45802fe2402560eb60a650
nikokaoja <nikola.vasiljevic@cognite.com>
2026-04-17 14:34:57 +0200

    extend test

--- tests/tests_unit/test_plugins.py
+++ tests/tests_unit/test_plugins.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cognite.neat._plugin._interfaces import PhysicalDataModelReaderPlugin
+from cognite.neat._plugin._interfaces import PhysicalDataModelFileWriterPlugin, PhysicalDataModelReaderPlugin
 from cognite.neat._plugin._manager import PluginManager
 
 
@@ -18,4 +18,5 @@ class TestPluginManager:
         """Test loading an external plugin."""
         # Assuming external plugins are discovered automatically
         assert len(manager.get(PhysicalDataModelReaderPlugin)) == 1, "No PhysicalDataModelReaderPlugin found"
+        assert len(manager.get(PhysicalDataModelFileWriterPlugin)) == 1, "No PhysicalDataModelFileWriterPlugin found"
         assert len(manager.get(TestPlugin)) == 0, "TestPlugin should not be loaded"
